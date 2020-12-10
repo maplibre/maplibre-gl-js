8ce2f8fad9e790a9e073cf4bbe4fec2b0dab64f0
Seth Nickell <snickell@gmail.com>
2020-12-10 00:28:30 -1000

    Update docs in publish-release.sh to help others carry the torch

--- build/publish-release.sh
+++ build/publish-release.sh
@@ -3,9 +3,9 @@
 # Publishes a repeatable release of maplibre-gl to NPM
 #
 # USAGE:
-# 1. Update version: in package.json
-# 2. Commit and push package.json: to maplibre/maplibre-gl-js#main
-# 3. Run: ./build/publish-release.sh
+# 1. Run: ./build/publish-release.sh
+# 2. Yarn will prompt you for a new version, enter it, and it will publish
+# 3. Commit the new package.json on your host with the updated version
 # 4. Verify: new version is up at https://www.npmjs.com/package/maplibre-gl
 
 export NPM_TOKEN=$(cat ~/.npmrc | grep -o '_authToken=.*' | sed 's/_authToken=//g')
