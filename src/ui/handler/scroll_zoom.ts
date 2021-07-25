2bbf9d62f3d000db3cf76c47fa30f8f94a96b43b
James Hamilton <james@thechipfactory.co.uk>
2021-07-25 19:33:17 +0900

    more fixes

--- src/ui/handler/scroll_zoom.ts
+++ src/ui/handler/scroll_zoom.ts
@@ -148,8 +148,6 @@ class ScrollZoomHandler {
 
     wheel(e: WheelEvent) {
         if (!this.isEnabled()) return;
-
-        // Remove `any` cast when https://github.com/facebook/flow/issues/4879 is fixed.
         let value = e.deltaMode === WheelEvent.DOM_DELTA_LINE ? e.deltaY * 40 : e.deltaY;
         const now = browser.now(),
             timeDelta = now - (this._lastWheelEventTime || 0);
