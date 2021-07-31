e0c7f894ba4b5b32fbd25f52ef5a07680fe5de67
James Hamilton <chippieTV@users.noreply.github.com>
2021-08-01 02:51:28 +0900

    get node query tests running (#233)

--- test/query.test.js
+++ test/query.test.js
@@ -1,11 +1,14 @@
 /* eslint-disable import/unambiguous, import/no-commonjs, no-global-assign */
 
 import './stub_loader';
-import querySuite from './integration/lib/query';
+import * as querySuite from './integration/lib/query';
 import suiteImplementation from './suite_implementation';
 import ignores from './ignores.json';
+import {fileURLToPath} from 'url';
 
 let tests;
+
+const __filename = fileURLToPath(import.meta.url);
 
 if (process.argv[1] === __filename && process.argv.length > 2) {
     tests = process.argv.slice(2);
