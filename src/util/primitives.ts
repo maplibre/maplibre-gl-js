c15469fc9fe6fe4f326254a20545b47bdd5a474e
HarelM <harel.mazor@gmail.com>
2021-08-04 09:58:12 +0300

    Remove to vec3 as to code review requirements

--- src/util/primitives.ts
+++ src/util/primitives.ts
@@ -6,10 +6,6 @@ class Frustum {
     constructor(public points: vec4[], public planes: vec4[]) { } // eslint-disable-line
 
     public static fromInvProjectionMatrix(invProj: mat4, worldSize: number, zoom: number): Frustum {
-        const toVec3 = (vec4: vec4): vec3 => {
-            return vec3.fromValues(vec4[0], vec4[1], vec4[2]);
-        };
-
         const clipSpaceCorners = [
             vec4.fromValues(-1, 1, -1, 1),
             vec4.fromValues(1, 1, -1, 1),
@@ -29,19 +25,19 @@ class Frustum {
             .map(v => vec4.scale([] as any, v, 1.0 / v[3] / worldSize * scale));
 
         const frustumPlanePointIndices = [
-            vec3.fromValues(0, 1, 2),  // near
-            vec3.fromValues(6, 5, 4),  // far
-            vec3.fromValues(0, 3, 7),  // left
-            vec3.fromValues(2, 1, 5),  // right
-            vec3.fromValues(3, 2, 6),  // bottom
-            vec3.fromValues(0, 4, 5)   // top
+            [0, 1, 2],  // near
+            [6, 5, 4],  // far
+            [0, 3, 7],  // left
+            [2, 1, 5],  // right
+            [3, 2, 6],  // bottom
+            [0, 4, 5]   // top
         ];
 
-        const frustumPlanes = frustumPlanePointIndices.map((p: vec3) => {
-            const a = vec3.sub(vec3.create(), toVec3(frustumCoords[p[0]]), toVec3(frustumCoords[p[1]]));
-            const b = vec3.sub(vec3.create(), toVec3(frustumCoords[p[2]]), toVec3(frustumCoords[p[1]]));
+        const frustumPlanes = frustumPlanePointIndices.map((p: number[]) => {
+            const a = vec3.sub(vec3.create(), frustumCoords[p[0]] as vec3, frustumCoords[p[1]] as vec3);
+            const b = vec3.sub(vec3.create(), frustumCoords[p[2]] as vec3, frustumCoords[p[1]] as vec3);
             const n = vec3.normalize(vec3.create(), vec3.cross(vec3.create(), a, b));
-            const d = -vec3.dot(n, toVec3(frustumCoords[p[1]]));
+            const d = -vec3.dot(n, frustumCoords[p[1]] as vec3);
             return [n[0], n[1], n[2], d] as any as vec4;
         });
 
