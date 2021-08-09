1d8e3f7b65b147f49aa6d4be5e808727aa28633d
HarelM <harel.mazor@gmail.com>
2021-08-09 23:45:49 +0300

    Fix browser tests due to incorrect lint fix

--- test/unit/ui/control/fullscreen.test.js
+++ test/unit/ui/control/fullscreen.test.js
@@ -35,7 +35,7 @@ test('FullscreenControl makes optional container element full screen', (t) => {
     const fullscreen = new FullscreenControl({container: window.document.querySelector('body')});
     map.addControl(fullscreen);
     const control = map._controls.find((ctrl) => {
-        return Object.hasOwnProperty.call(ctrl, '_fullscreen');
+        return Object.prototype.hasOwnProperty.call(ctrl, '_fullscreen');
     });
     control._onClickFullscreen();
 
