db2bed5426b0e3ef5c84baebf6de2512b8d15576
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2021-08-29 00:28:15 +0200

    Declare `emplace()` with variable number of arguments (#262)

--- src/util/struct_array.ts
+++ src/util/struct_array.ts
@@ -95,8 +95,8 @@ abstract class StructArray {
     // The following properties are defined on the prototype.
     members: Array<StructArrayMember>;
     bytesPerElement: number;
-    abstract emplaceBack(v0: number, v1?: number, v2?: number, v3?: number, v4?:number, v5?:number, v6?:number, v7?:number, v8?:number, v9?:number, v10?:number, v11?:number, v12?:number, v13?:number, v14?:number, v15?:number, v16?:number, v17?:number, v18?:number, v19?:number, v20?:number, v21?:number, v22?:number, v23?:number, v24?:number, v25?:number, v26?:number, v27?:number);
-    abstract emplace(i: number, v0: number, v1?: number, v2?:number, v3?:number, v4?:number, v5?:number, v6?:number, v7?:number, v8?:number, v9?:number, v10?:number, v11?:number, v12?:number, v13?:number, v14?:number, v15?:number, v16?:number, v17?:number, v18?:number, v19?:number, v20?:number, v21?:number, v22?:number, v23?:number, v24?:number, v25?:number, v26?:number, v27?:number);
+    abstract emplaceBack(...v: number[]);
+    abstract emplace(i: number, ...v: number[]);
 
     constructor() {
         this.isTransferred = false;
