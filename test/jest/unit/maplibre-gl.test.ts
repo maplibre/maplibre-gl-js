f82fa5f4e0f775f40de89ed0e4b0cd0243e01b93
astridx <info@astrid-guenther.de>
2021-09-06 10:01:54 +0200

    wip

--- test/jest/unit/maplibre-gl.test.ts
+++ test/jest/unit/maplibre-gl.test.ts
@@ -1,11 +1,15 @@
 import maplibregl from '../../../rollup/build/tsc';
 
-test('maplibregl', () => {
+test('Make sure, that maplibregl is defined', () => {
+    expect(maplibregl).not.toBeNull();
+});
+
+test('Make sure, that maplibregl has workers', () => {
     expect(typeof maplibregl.workerCount).toBe('number');
 });
 
-test('maplibregl', () => {
-    expect(maplibregl.workerCount).not.toBeNull();
+test('Check that the tests also work the other way round', () => {
+    expect(typeof maplibregl.workerCount).not.toBe('string');
 });
 
 export {};
