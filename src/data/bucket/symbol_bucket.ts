00185140f2852342b455477457b9f0ae1f9413b9
James Hamilton <chippieTV@users.noreply.github.com>
2021-09-23 19:44:23 +0900

    Test render revert (#375)
    * update WritingMode to use enum properly
    
    * add WritingMode.none

--- src/data/bucket/symbol_bucket.ts
+++ src/data/bucket/symbol_bucket.ts
@@ -23,7 +23,7 @@ import {TriangleIndexArray, LineIndexArray} from '../index_array_type';
 import transformText from '../../symbol/transform_text';
 import mergeLines from '../../symbol/mergelines';
 import {allowsVerticalWritingMode, stringContainsRTLText} from '../../util/script_detection';
-import {WriteModeMap, WritingMode} from '../../symbol/shaping';
+import {WritingMode} from '../../symbol/shaping';
 import loadGeometry from '../load_geometry';
 import toEvaluationFeature from '../evaluation_feature';
 import mvt from '@mapbox/vector-tile';
@@ -356,7 +356,7 @@ class SymbolBucket implements Bucket {
     sourceLayerIndex: number;
     sourceID: string;
     symbolInstanceIndexes: Array<number>;
-    writingModes: Array<number>;
+    writingModes: WritingMode[];
     allowVerticalPlacement: boolean;
     hasRTLText: boolean;
 
@@ -396,7 +396,7 @@ class SymbolBucket implements Bucket {
         this.sortFeaturesByY = zOrderByViewportY && this.canOverlap;
 
         if (layout.get('symbol-placement') === 'point') {
-            this.writingModes = layout.get('text-writing-mode').map(wm => WriteModeMap[wm]);
+            this.writingModes = layout.get('text-writing-mode').map(wm => WritingMode[wm]);
         }
 
         this.stateDependentLayerIds = this.layers.filter((l) => l.isStateDependent()).map((l) => l.id);
