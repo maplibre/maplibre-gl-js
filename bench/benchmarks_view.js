aab2aa93da91c124229ff2a0d4e40e69461b0a45
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2021-09-30 19:15:57 +0200

    Fix page title

--- bench/benchmarks_view.js
+++ bench/benchmarks_view.js
@@ -458,7 +458,7 @@ class BenchmarksTable extends React.Component {
     render() {
         return (
             <div style={{width: 960, margin: '2em auto'}}>
-                <h1 className="space-bottom1">Mapbox GL JS Benchmarks – {
+                <h1 className="space-bottom1">MapLibre GL JS Benchmarks – {
                     this.props.finished ?
                         <span>Finished</span> :
                         <span>Running</span>}</h1>
