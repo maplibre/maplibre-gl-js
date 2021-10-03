1ed5a31ef7f0e005af8a1ce172627088d8262eab
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2021-10-03 17:32:14 +0200

    Remove `as any`

--- bench/benchmarks/expressions.ts
+++ bench/benchmarks/expressions.ts
@@ -10,14 +10,16 @@ import type {StyleSpecification} from '../../src/style-spec/types';
 import type {StylePropertySpecification} from '../../src/style-spec/style-spec';
 import type {StylePropertyExpression} from '../../src/style-spec/expression';
 
+interface DataT {
+    propertySpec: StylePropertySpecification;
+    rawValue: unknown;
+    rawExpression: unknown;
+    compiledFunction: StylePropertyExpression;
+    compiledExpression: StylePropertyExpression;
+}
+
 class ExpressionBenchmark extends Benchmark {
-    data: Array<{
-      propertySpec: StylePropertySpecification;
-      rawValue: unknown;
-      rawExpression: unknown;
-      compiledFunction: StylePropertyExpression;
-      compiledExpression: StylePropertyExpression;
-    }>;
+    data: Array<DataT>;
     style: string | StyleSpecification;
 
     constructor(style: string | StyleSpecification) {
@@ -37,9 +39,9 @@ class ExpressionBenchmark extends Benchmark {
                         continue;
                     }
 
-                    const expressionData = function(rawValue, propertySpec: StylePropertySpecification) {
+                    const expressionData = (rawValue, propertySpec: StylePropertySpecification): DataT => {
                         const rawExpression = convertFunction(rawValue, propertySpec);
-                        const compiledFunction = createFunction(rawValue, propertySpec);
+                        const compiledFunction = createFunction(rawValue, propertySpec) as StylePropertyExpression;
                         const compiledExpression = createPropertyExpression(rawExpression, propertySpec);
                         if (compiledExpression.result === 'error') {
                             throw new Error(compiledExpression.value.map(err => `${err.key}: ${err.message}`).join(', '));
@@ -55,13 +57,13 @@ class ExpressionBenchmark extends Benchmark {
 
                     for (const key in layer.paint) {
                         if (isFunction(layer.paint[key])) {
-                            this.data.push(expressionData(layer.paint[key], spec[`paint_${layer.type}`][key]) as any);
+                            this.data.push(expressionData(layer.paint[key], spec[`paint_${layer.type}`][key]));
                         }
                     }
 
                     for (const key in layer.layout) {
                         if (isFunction(layer.layout[key])) {
-                            this.data.push(expressionData(layer.layout[key], spec[`layout_${layer.type}`][key]) as any);
+                            this.data.push(expressionData(layer.layout[key], spec[`layout_${layer.type}`][key]));
                         }
                     }
                 }
