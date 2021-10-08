d3fb3b614737b92f65797262c739ef8d9725e0ea
astridx <info@astrid-guenther.de>
2021-10-09 01:43:45 +0200

    2. Error Jest encountered an unexpected token

--- jest.config.cjs
+++ jest.config.cjs
@@ -7,7 +7,10 @@ module.exports = {
     "**/?(*.)+(spec|test).+(ts|tsx|js)"
   ],
   "transform": {
-    "^.+\\.(ts|tsx)$": "ts-jest"
+    "^.+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.(js)$": "babel-jest",
   },
   testEnvironment: "jsdom",
+  transformIgnorePatterns: [
+  ],
 }
