1fb8885a0f9fde0b666ae8ad43fcf526760665bc
astridx <info@astrid-guenther.de>
2021-10-09 13:37:17 +0200

    run codemods, codemods.js and lint

--- src/geo/edge_insets.test.ts
+++ src/geo/edge_insets.test.ts
@@ -1,99 +1,90 @@
-import {test} from '../../util/test';
-import EdgeInsets from '../../../rollup/build/tsc/src/geo/edge_insets';
+import expect from 'expect';
+import EdgeInsets from '../geo/edge_insets';
 
-test('EdgeInsets', (t) => {
-    t.test('#constructor', (t) => {
-        t.ok(new EdgeInsets() instanceof EdgeInsets, 'creates an object with default values');
-        t.throws(() => {
+describe('EdgeInsets', () => {
+    test('#constructor', () => {
+        expect(new EdgeInsets() instanceof EdgeInsets).toBeTruthy();
+        expect(() => {
             new EdgeInsets(NaN, 10);
-        }, `Invalid input EdgeInsets(NaN, 10) gets detected and error is thrown`);
-        t.throws(() => {
+        }).toThrowError('Invalid input EdgeInsets(NaN, 10) gets detected and error is thrown');
+        expect(() => {
             new EdgeInsets(-10, 10, 20, 10);
-        }, `Invalid input EdgeInsets(-10, 10, 20, 10) gets detected and error is thrown`);
+        }).toThrowError(
+            'Invalid input EdgeInsets(-10, 10, 20, 10) gets detected and error is thrown'
+        );
 
-        t.test('valid initialization', (t) => {
+        test('valid initialization', () => {
             const top = 10;
             const bottom = 15;
             const left = 26;
             const right = 19;
 
             const inset = new EdgeInsets(top, bottom, left, right);
-            t.equal(inset.top, top);
-            t.equal(inset.bottom, bottom);
-            t.equal(inset.left, left);
-            t.equal(inset.right, right);
-            t.end();
+            expect(inset.top).toBe(top);
+            expect(inset.bottom).toBe(bottom);
+            expect(inset.left).toBe(left);
+            expect(inset.right).toBe(right);
         });
-        t.end();
     });
 
-    t.test('#getCenter', (t) => {
-        t.test('valid input', (t) => {
+    test('#getCenter', () => {
+        test('valid input', () => {
             const inset = new EdgeInsets(10, 15, 50, 10);
             const center = inset.getCenter(600, 400);
-            t.equal(center.x, 320);
-            t.equal(center.y, 197.5);
-            t.end();
+            expect(center.x).toBe(320);
+            expect(center.y).toBe(197.5);
         });
 
-        t.test('center clamping', (t) => {
+        test('center clamping', () => {
             const inset = new EdgeInsets(300, 200, 500, 200);
             const center = inset.getCenter(600, 400);
             // Midpoint of the overlap when padding overlaps
-            t.equal(center.x, 450);
-            t.equal(center.y, 250);
-            t.end();
+            expect(center.x).toBe(450);
+            expect(center.y).toBe(250);
         });
-        t.end();
     });
 
-    t.test('#interpolate', (t) => {
-        t.test('it works', (t) => {
+    test('#interpolate', () => {
+        test('it works', () => {
             const inset1 = new EdgeInsets(10, 15, 50, 10);
             const inset2 = new EdgeInsets(20, 30, 100, 10);
             const inset3 = inset1.interpolate(inset1, inset2, 0.5);
             // inset1 is mutated in-place
-            t.equal(inset3, inset1);
+            expect(inset3).toBe(inset1);
 
-            t.equal(inset3.top, 15);
-            t.equal(inset3.bottom, 22.5);
-            t.equal(inset3.left, 75);
-            t.equal(inset3.right, 10);
-            t.end();
+            expect(inset3.top).toBe(15);
+            expect(inset3.bottom).toBe(22.5);
+            expect(inset3.left).toBe(75);
+            expect(inset3.right).toBe(10);
         });
 
-        t.test('it retains insets that dont have new parameters passed in', (t) => {
+        test('it retains insets that dont have new parameters passed in', () => {
             const inset = new EdgeInsets(10, 15, 50, 10);
             const target = {
                 top: 20
             };
             inset.interpolate(inset, target, 0.5);
-            t.equal(inset.top, 15);
-            t.equal(inset.bottom, 15);
-            t.equal(inset.left, 50);
-            t.equal(inset.right, 10);
-            t.end();
+            expect(inset.top).toBe(15);
+            expect(inset.bottom).toBe(15);
+            expect(inset.left).toBe(50);
+            expect(inset.right).toBe(10);
         });
 
-        t.end();
     });
 
-    t.test('#equals', (t) => {
+    test('#equals', () => {
         const inset1 = new EdgeInsets(10, 15, 50, 10);
         const inset2 = new EdgeInsets(10, 15, 50, 10);
         const inset3 = new EdgeInsets(10, 15, 50, 11);
-        t.ok(inset1.equals(inset2));
-        t.notOk(inset2.equals(inset3));
-        t.end();
+        expect(inset1.equals(inset2)).toBeTruthy();
+        expect(inset2.equals(inset3)).toBeFalsy();
     });
 
-    t.test('#clone', (t) => {
+    test('#clone', () => {
         const inset1 = new EdgeInsets(10, 15, 50, 10);
         const inset2 = inset1.clone();
-        t.notOk(inset2 === inset1);
-        t.ok(inset1.equals(inset2));
-        t.end();
+        expect(inset2 === inset1).toBeFalsy();
+        expect(inset1.equals(inset2)).toBeTruthy();
     });
 
-    t.end();
 });
