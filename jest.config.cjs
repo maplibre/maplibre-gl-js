3d2aed7b97adb42216f3febfc352325306bc1cef
James Hamilton <james@thechipfactory.co.uk>
2021-10-09 18:45:54 +0900

    add jsdom setting to jest so window exists in tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,12 +1,13 @@
 module.exports = {
-  "roots": [
-    "<rootDir>/src"
-  ],
-  "testMatch": [
-    "**/__tests__/**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)"
-  ],
-  "transform": {
-    "^.+\\.(ts|tsx)$": "ts-jest"
-  },
-}
+    roots: [
+        '<rootDir>/src'
+    ],
+    testEnvironment: 'jsdom',
+    testMatch: [
+        '**/__tests__/**/*.+(ts|tsx|js)',
+        '**/?(*.)+(spec|test).+(ts|tsx|js)'
+    ],
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest'
+    },
+};
