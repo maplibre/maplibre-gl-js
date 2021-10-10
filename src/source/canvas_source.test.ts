1e3f1fefda633417b317c4a9fbff7a5ae2f4b560
James Hamilton <james@thechipfactory.co.uk>
2021-10-10 18:47:28 +0900

    add done to async tests

--- src/source/canvas_source.test.ts
+++ src/source/canvas_source.test.ts
@@ -44,7 +44,7 @@ describe('CanvasSource', () => {
         map = new StubMap();
     });
 
-    test('constructor', () => {
+    test('constructor', done => {
         const source = createSource();
 
         expect(source.minzoom).toBe(0);
@@ -54,6 +54,7 @@ describe('CanvasSource', () => {
         source.on('data', (e) => {
             if (e.dataType === 'source' && e.sourceDataType === 'metadata') {
                 expect(typeof source.play).toBe('function');
+                done();
             }
         });
 
@@ -85,7 +86,7 @@ describe('CanvasSource', () => {
 
     });
 
-    test('can be initialized with HTML element', () => {
+    test('can be initialized with HTML element', done => {
         const el = window.document.createElement('canvas');
         const source = createSource({
             canvas: el
@@ -94,34 +95,39 @@ describe('CanvasSource', () => {
         source.on('data', (e) => {
             if (e.dataType === 'source' && e.sourceDataType === 'metadata') {
                 expect(source.canvas).toBe(el);
+                done();
             }
         });
 
         source.onAdd(map);
     });
 
-    test('rerenders if animated', () => {
+    test('rerenders if animated', done => {
         const source = createSource();
 
         map.on('rerender', () => {
             expect(true).toBeTruthy();
+            done();
         });
 
         source.onAdd(map);
     });
 
-    test('can be static', () => {
+    test('can be static', done => {
         const source = createSource({
             animate: false
         });
 
         map.on('rerender', () => {
+            // this just confirms it didn't happen, so no need to run done() here
+            // if called the test will fail
             expect(true).toBeFalsy();
         });
 
         source.on('data', (e) => {
             if (e.sourceDataType === 'metadata' && e.dataType === 'source') {
                 expect(true).toBeTruthy();
+                done();
             }
         });
 
