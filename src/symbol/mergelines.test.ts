aead177f488fb582594f985c2b88ccdd4d97fe50
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2021-10-14 21:16:09 +0200

    Convert describe to test

--- src/symbol/mergelines.test.ts
+++ src/symbol/mergelines.test.ts
@@ -13,16 +13,18 @@ function makeFeatures(lines) {
     return features;
 }
 
-describe('mergeLines merges lines with the same text', () => {
-    expect(
-        mergeLines(makeFeatures([['a', 0, 1, 2], ['b', 4, 5, 6], ['a', 8, 9], ['a', 2, 3, 4], ['a', 6, 7, 8], ['a', 5, 6]]))
-    ).toEqual(makeFeatures([['a', 0, 1, 2, 3, 4], ['b', 4, 5, 6], ['a', 5, 6, 7, 8, 9]]));
-});
+describe('mergeLines', () => {
+    test('mergeLines merges lines with the same text', () => {
+        expect(
+            mergeLines(makeFeatures([['a', 0, 1, 2], ['b', 4, 5, 6], ['a', 8, 9], ['a', 2, 3, 4], ['a', 6, 7, 8], ['a', 5, 6]]))
+        ).toEqual(makeFeatures([['a', 0, 1, 2, 3, 4], ['b', 4, 5, 6], ['a', 5, 6, 7, 8, 9]]));
+    });
 
-describe('mergeLines handles merge from both ends', () => {
-    expect(mergeLines(makeFeatures([['a', 0, 1, 2], ['a', 4, 5, 6], ['a', 2, 3, 4]]))).toEqual(makeFeatures([['a', 0, 1, 2, 3, 4, 5, 6]]));
-});
+    test('mergeLines handles merge from both ends', () => {
+        expect(mergeLines(makeFeatures([['a', 0, 1, 2], ['a', 4, 5, 6], ['a', 2, 3, 4]]))).toEqual(makeFeatures([['a', 0, 1, 2, 3, 4, 5, 6]]));
+    });
 
-describe('mergeLines handles circular lines', () => {
-    expect(mergeLines(makeFeatures([['a', 0, 1, 2], ['a', 2, 3, 4], ['a', 4, 0]]))).toEqual(makeFeatures([['a', 0, 1, 2, 3, 4, 0]]));
+    test('mergeLines handles circular lines', () => {
+        expect(mergeLines(makeFeatures([['a', 0, 1, 2], ['a', 2, 3, 4], ['a', 4, 0]]))).toEqual(makeFeatures([['a', 0, 1, 2, 3, 4, 0]]));
+    });
 });
