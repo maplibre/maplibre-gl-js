b115e1ec2de2fb80c78b47f0da6de85a81d6860b
astridx <info@astrid-guenther.de>
2021-10-14 19:12:05 +0200

    add tests for better coverage

--- src/data/dem_data.test.ts
+++ src/data/dem_data.test.ts
@@ -40,88 +40,172 @@ describe('DEMData', () => {
     });
 });
 
-describe('DEMData#backfillBorder', () => {
-    const dem0 = new DEMData('0', createMockImage(4, 4), 'terrarium');
-    const dem1 = new DEMData('1', createMockImage(4, 4), 'terrarium');
+describe('DEMData#backfillBorder with endocding', () => {
+    describe('mabox endocding', () => {
+        const dem0 = new DEMData('0', createMockImage(4, 4), 'mapbox');
+        const dem1 = new DEMData('1', createMockImage(4, 4), 'mapbox');
 
-    test('border region is initially populated with neighboring data', () => {
-        let nonempty = true;
-        for (let x = -1; x < 5; x++) {
-            for (let y = -1; y < 5; y++) {
-                if (dem0.get(x, y) === -65536) {
-                    nonempty = false;
-                    break;
+        test('border region is initially populated with neighboring data', () => {
+            let nonempty = true;
+            for (let x = -1; x < 5; x++) {
+                for (let y = -1; y < 5; y++) {
+                    if (dem0.get(x, y) === -65536) {
+                        nonempty = false;
+                        break;
+                    }
                 }
             }
-        }
-        expect(nonempty).toBeTruthy();
+            expect(nonempty).toBeTruthy();
 
-        let verticalBorderMatch = true;
-        for (const x of [-1, 4]) {
+            let verticalBorderMatch = true;
+            for (const x of [-1, 4]) {
+                for (let y = 0; y < 4; y++) {
+                    if (dem0.get(x, y) !== dem0.get(x < 0 ? x + 1 : x - 1, y)) {
+                        verticalBorderMatch = false;
+                        break;
+                    }
+                }
+            }
+            expect(verticalBorderMatch).toBeTruthy();
+
+            // horizontal borders empty
+            let horizontalBorderMatch = true;
+            for (const y of [-1, 4]) {
+                for (let x = 0; x < 4; x++) {
+                    if (dem0.get(x, y) !== dem0.get(x, y < 0 ? y + 1 : y - 1)) {
+                        horizontalBorderMatch = false;
+                        break;
+                    }
+                }
+            }
+            expect(horizontalBorderMatch).toBeTruthy();
+
+            expect(dem0.get(-1, 4) === dem0.get(0, 3)).toBeTruthy();
+            expect(dem0.get(4, 4) === dem0.get(3, 3)).toBeTruthy();
+            expect(dem0.get(-1, -1) === dem0.get(0, 0)).toBeTruthy();
+            expect(dem0.get(4, -1) === dem0.get(3, 0)).toBeTruthy();
+        });
+
+        test('backfillBorder correctly populates borders with neighboring data', () => {
+            dem0.backfillBorder(dem1, -1, 0);
             for (let y = 0; y < 4; y++) {
-                if (dem0.get(x, y) !== dem0.get(x < 0 ? x + 1 : x - 1, y)) {
-                    verticalBorderMatch = false;
-                    break;
-                }
-            }
-        }
-        expect(verticalBorderMatch).toBeTruthy();
-
-        // horizontal borders empty
-        let horizontalBorderMatch = true;
-        for (const y of [-1, 4]) {
-            for (let x = 0; x < 4; x++) {
-                if (dem0.get(x, y) !== dem0.get(x, y < 0 ? y + 1 : y - 1)) {
-                    horizontalBorderMatch = false;
-                    break;
-                }
-            }
-        }
-        expect(horizontalBorderMatch).toBeTruthy();
-
-        expect(dem0.get(-1, 4) === dem0.get(0, 3)).toBeTruthy();
-        expect(dem0.get(4, 4) === dem0.get(3, 3)).toBeTruthy();
-        expect(dem0.get(-1, -1) === dem0.get(0, 0)).toBeTruthy();
-        expect(dem0.get(4, -1) === dem0.get(3, 0)).toBeTruthy();
-    });
-
-    test('backfillBorder correctly populates borders with neighboring data', () => {
-        dem0.backfillBorder(dem1, -1, 0);
-        for (let y = 0; y < 4; y++) {
             // dx = -1, dy = 0, so the left edge of dem1 should equal the right edge of dem0
-            expect(dem0.get(-1, y) === dem1.get(3, y)).toBeTruthy();
+                expect(dem0.get(-1, y) === dem1.get(3, y)).toBeTruthy();
+            }
 
-        }
+            dem0.backfillBorder(dem1, 0, -1);
+            for (let x = 0; x < 4; x++) {
+                expect(dem0.get(x, -1) === dem1.get(x, 3)).toBeTruthy();
+            }
 
-        dem0.backfillBorder(dem1, 0, -1);
-        for (let x = 0; x < 4; x++) {
-            expect(dem0.get(x, -1) === dem1.get(x, 3)).toBeTruthy();
-        }
+            dem0.backfillBorder(dem1, 1, 0);
+            for (let y = 0; y < 4; y++) {
+                expect(dem0.get(4, y) === dem1.get(0, y)).toBeTruthy();
+            }
 
-        dem0.backfillBorder(dem1, 1, 0);
-        for (let y = 0; y < 4; y++) {
-            expect(dem0.get(4, y) === dem1.get(0, y)).toBeTruthy();
-        }
+            dem0.backfillBorder(dem1, 0, 1);
+            for (let x = 0; x < 4; x++) {
+                expect(dem0.get(x, 4) === dem1.get(x, 0)).toBeTruthy();
+            }
 
-        dem0.backfillBorder(dem1, 0, 1);
-        for (let x = 0; x < 4; x++) {
-            expect(dem0.get(x, 4) === dem1.get(x, 0)).toBeTruthy();
-        }
+            dem0.backfillBorder(dem1, -1, 1);
+            expect(dem0.get(-1, 4) === dem1.get(3, 0)).toBeTruthy();
 
-        dem0.backfillBorder(dem1, -1, 1);
-        expect(dem0.get(-1, 4) === dem1.get(3, 0)).toBeTruthy();
+            dem0.backfillBorder(dem1, 1, 1);
+            expect(dem0.get(4, 4) === dem1.get(0, 0)).toBeTruthy();
 
-        dem0.backfillBorder(dem1, 1, 1);
-        expect(dem0.get(4, 4) === dem1.get(0, 0)).toBeTruthy();
+            dem0.backfillBorder(dem1, -1, -1);
+            expect(dem0.get(-1, -1) === dem1.get(3, 3)).toBeTruthy();
 
-        dem0.backfillBorder(dem1, -1, -1);
-        expect(dem0.get(-1, -1) === dem1.get(3, 3)).toBeTruthy();
-
-        dem0.backfillBorder(dem1, 1, -1);
-        expect(dem0.get(4, -1) === dem1.get(0, 3)).toBeTruthy();
+            dem0.backfillBorder(dem1, 1, -1);
+            expect(dem0.get(4, -1) === dem1.get(0, 3)).toBeTruthy();
+        });
     });
 
-    test('DEMData is correctly serialized', () => {
+    describe('terrarium endocding', () => {
+        const dem0 = new DEMData('0', createMockImage(4, 4), 'terrarium');
+        const dem1 = new DEMData('1', createMockImage(4, 4), 'terrarium');
+
+        test('border region is initially populated with neighboring data', () => {
+            let nonempty = true;
+            for (let x = -1; x < 5; x++) {
+                for (let y = -1; y < 5; y++) {
+                    if (dem0.get(x, y) === -65536) {
+                        nonempty = false;
+                        break;
+                    }
+                }
+            }
+            expect(nonempty).toBeTruthy();
+
+            let verticalBorderMatch = true;
+            for (const x of [-1, 4]) {
+                for (let y = 0; y < 4; y++) {
+                    if (dem0.get(x, y) !== dem0.get(x < 0 ? x + 1 : x - 1, y)) {
+                        verticalBorderMatch = false;
+                        break;
+                    }
+                }
+            }
+            expect(verticalBorderMatch).toBeTruthy();
+
+            // horizontal borders empty
+            let horizontalBorderMatch = true;
+            for (const y of [-1, 4]) {
+                for (let x = 0; x < 4; x++) {
+                    if (dem0.get(x, y) !== dem0.get(x, y < 0 ? y + 1 : y - 1)) {
+                        horizontalBorderMatch = false;
+                        break;
+                    }
+                }
+            }
+            expect(horizontalBorderMatch).toBeTruthy();
+
+            expect(dem0.get(-1, 4) === dem0.get(0, 3)).toBeTruthy();
+            expect(dem0.get(4, 4) === dem0.get(3, 3)).toBeTruthy();
+            expect(dem0.get(-1, -1) === dem0.get(0, 0)).toBeTruthy();
+            expect(dem0.get(4, -1) === dem0.get(3, 0)).toBeTruthy();
+        });
+
+        test('backfillBorder correctly populates borders with neighboring data', () => {
+            dem0.backfillBorder(dem1, -1, 0);
+            for (let y = 0; y < 4; y++) {
+                // dx = -1, dy = 0, so the left edge of dem1 should equal the right edge of dem0
+                expect(dem0.get(-1, y) === dem1.get(3, y)).toBeTruthy();
+            }
+
+            dem0.backfillBorder(dem1, 0, -1);
+            for (let x = 0; x < 4; x++) {
+                expect(dem0.get(x, -1) === dem1.get(x, 3)).toBeTruthy();
+            }
+
+            dem0.backfillBorder(dem1, 1, 0);
+            for (let y = 0; y < 4; y++) {
+                expect(dem0.get(4, y) === dem1.get(0, y)).toBeTruthy();
+            }
+
+            dem0.backfillBorder(dem1, 0, 1);
+            for (let x = 0; x < 4; x++) {
+                expect(dem0.get(x, 4) === dem1.get(x, 0)).toBeTruthy();
+            }
+
+            dem0.backfillBorder(dem1, -1, 1);
+            expect(dem0.get(-1, 4) === dem1.get(3, 0)).toBeTruthy();
+
+            dem0.backfillBorder(dem1, 1, 1);
+            expect(dem0.get(4, 4) === dem1.get(0, 0)).toBeTruthy();
+
+            dem0.backfillBorder(dem1, -1, -1);
+            expect(dem0.get(-1, -1) === dem1.get(3, 3)).toBeTruthy();
+
+            dem0.backfillBorder(dem1, 1, -1);
+            expect(dem0.get(4, -1) === dem1.get(0, 3)).toBeTruthy();
+        });
+    });
+});
+
+describe('DEMData is correctly serialized and deserialized', () => {
+    test('serialized', () => {
         const imageData0 = createMockImage(4, 4);
         const dem0 = new DEMData('0', imageData0, 'mapbox');
         const serialized = serialize(dem0);
@@ -140,12 +224,34 @@ describe('DEMData#backfillBorder', () => {
         expect(new Uint32Array(transferrables[0])).toEqual(dem0.data);
     });
 
-    test('DEMData is correctly deserialized', () => {
+    test('deserialized', () => {
         const imageData0 = createMockImage(4, 4);
         const dem0 = new DEMData('0', imageData0, 'terrarium');
         const serialized = serialize(dem0);
 
         const deserialized = deserialize(serialized);
         expect(deserialized).toEqual(dem0);
+    });
+});
+
+describe('UnpackVector is correctly returned', () => {
+    test('terrarium and mapbox', () => {
+        const imageData1 = createMockImage(4, 4);
+        const dem1 = new DEMData('0', imageData1, 'terrarium');
+
+        const imageData2 = createMockImage(4, 4);
+        const dem2 = new DEMData('0', imageData2, 'mapbox');
+
+        expect(dem1.getUnpackVector()).toEqual([256.0, 1.0, 1.0 / 256.0, 32768.0]);
+        expect(dem2.getUnpackVector()).toEqual([6553.6, 25.6, 0.1, 10000.0]);
+    });
+});
+
+describe('DEMData#getImage', () => {
+    test('Image is correctly returned', () => {
+        const imageData = createMockImage(4, 4);
+        const dem = new DEMData('0', imageData, 'terrarium');
+
+        expect(dem.getPixels()).toEqual(imageData);
     });
 });
