2877c2c42d598f2215e086cec6fc365750aac87e
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2021-10-17 12:00:40 +0200

    Run custom codemods

--- src/util/request_manager.test.ts
+++ src/util/request_manager.test.ts
@@ -1,30 +1,24 @@
-import {test} from '../../util/test';
-import {RequestManager} from '../../../rollup/build/tsc/src/util/request_manager';
+import {RequestManager} from '../util/request_manager';
 
-test('RequestManager', (t) => {
-    t.test('.normalizeSpriteURL', (t) => {
-        t.test('concantenates path, ratio, and extension for non-mapbox:// scheme', (t) => {
+describe('RequestManager', () => {
+    test('.normalizeSpriteURL', () => {
+        test('concantenates path, ratio, and extension for non-mapbox:// scheme', () => {
             expect(
                 new RequestManager().normalizeSpriteURL('http://www.foo.com/bar', '@2x', '.png')
             ).toBe('http://www.foo.com/bar@2x.png');
-            t.end();
         });
 
-        t.test('concantenates path, ratio, and extension for file:/// scheme', (t) => {
+        test('concantenates path, ratio, and extension for file:/// scheme', () => {
             expect(
                 new RequestManager().normalizeSpriteURL('file:///path/to/bar', '@2x', '.png')
             ).toBe('file:///path/to/bar@2x.png');
-            t.end();
         });
 
-        t.test('normalizes non-mapbox:// scheme when query string exists', (t) => {
+        test('normalizes non-mapbox:// scheme when query string exists', () => {
             expect(
                 new RequestManager().normalizeSpriteURL('http://www.foo.com/bar?fresh=true', '@2x', '.png')
             ).toBe('http://www.foo.com/bar@2x.png?fresh=true');
-            t.end();
         });
-        t.end();
     });
 
-    t.end();
 });
