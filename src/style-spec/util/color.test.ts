f28875b9bc5b99329dfc8075d6b19a980eb3b379
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2021-10-17 15:13:06 +0200

    Manual edits

--- src/style-spec/util/color.test.ts
+++ src/style-spec/util/color.test.ts
@@ -1,18 +1,16 @@
-// @flow
+import Color from './color';
 
-import Color from '../style-spec/util/color';
-
-describe('Color.parse', () => {
+test('Color.parse', () => {
     expect(Color.parse('red')).toEqual(new Color(1, 0, 0, 1));
     expect(Color.parse('#ff00ff')).toEqual(new Color(1, 0, 1, 1));
     expect(Color.parse('invalid')).toEqual(undefined);
     expect(Color.parse(null)).toEqual(undefined);
     expect(Color.parse(undefined)).toEqual(undefined);
-    });
+});
 
-describe('Color#toString', () => {
+test('Color#toString', () => {
     const purple = Color.parse('purple');
     expect(purple && purple.toString()).toBe('rgba(128,0,128,1)');
     const translucentGreen = Color.parse('rgba(26, 207, 26, .73)');
     expect(translucentGreen && translucentGreen.toString()).toBe('rgba(26,207,26,0.73)');
-    });
+});
