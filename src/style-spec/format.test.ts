3e601cc0458ec3f1c72ddffe156271d875cba188
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2021-10-17 19:34:48 +0200

    Run jest-codemods

--- src/style-spec/format.test.ts
+++ src/style-spec/format.test.ts
@@ -6,25 +6,25 @@ function roundtrip(style) {
 }
 
 test('orders top-level keys', (t) => {
-    t.deepEqual(Object.keys(roundtrip({
+    expect(Object.keys(roundtrip({
         "layers": [],
         "other": {},
         "sources": {},
         "glyphs": "",
         "sprite": "",
         "version": 6
-    })), ['version', 'sources', 'sprite', 'glyphs', 'layers', 'other']);
+    }))).toEqual(['version', 'sources', 'sprite', 'glyphs', 'layers', 'other']);
     t.end();
 });
 
 test('orders layer keys', (t) => {
-    t.deepEqual(Object.keys(roundtrip({
+    expect(Object.keys(roundtrip({
         "layers": [{
             "paint": {},
             "layout": {},
             "id": "id",
             "type": "type"
         }]
-    }).layers[0]), ['id', 'type', 'layout', 'paint']);
+    }).layers[0])).toEqual(['id', 'type', 'layout', 'paint']);
     t.end();
 });
