16b831c664f968c1c860ff0afbf4a10dc7d3133b
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2021-10-17 12:31:47 +0200

    Wrap in describe

--- src/render/uniform_binding.test.ts
+++ src/render/uniform_binding.test.ts
@@ -7,111 +7,114 @@ import {
     UniformMatrix4f
 } from '../render/uniform_binding';
 
-describe('Uniform1i', () => {
-    // test counts ensure we don't call the gl.uniform* setters more than expected
-    expect.assertions(4);
+describe('Uniform Binding', () => {
+    test('Uniform1i', () => {
+        // test counts ensure we don't call the gl.uniform* setters more than expected
+        expect.assertions(4);
 
-    const context = {
-        gl: {
-            uniform1i: () => { expect(true).toBeTruthy(); }
-        }
-    };
+        const context = {
+            gl: {
+                uniform1i: () => { expect(true).toBeTruthy(); }
+            }
+        };
 
-    const u = new Uniform1i(context, 0);
+        const u = new Uniform1i(context, 0);
 
-    expect(u.current).toBe(0);
-    u.set(1);
-    expect(u.current).toBe(1);
-    u.set(1);
-    u.set(2);
-});
+        expect(u.current).toBe(0);
+        u.set(1);
+        expect(u.current).toBe(1);
+        u.set(1);
+        u.set(2);
+    });
 
-describe('Uniform1f', () => {
-    expect.assertions(4);
+    test('Uniform1f', () => {
+        expect.assertions(4);
 
-    const context = {
-        gl: {
-            uniform1f: () => { expect(true).toBeTruthy(); }
-        }
-    };
+        const context = {
+            gl: {
+                uniform1f: () => { expect(true).toBeTruthy(); }
+            }
+        };
 
-    const u = new Uniform1f(context, 0);
+        const u = new Uniform1f(context, 0);
 
-    expect(u.current).toBe(0);
-    u.set(1);
-    expect(u.current).toBe(1);
-    u.set(1);
-    u.set(2);
-});
+        expect(u.current).toBe(0);
+        u.set(1);
+        expect(u.current).toBe(1);
+        u.set(1);
+        u.set(2);
+    });
 
-describe('Uniform2f', () => {
-    expect.assertions(4);
+    test('Uniform2f', () => {
+        expect.assertions(4);
 
-    const context = {
-        gl: {
-            uniform2f: () => { expect(true).toBeTruthy(); }
-        }
-    };
+        const context = {
+            gl: {
+                uniform2f: () => { expect(true).toBeTruthy(); }
+            }
+        };
 
-    const u = new Uniform2f(context, 0);
+        const u = new Uniform2f(context, 0);
 
-    expect(u.current).toEqual([0, 0]);
-    u.set([1, 1]);
-    expect(u.current).toEqual([1, 1]);
-    u.set([1, 1]);
-    u.set([1, 2]);
-});
+        expect(u.current).toEqual([0, 0]);
+        u.set([1, 1]);
+        expect(u.current).toEqual([1, 1]);
+        u.set([1, 1]);
+        u.set([1, 2]);
+    });
 
-describe('Uniform3f', () => {
-    expect.assertions(4);
+    test('Uniform3f', () => {
+        expect.assertions(4);
 
-    const context = {
-        gl: {
-            uniform3f: () => { expect(true).toBeTruthy(); }
-        }
-    };
+        const context = {
+            gl: {
+                uniform3f: () => { expect(true).toBeTruthy(); }
+            }
+        };
 
-    const u = new Uniform3f(context, 0);
+        const u = new Uniform3f(context, 0);
 
-    expect(u.current).toEqual([0, 0, 0]);
-    u.set([1, 1, 1]);
-    expect(u.current).toEqual([1, 1, 1]);
-    u.set([1, 1, 1]);
-    u.set([1, 1, 2]);
-});
+        expect(u.current).toEqual([0, 0, 0]);
+        u.set([1, 1, 1]);
+        expect(u.current).toEqual([1, 1, 1]);
+        u.set([1, 1, 1]);
+        u.set([1, 1, 2]);
+    });
 
-describe('Uniform4f', () => {
-    expect.assertions(4);
+    test('Uniform4f', () => {
+        expect.assertions(4);
 
-    const context = {
-        gl: {
-            uniform4f: () => { expect(true).toBeTruthy(); }
-        }
-    };
+        const context = {
+            gl: {
+                uniform4f: () => { expect(true).toBeTruthy(); }
+            }
+        };
 
-    const u = new Uniform4f(context, 0);
+        const u = new Uniform4f(context, 0);
 
-    expect(u.current).toEqual([0, 0, 0, 0]);
-    u.set([1, 1, 1, 1]);
-    expect(u.current).toEqual([1, 1, 1, 1]);
-    u.set([1, 1, 1, 1]);
-    u.set([2, 1, 1, 1]);
-});
+        expect(u.current).toEqual([0, 0, 0, 0]);
+        u.set([1, 1, 1, 1]);
+        expect(u.current).toEqual([1, 1, 1, 1]);
+        u.set([1, 1, 1, 1]);
+        u.set([2, 1, 1, 1]);
+    });
 
-describe('UniformMatrix4f', () => {
-    expect.assertions(4);
+    test('UniformMatrix4f', () => {
+        expect.assertions(4);
 
-    const context = {
-        gl: {
-            uniformMatrix4fv: () => { expect(true).toBeTruthy(); }
-        }
-    };
+        const context = {
+            gl: {
+                uniformMatrix4fv: () => { expect(true).toBeTruthy(); }
+            }
+        };
 
-    const u = new UniformMatrix4f(context, 0);
-    const ident = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
-    expect(u.current).toEqual([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
-    u.set(ident);
-    expect(u.current).toEqual(ident);
-    u.set(ident);
-    u.set([2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]);
+        const u = new UniformMatrix4f(context, 0);
+        const ident = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
+        expect(u.current).toEqual([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+        u.set(ident);
+        expect(u.current).toEqual(ident);
+        u.set(ident);
+        u.set([2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]);
+    });
+
 });
