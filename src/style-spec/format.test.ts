1492e3c1f32d115b08ba7785880a0cd2b3d13f20
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2021-10-17 19:37:47 +0200

    Wrap in describe

--- src/style-spec/format.test.ts
+++ src/style-spec/format.test.ts
@@ -1,28 +1,30 @@
-import { Test } from 'tap';
 import format from './format';
 
 function roundtrip(style) {
     return JSON.parse(format(style));
 }
 
-test('orders top-level keys', () => {
-    expect(Object.keys(roundtrip({
-        'layers': [],
-        'other': {},
-        'sources': {},
-        'glyphs': '',
-        'sprite': '',
-        'version': 6
-    }))).toEqual(['version', 'sources', 'sprite', 'glyphs', 'layers', 'other']);
-});
+describe('format', () => {
 
-test('orders layer keys', () => {
-    expect(Object.keys(roundtrip({
-        'layers': [{
-            'paint': {},
-            'layout': {},
-            'id': 'id',
-            'type': 'type'
-        }]
-    }).layers[0])).toEqual(['id', 'type', 'layout', 'paint']);
+    test('orders top-level keys', () => {
+        expect(Object.keys(roundtrip({
+            'layers': [],
+            'other': {},
+            'sources': {},
+            'glyphs': '',
+            'sprite': '',
+            'version': 6
+        }))).toEqual(['version', 'sources', 'sprite', 'glyphs', 'layers', 'other']);
+    });
+
+    test('orders layer keys', () => {
+        expect(Object.keys(roundtrip({
+            'layers': [{
+                'paint': {},
+                'layout': {},
+                'id': 'id',
+                'type': 'type'
+            }]
+        }).layers[0])).toEqual(['id', 'type', 'layout', 'paint']);
+    });
 });
