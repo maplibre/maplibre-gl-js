87ecfbaaaa3c3befc464842acab7eeb8fc5cd71c
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2021-10-17 14:50:33 +0200

    Run jest-codemods

--- src/shaders/encode_attribute.test.ts
+++ src/shaders/encode_attribute.test.ts
@@ -2,12 +2,12 @@ import {test} from '../../util/test';
 import {packUint8ToFloat} from '../../../rollup/build/tsc/src/shaders/encode_attribute';
 
 test('packUint8ToFloat', (t) => {
-    t.equal(packUint8ToFloat(0, 0), 0);
-    t.equal(packUint8ToFloat(255, 255), 65535);
-    t.equal(packUint8ToFloat(123, 45), 31533);
+    expect(packUint8ToFloat(0, 0)).toBe(0);
+    expect(packUint8ToFloat(255, 255)).toBe(65535);
+    expect(packUint8ToFloat(123, 45)).toBe(31533);
 
-    t.equal(packUint8ToFloat(-1, -1), 0);
-    t.equal(packUint8ToFloat(256, 256), 65535);
+    expect(packUint8ToFloat(-1, -1)).toBe(0);
+    expect(packUint8ToFloat(256, 256)).toBe(65535);
 
     t.end();
 });
