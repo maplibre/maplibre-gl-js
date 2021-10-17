13f415675eb49b5e2aaf9de7fef0142edaef7e6a
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2021-10-17 18:51:54 +0200

    Run custom codemods

--- src/style-spec/empty.test.ts
+++ src/style-spec/empty.test.ts
@@ -1,15 +1,12 @@
-import {test} from '../../util/test';
-import emptyStyle from '../../../rollup/build/tsc/src/style-spec/empty';
-import validateStyleMin from '../../../rollup/build/tsc/src/style-spec/validate_style.min';
+import emptyStyle from '../style-spec/empty';
+import validateStyleMin from '../style-spec/validate_style.min';
 
-test('it generates something', (t) => {
+describe('it generates something', () => {
     const style = emptyStyle();
     expect(style).toBeTruthy();
-    t.end();
 });
 
-test('generated empty style is a valid style', (t) => {
+describe('generated empty style is a valid style', () => {
     const errors = validateStyleMin(emptyStyle());
     expect(errors.length).toBe(0);
-    t.end();
 });
