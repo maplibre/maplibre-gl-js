88bb45e0378e50dd29b29bd3c622d4909a294f3f
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2021-10-17 20:15:47 +0200

    Manual edits

--- src/style-spec/util/interpolate.test.ts
+++ src/style-spec/util/interpolate.test.ts
@@ -1,14 +1,14 @@
-import * as interpolate from '../style-spec/util/interpolate';
-import Color from '../style-spec/util/color';
+import * as interpolate from './interpolate';
+import Color from './color';
 
-describe('interpolate.number', () => {
+test('interpolate.number', () => {
     expect(interpolate.number(0, 1, 0.5)).toBe(0.5);
 });
 
-describe('interpolate.color', () => {
+test('interpolate.color', () => {
     expect(interpolate.color(new Color(0, 0, 0, 0), new Color(1, 2, 3, 4), 0.5)).toEqual(new Color(0.5, 1, 3 / 2, 2));
 });
 
-describe('interpolate.array', () => {
+test('interpolate.array', () => {
     expect(interpolate.array([0, 0, 0, 0], [1, 2, 3, 4], 0.5)).toEqual([0.5, 1, 3 / 2, 2]);
 });
