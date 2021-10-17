97fa0d58ff3729d2cfe9a647b443570aea86a5b9
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2021-10-17 19:35:42 +0200

    Run custom codemods

--- src/style-spec/format.test.ts
+++ src/style-spec/format.test.ts
@@ -1,30 +1,27 @@
-import {test} from '../../util/test';
-import format from '../../../rollup/build/tsc/src/style-spec/format';
+import format from '../style-spec/format';
 
 function roundtrip(style) {
     return JSON.parse(format(style));
 }
 
-test('orders top-level keys', (t) => {
+describe('orders top-level keys', () => {
     expect(Object.keys(roundtrip({
-        "layers": [],
-        "other": {},
-        "sources": {},
-        "glyphs": "",
-        "sprite": "",
-        "version": 6
+        'layers': [],
+        'other': {},
+        'sources': {},
+        'glyphs': '',
+        'sprite': '',
+        'version': 6
     }))).toEqual(['version', 'sources', 'sprite', 'glyphs', 'layers', 'other']);
-    t.end();
 });
 
-test('orders layer keys', (t) => {
+describe('orders layer keys', () => {
     expect(Object.keys(roundtrip({
-        "layers": [{
-            "paint": {},
-            "layout": {},
-            "id": "id",
-            "type": "type"
+        'layers': [{
+            'paint': {},
+            'layout': {},
+            'id': 'id',
+            'type': 'type'
         }]
     }).layers[0])).toEqual(['id', 'type', 'layout', 'paint']);
-    t.end();
 });
