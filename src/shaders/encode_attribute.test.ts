1db1b6076fa08d66b9bd0dbe017ef37b1f3e41cf
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2021-10-17 14:51:27 +0200

    Run custom codemods

--- src/shaders/encode_attribute.test.ts
+++ src/shaders/encode_attribute.test.ts
@@ -1,7 +1,6 @@
-import {test} from '../../util/test';
-import {packUint8ToFloat} from '../../../rollup/build/tsc/src/shaders/encode_attribute';
+import {packUint8ToFloat} from '../shaders/encode_attribute';
 
-test('packUint8ToFloat', (t) => {
+describe('packUint8ToFloat', () => {
     expect(packUint8ToFloat(0, 0)).toBe(0);
     expect(packUint8ToFloat(255, 255)).toBe(65535);
     expect(packUint8ToFloat(123, 45)).toBe(31533);
@@ -9,5 +8,4 @@ test('packUint8ToFloat', (t) => {
     expect(packUint8ToFloat(-1, -1)).toBe(0);
     expect(packUint8ToFloat(256, 256)).toBe(65535);
 
-    t.end();
 });
