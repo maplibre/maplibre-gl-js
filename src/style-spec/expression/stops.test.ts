9802ce517a94706c3d44723d36fc88240027ea7d
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2021-10-17 20:40:26 +0200

    Run jest-codemods

--- src/style-spec/expression/stops.test.ts
+++ src/style-spec/expression/stops.test.ts
@@ -4,7 +4,7 @@ import {findStopLessThanOrEqualTo} from '../../../rollup/build/tsc/src/style-spe
 test('findStopLessThanOrEqualTo', (t) => {
     test('When the input > all stops it returns the last stop.', (t) => {
         const index = findStopLessThanOrEqualTo([0, 1, 2, 3, 4, 5, 6, 7], 8);
-        t.equal(index, 7);
+        expect(index).toBe(7);
         t.end();
     });
 
@@ -12,13 +12,13 @@ test('findStopLessThanOrEqualTo', (t) => {
         let index;
 
         index = findStopLessThanOrEqualTo([0.5, 0.5], 0.5);
-        t.equal(index, 1);
+        expect(index).toBe(1);
 
         index = findStopLessThanOrEqualTo([0.5, 0.5, 0.5], 0.5);
-        t.equal(index, 2);
+        expect(index).toBe(2);
 
         index = findStopLessThanOrEqualTo([0.4, 0.5, 0.5, 0.6, 0.7], 0.5);
-        t.equal(index, 2);
+        expect(index).toBe(2);
 
         t.end();
     });
