e8de4899c728c1e475c6bba98b1bd017de5696f0
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2021-10-17 20:13:52 +0200

    Run custom codemods

--- src/style-spec/interpolate.test.ts
+++ src/style-spec/interpolate.test.ts
@@ -1,18 +1,14 @@
-import {test} from '../../util/test';
-import * as interpolate from '../../../rollup/build/tsc/src/style-spec/util/interpolate';
-import Color from '../../../rollup/build/tsc/src/style-spec/util/color';
+import * as interpolate from '../style-spec/util/interpolate';
+import Color from '../style-spec/util/color';
 
-test('interpolate.number', (t) => {
+describe('interpolate.number', () => {
     expect(interpolate.number(0, 1, 0.5)).toBe(0.5);
-    t.end();
 });
 
-test('interpolate.color', (t) => {
+describe('interpolate.color', () => {
     expect(interpolate.color(new Color(0, 0, 0, 0), new Color(1, 2, 3, 4), 0.5)).toEqual(new Color(0.5, 1, 3 / 2, 2));
-    t.end();
 });
 
-test('interpolate.array', (t) => {
+describe('interpolate.array', () => {
     expect(interpolate.array([0, 0, 0, 0], [1, 2, 3, 4], 0.5)).toEqual([0.5, 1, 3 / 2, 2]);
-    t.end();
 });
