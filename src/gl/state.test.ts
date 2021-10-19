84e6a1ee94d9dfc6ae138dbc5c698ccb2db14f16
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2021-10-19 20:30:03 +0200

    Use arrow function

--- src/gl/state.test.ts
+++ src/gl/state.test.ts
@@ -6,7 +6,7 @@ import gl from 'gl';
 
 const context = new Context(gl(10, 10));
 
-function ValueTest(Constructor, options) {
+const valueTest = (Constructor, options) => {
     test('#constructor', () => {
         const v = new Constructor(context);
         expect(v).toBeTruthy();
@@ -21,119 +21,171 @@ function ValueTest(Constructor, options) {
         expect(equality(v.get(), options.setValue)).toBeTruthy();
     });
 
-}
+};
 
-describe('ClearColor', ValueTest.bind(ValueTest, ClearColor, {
-    setValue: new Color(1, 1, 0, 1)
-}));
+describe('ClearColor', () => {
+    valueTest(ClearColor, {
+        setValue: new Color(1, 1, 0, 1)
+    });
+});
 
-describe('ClearDepth', ValueTest.bind(ValueTest, ClearDepth, {
-    setValue: 0.5
-}));
+describe('ClearDepth', () => {
+    valueTest(ClearDepth, {
+        setValue: 0.5
+    });
+});
 
-describe('ClearStencil', ValueTest.bind(ValueTest, ClearStencil, {
-    setValue: 0.5
-}));
+describe('ClearStencil', () => {
+    valueTest(ClearStencil, {
+        setValue: 0.5
+    });
+});
 
-describe('ColorMask', ValueTest.bind(ValueTest, ColorMask, {
-    setValue: [false, false, true, true]
-}));
+describe('ColorMask', () => {
+    valueTest(ColorMask, {
+        setValue: [false, false, true, true]
+    });
+});
 
-describe('DepthMask', ValueTest.bind(ValueTest, DepthMask, {
-    setValue: false
-}));
+describe('DepthMask', () => {
+    valueTest(DepthMask, {
+        setValue: false
+    });
+});
 
-describe('StencilMask', ValueTest.bind(ValueTest, StencilMask, {
-    setValue: [0x00, 4]
-}));
+describe('StencilMask', () => {
+    valueTest(StencilMask, {
+        setValue: [0x00, 4]
+    });
+});
 
-describe('StencilFunc', ValueTest.bind(ValueTest, StencilFunc, {
-    setValue: {
-        func: context.gl.LEQUAL,
-        ref: 1,
-        mask: 0xFF
-    }
-}));
+describe('StencilFunc', () => {
+    valueTest(StencilFunc, {
+        setValue: {
+            func: context.gl.LEQUAL,
+            ref: 1,
+            mask: 0xFF
+        }
+    });
+});
 
-describe('StencilOp', ValueTest.bind(ValueTest, StencilOp, {
-    setValue: [context.gl.KEEP, context.gl.REPLACE, context.gl.REPLACE]
-}));
+describe('StencilOp', () => {
+    valueTest(StencilOp, {
+        setValue: [context.gl.KEEP, context.gl.REPLACE, context.gl.REPLACE]
+    });
+});
 
-describe('StencilTest', ValueTest.bind(ValueTest, StencilTest, {
-    setValue: true
-}));
+describe('StencilTest', () => {
+    valueTest(StencilTest, {
+        setValue: true
+    });
+});
 
-describe('DepthRange', ValueTest.bind(ValueTest, DepthRange, {
-    setValue: [0, 0.1]
-}));
+describe('DepthRange', () => {
+    valueTest(DepthRange, {
+        setValue: [0, 0.1]
+    });
+});
 
-describe('DepthTest', ValueTest.bind(ValueTest, DepthTest, {
-    setValue: true
-}));
+describe('DepthTest', () => {
+    valueTest(DepthTest, {
+        setValue: true
+    });
+});
 
-describe('DepthFunc', ValueTest.bind(ValueTest, DepthFunc, {
-    setValue: context.gl.EQUAL
-}));
+describe('DepthFunc', () => {
+    valueTest(DepthFunc, {
+        setValue: context.gl.EQUAL
+    });
+});
 
-describe('Blend', ValueTest.bind(ValueTest, Blend, {
-    setValue: false
-}));
+describe('Blend', () => {
+    valueTest(Blend, {
+        setValue: false
+    });
+});
 
-describe('BlendFunc', ValueTest.bind(ValueTest, BlendFunc, {
-    setValue: [context.gl.SRC_ALPHA, context.gl.SRC_ALPHA]
-}));
+describe('BlendFunc', () => {
+    valueTest(BlendFunc, {
+        setValue: [context.gl.SRC_ALPHA, context.gl.SRC_ALPHA]
+    });
+});
 
-describe('BlendColor', ValueTest.bind(ValueTest, BlendColor, {
-    setValue: Color.white
-}));
+describe('BlendColor', () => {
+    valueTest(BlendColor, {
+        setValue: Color.white
+    });
+});
 
-describe('Program', ValueTest.bind(ValueTest, Program, {
-    equality: (a, b) => a === b,
-    setValue: context.gl.createProgram()
-}));
+describe('Program', () => {
+    valueTest(Program, {
+        equality: (a, b) => a === b,
+        setValue: context.gl.createProgram()
+    });
+});
 
-describe('ActiveTextureUnit', ValueTest.bind(ValueTest, ActiveTextureUnit, {
-    setValue: context.gl.TEXTURE1
-}));
+describe('ActiveTextureUnit', () => {
+    valueTest(ActiveTextureUnit, {
+        setValue: context.gl.TEXTURE1
+    });
+});
 
-describe('Viewport', ValueTest.bind(ValueTest, Viewport, {
-    setValue: [0, 0, 1, 1]
-}));
+describe('Viewport', () => {
+    valueTest(Viewport, {
+        setValue: [0, 0, 1, 1]
+    });
+});
 
-describe('BindFramebuffer', ValueTest.bind(ValueTest, BindFramebuffer, {
-    equality: (a, b) => a === b,
-    setValue: context.gl.createFramebuffer()
-}));
+describe('BindFramebuffer', () => {
+    valueTest(BindFramebuffer, {
+        equality: (a, b) => a === b,
+        setValue: context.gl.createFramebuffer()
+    });
+});
 
-describe('BindRenderbuffer', ValueTest.bind(ValueTest, BindRenderbuffer, {
-    equality: (a, b) => a === b,
-    setValue: context.gl.createRenderbuffer()
-}));
+describe('BindRenderbuffer', () => {
+    valueTest(BindRenderbuffer, {
+        equality: (a, b) => a === b,
+        setValue: context.gl.createRenderbuffer()
+    });
+});
 
-describe('BindTexture', ValueTest.bind(ValueTest, BindTexture, {
-    equality: (a, b) => a === b,
-    setValue: context.gl.createTexture()
-}));
+describe('BindTexture', () => {
+    valueTest(BindTexture, {
+        equality: (a, b) => a === b,
+        setValue: context.gl.createTexture()
+    });
+});
 
-describe('BindVertexBuffer', ValueTest.bind(ValueTest, BindVertexBuffer, {
-    equality: (a, b) => a === b,
-    setValue: context.gl.createBuffer()
-}));
+describe('BindVertexBuffer', () => {
+    valueTest(BindVertexBuffer, {
+        equality: (a, b) => a === b,
+        setValue: context.gl.createBuffer()
+    });
+});
 
-describe('BindElementBuffer', ValueTest.bind(ValueTest, BindElementBuffer, {
-    equality: (a, b) => a === b,
-    setValue: context.gl.createBuffer()
-}));
+describe('BindElementBuffer', () => {
+    valueTest(BindElementBuffer, {
+        equality: (a, b) => a === b,
+        setValue: context.gl.createBuffer()
+    });
+});
 
-describe('BindVertexArrayOES', ValueTest.bind(ValueTest, BindVertexArrayOES, {
-    equality: (a, b) => a === b,
-    setValue: context.extVertexArrayObject
-}));
+describe('BindVertexArrayOES', () => {
+    valueTest(BindVertexArrayOES, {
+        equality: (a, b) => a === b,
+        setValue: context.extVertexArrayObject
+    });
+});
 
-describe('PixelStoreUnpack', ValueTest.bind(ValueTest, PixelStoreUnpack, {
-    setValue: 8
-}));
+describe('PixelStoreUnpack', () => {
+    valueTest(PixelStoreUnpack, {
+        setValue: 8
+    });
+});
 
-describe('PixelStoreUnpackPremultiplyAlpha', ValueTest.bind(ValueTest, PixelStoreUnpackPremultiplyAlpha, {
-    setValue: true
-}));
+describe('PixelStoreUnpackPremultiplyAlpha', () => {
+    valueTest(PixelStoreUnpackPremultiplyAlpha, {
+        setValue: true
+    });
+});
