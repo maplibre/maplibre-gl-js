9687bf658f7fdd7c848fbc4629f85eef3325c192
HarelM <harel.mazor@gmail.com>
2021-10-20 21:35:31 +0300

    Run custom codemods

--- src/source/worker.test.ts
+++ src/source/worker.test.ts
@@ -1,13 +1,12 @@
 import '../../stub_loader';
-import {test} from '../../util/test';
-import Worker from '../../../rollup/build/tsc/src/source/worker';
+import Worker from '../source/worker';
 
 const _self = {
     addEventListener() {}
 };
 
-test('load tile', (t) => {
-    t.test('calls callback on error', (t) => {
+describe('load tile', done => {
+    test('calls callback on error', done => {
         window.useFakeXMLHttpRequest();
         const worker = new Worker(_self);
         worker.loadTile(0, {
@@ -19,15 +18,15 @@ test('load tile', (t) => {
         }, (err) => {
             expect(err).toBeTruthy();
             window.clearFakeXMLHttpRequest();
-            t.end();
+            done();
         });
         window.server.respond();
     });
 
-    t.end();
+    done();
 });
 
-test('isolates different instances\' data', (t) => {
+describe('isolates different instances\' data', done => {
     const worker = new Worker(_self);
 
     worker.setLayers(0, [
@@ -40,16 +39,16 @@ test('isolates different instances\' data', (t) => {
     ], () => {});
 
     expect(worker.layerIndexes[0]).not.toBe(worker.layerIndexes[1]);
-    t.end();
+    done();
 });
 
-test('worker source messages dispatched to the correct map instance', (t) => {
+describe('worker source messages dispatched to the correct map instance', done => {
     const worker = new Worker(_self);
 
     worker.actor.send = function (type, data, callback, mapId) {
         expect(type).toBe('main thread task');
         expect(mapId).toBe(999);
-        t.end();
+        done();
     };
 
     _self.registerWorkerSource('test', function(actor) {
