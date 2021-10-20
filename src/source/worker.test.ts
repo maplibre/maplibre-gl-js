c3957a0695d1853947b8d6b703514f0d1b02e4c2
HarelM <harel.mazor@gmail.com>
2021-10-20 22:48:37 +0300

    Manual fixes

--- src/source/worker.test.ts
+++ src/source/worker.test.ts
@@ -1,63 +1,78 @@
-import '../../stub_loader';
+import {fakeServer} from 'sinon';
 import Worker from '../source/worker';
+import {LayerSpecification} from '../style-spec/types';
+import {Cancelable} from '../types/cancelable';
+import {WorkerGlobalScopeInterface} from '../util/web_worker';
+import {CanonicalTileID, OverscaledTileID} from './tile_id';
+import {TileParameters, WorkerSource, WorkerTileCallback, WorkerTileParameters} from './worker_source';
 
 const _self = {
     addEventListener() {}
-};
+} as any as WorkerGlobalScopeInterface;
 
-describe('load tile', done => {
+class WorkerSourceMock implements WorkerSource {
+    availableImages: string[];
+    constructor(private actor: any) {}
+    loadTile(_: WorkerTileParameters, __: WorkerTileCallback): void {
+        this.actor.send('main thread task', {}, () => {}, null);
+    }
+    reloadTile(_: WorkerTileParameters, __: WorkerTileCallback): void {
+        throw new Error('Method not implemented.');
+    }
+    abortTile(_: TileParameters, __: WorkerTileCallback): void {
+        throw new Error('Method not implemented.');
+    }
+    removeTile(_: TileParameters, __: WorkerTileCallback): void {
+        throw new Error('Method not implemented.');
+    }
+}
+
+describe('load tile', () => {
     test('calls callback on error', done => {
-        window.useFakeXMLHttpRequest();
+        const server = fakeServer.create();
+        global.fetch = null;
         const worker = new Worker(_self);
-        worker.loadTile(0, {
+        worker.loadTile('0', {
             type: 'vector',
             source: 'source',
-            uid: 0,
-            tileID: {overscaledZ: 0, wrap: 0, canonical: {x: 0, y: 0, z: 0, w: 0}},
+            uid: '0',
+            tileID: {overscaledZ: 0, wrap: 0, canonical: {x: 0, y: 0, z: 0} as CanonicalTileID} as any as OverscaledTileID,
             request: {url: '/error'}// Sinon fake server gives 404 responses by default
-        }, (err) => {
+        } as WorkerTileParameters & { type: string }, (err) => {
             expect(err).toBeTruthy();
-            window.clearFakeXMLHttpRequest();
+            server.restore();
             done();
         });
-        window.server.respond();
+        server.respond();
     });
 
-    done();
-});
+    test('isolates different instances\' data', () => {
+        const worker = new Worker(_self);
 
-describe('isolates different instances\' data', done => {
-    const worker = new Worker(_self);
+        worker.setLayers('0', [
+            {id: 'one', type: 'circle'} as LayerSpecification
+        ], () => {});
 
-    worker.setLayers(0, [
-        {id: 'one', type: 'circle'}
-    ], () => {});
+        worker.setLayers('1', [
+            {id: 'one', type: 'circle'} as LayerSpecification,
+            {id: 'two', type: 'circle'} as LayerSpecification,
+        ], () => {});
 
-    worker.setLayers(1, [
-        {id: 'one', type: 'circle'},
-        {id: 'two', type: 'circle'},
-    ], () => {});
+        expect(worker.layerIndexes[0]).not.toBe(worker.layerIndexes[1]);
+    });
 
-    expect(worker.layerIndexes[0]).not.toBe(worker.layerIndexes[1]);
-    done();
-});
+    test('worker source messages dispatched to the correct map instance', done => {
+        const worker = new Worker(_self);
 
-describe('worker source messages dispatched to the correct map instance', done => {
-    const worker = new Worker(_self);
-
-    worker.actor.send = function (type, data, callback, mapId) {
-        expect(type).toBe('main thread task');
-        expect(mapId).toBe(999);
-        done();
-    };
-
-    _self.registerWorkerSource('test', function(actor) {
-        this.loadTile = function() {
-            // we expect the map id to get appended in the call to the "real"
-            // actor.send()
-            actor.send('main thread task', {}, () => {}, null);
+        worker.actor.send = (type, data, callback, mapId): Cancelable => {
+            expect(type).toBe('main thread task');
+            expect(mapId).toBe('999');
+            done();
+            return {cancel: () => {}};
         };
-    });
 
-    worker.loadTile(999, {type: 'test'});
+        _self.registerWorkerSource('test', WorkerSourceMock);
+
+        worker.loadTile('999', {type: 'test'} as WorkerTileParameters & { type: string }, () => {});
+    });
 });
