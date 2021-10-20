818fadc5d791be214cc0d70a78e4ecfb2a6ba4f2
astridx <info@astrid-guenther.de>
2021-10-20 11:54:32 +0200

    jest/no-done-callback

--- src/util/throttle.test.ts
+++ src/util/throttle.test.ts
@@ -8,18 +8,18 @@ describe('throttle', () => {
         expect(executionCount).toBe(0);
     });
 
-    test('executes unthrottled function once per tick when period is 0', done => {
+    test('executes unthrottled function once per tick when period is 0', async () => {
         let executionCount = 0;
         const throttledFunction = throttle(() => { executionCount++; }, 0);
         throttledFunction();
         throttledFunction();
         expect(executionCount).toBe(1);
-        setTimeout(() => {
-            throttledFunction();
-            throttledFunction();
-            expect(executionCount).toBe(2);
-            done();
-        }, 0);
+
+        await new Promise(resolve => setTimeout(resolve, 0));
+        throttledFunction();
+        throttledFunction();
+
+        expect(executionCount).toBe(2);
     });
 
     test('executes unthrottled function immediately once when period is > 0', () => {
@@ -31,15 +31,15 @@ describe('throttle', () => {
         expect(executionCount).toBe(1);
     });
 
-    test('queues exactly one execution of unthrottled function when period is > 0', done => {
+    test('queues exactly one execution of unthrottled function when period is > 0', async () => {
         let executionCount = 0;
         const throttledFunction = throttle(() => { executionCount++; }, 5);
         throttledFunction();
         throttledFunction();
         throttledFunction();
-        setTimeout(() => {
-            expect(executionCount).toBe(2);
-            done();
-        }, 10);
+
+        await new Promise(resolve => setTimeout(resolve, 10));
+
+        expect(executionCount).toBe(2);
     });
 });
