19ab3d5b1035bd52a8fb0b34809d8d177580a09c
HarelM <harel.mazor@gmail.com>
2021-10-21 16:37:24 +0300

    Try automating codemods

--- codemods.js
+++ codemods.js
@@ -1,50 +1,73 @@
 import replace from 'replace-in-file';
+import {execSync} from 'child_process';
 
-const args = process.argv.slice(2);
 
-if (args.length === 0) {
+const file = process.argv[process.argv.length];
+const useAsync = process.argv.includes('--async');
+const automate = process.argv.includes('--auto');
+
+if (process.argv.length === 1) {
     console.log('Error: Path argument missing.');
     process.exit();
 }
 
+if (automate) {
+    let fileName = file.split('/').splice(-1)[0];
+    execSync(`git barnch ${fileName}`);
+    let destinationFile = file.replace('test/unit', 'src');
+    execSync(`git mv ${file} ${destinationFile}`);
+    execSync('git commit -m "Move and rename"');
+    file = destinationFile;
+    execSync(`jest-codemods --force ${file}`);
+    execSync(`git add ${file}`);
+    execSync('git commit -m "Run jest-codemods"');
+}
+
 replace.sync({
-    files: args,
+    files: [file],
     from: /import {test} from '..\/..\/util\/test';\n/g,
     to: '',
 })
 
 replace.sync({
-  files: args,
+  files: [file],
   from: /\ntest/g,
   to: '\ndescribe',
 });
 
 replace.sync({
-    files: args,
+    files: [file],
     from: /\(t\) =>/g,
-    to: process.argv.includes('--async') ? 'done =>' : '() =>',
+    to: useAsync ? 'done =>' : '() =>',
 });
 
 replace.sync({
-    files: args,
+    files: [file],
     from: /t.end\(\);\n/g,
-    to: process.argv.includes('--async') ? 'done();\n' : '',
+    to: useAsync ? 'done();\n' : '',
 });
 
 replace.sync({
-    files: args,
+    files: [file],
     from: /t.pass\(\);/g,
     to: ''
 });
 
 replace.sync({
-    files: args,
+    files: [file],
     from: /..\/..\/..\/rollup\/build\/tsc\/src\//g,
     to: '../',
 });
 
 replace.sync({
-    files: args,
+    files: [file],
     from: /t.test\(/g,
     to: 'test('
-})
+});
+
+if (automate) {
+    execSync('npm run lint -- --fix');
+    execSync(`git add ${file}`);
+    execSync('git commit -m "Run custom codemods"');
+}
+
