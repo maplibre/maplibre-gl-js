5e9c746a0ad72b4d22ef3f47c74d8b2a8f0b5c2d
Harel M <harel.mazor@gmail.com>
2021-10-21 19:41:25 +0300

    Automate jest migration steps (#548)

--- codemods.js
+++ codemods.js
@@ -1,50 +1,73 @@
 import replace from 'replace-in-file';
+import {execSync} from 'child_process';
 
-const args = process.argv.slice(2);
 
-if (args.length === 0) {
+let file = process.argv[process.argv.length - 1];
+const useAsync = process.argv.includes('--async');
+const automate = process.argv.includes('--auto');
+
+if (process.argv.length === 1) {
     console.log('Error: Path argument missing.');
     process.exit();
 }
 
+if (automate) {
+    let fileName = file.split('/').splice(-1)[0];
+    execSync(`git checkout -b ${fileName}`);
+    let destinationFile = file.replace('test/unit', 'src').replace('.js', '.ts');
+    execSync(`git mv ${file} ${destinationFile}`);
+    execSync('git commit -m "Move and rename"');
+    file = destinationFile;
+    execSync(`npx jscodeshift --parser babel --skipImportDetection -t node_modules/jest-codemods/dist/transformers/tape.js ${file}`);
+    execSync(`git add ${file}`);
+    execSync('git commit -m "Run jscodeshift"');
+}
+
 replace.sync({
-    files: args,
+    files: [file],
     from: /import {test} from '..\/..\/util\/test';\n/g,
     to: '',
 })
 
 replace.sync({
-  files: args,
+  files: [file],
   from: /\ntest/g,
   to: '\ndescribe',
 });
 
 replace.sync({
-    files: args,
+    files: [file],
     from: /\(t\) =>/g,
-    to: process.argv.includes('--async') ? 'done =>' : '() =>',
+    to: useAsync ? 'done =>' : '() =>',
 });
 
 replace.sync({
-    files: args,
+    files: [file],
     from: /t.end\(\);\n/g,
-    to: process.argv.includes('--async') ? 'done();\n' : '',
+    to: useAsync ? 'done();\n' : '',
 });
 
 replace.sync({
-    files: args,
+    files: [file],
     from: /t.pass\(\);/g,
     to: ''
 });
 
 replace.sync({
-    files: args,
+    files: [file],
     from: /..\/..\/..\/rollup\/build\/tsc\/src\//g,
     to: '../',
 });
 
 replace.sync({
-    files: args,
+    files: [file],
     from: /t.test\(/g,
     to: 'test('
-})
+});
+
+if (automate) {
+    execSync('npm run lint -- --fix');
+    execSync(`git add ${file}`);
+    execSync('git commit -m "Run custom codemods and lint"');
+}
+
