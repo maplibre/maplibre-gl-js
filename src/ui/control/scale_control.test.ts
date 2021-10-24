2b07325063fe31034f81f22f867453db94fdee9e
astridx <info@astrid-guenther.de>
2021-10-24 19:22:08 +0200

    manuell changes

--- src/ui/control/scale_control.test.ts
+++ src/ui/control/scale_control.test.ts
@@ -1,48 +1,53 @@
+import {createMap, setWebGlContext} from '../../util/test/util';
+import ScaleControl from './scale_control';
 
-import {test} from '../../../util/test';
-import {createMap} from '../../../util';
-import ScaleControl from '../../ui/control/scale_control';
+beforeEach(() => {
+    setWebGlContext();
+    window.performance.mark = jest.fn();
+});
 
-describe('ScaleControl appears in bottom-left by default', () => {
-    const map = createMap(t);
-    map.addControl(new ScaleControl());
+describe('ScaleControl', () => {
+    test('appears in bottom-left by default', () => {
+        const map = createMap(undefined, undefined);
+        map.addControl(new ScaleControl(undefined));
 
-    expect(
+        expect(
         map.getContainer().querySelectorAll('.maplibregl-ctrl-bottom-left .maplibregl-ctrl-scale').length
-    ).toBe(1);
-});
+        ).toBe(1);
+    });
 
-describe('ScaleControl appears in the position specified by the position option', () => {
-    const map = createMap(t);
-    map.addControl(new ScaleControl(), 'top-left');
+    test('appears in the position specified by the position option', () => {
+        const map = createMap(undefined, undefined);
+        map.addControl(new ScaleControl(undefined), 'top-left');
 
-    expect(
+        expect(
         map.getContainer().querySelectorAll('.maplibregl-ctrl-top-left .maplibregl-ctrl-scale').length
-    ).toBe(1);
-});
+        ).toBe(1);
+    });
 
-describe('ScaleControl should change unit of distance after calling setUnit', () => {
-    const map = createMap(t);
-    const scale = new ScaleControl();
-    const selector = '.maplibregl-ctrl-bottom-left .maplibregl-ctrl-scale';
-    map.addControl(scale);
+    test('should change unit of distance after calling setUnit', () => {
+        const map = createMap(undefined, undefined);
+        const scale = new ScaleControl(undefined);
+        const selector = '.maplibregl-ctrl-bottom-left .maplibregl-ctrl-scale';
+        map.addControl(scale);
 
-    let contents = map.getContainer().querySelector(selector).innerHTML;
-    t.match(contents, /km/);
+        let contents = map.getContainer().querySelector(selector).innerHTML;
+        expect(contents).toMatch(/km/);
 
-    scale.setUnit('imperial');
-    contents = map.getContainer().querySelector(selector).innerHTML;
-    t.match(contents, /mi/);
-});
+        scale.setUnit('imperial');
+        contents = map.getContainer().querySelector(selector).innerHTML;
+        expect(contents).toMatch(/mi/);
+    });
 
-describe('ScaleControl should respect the maxWidth regardless of the unit and actual scale', () => {
-    const map = createMap(t);
-    const maxWidth = 100;
-    const scale = new ScaleControl({maxWidth, unit: 'nautical'});
-    const selector = '.maplibregl-ctrl-bottom-left .maplibregl-ctrl-scale';
-    map.addControl(scale);
-    map.setZoom(12.5);
+    test('should respect the maxWidth regardless of the unit and actual scale', () => {
+        const map = createMap(undefined, undefined);
+        const maxWidth = 100;
+        const scale = new ScaleControl({maxWidth, unit: 'nautical'});
+        const selector = '.maplibregl-ctrl-bottom-left .maplibregl-ctrl-scale';
+        map.addControl(scale);
+        map.setZoom(12.5);
 
-    const el = map.getContainer().querySelector(selector);
-    expect(parseFloat(el.style.width, 10) <= maxWidth).toBeTruthy();
+        const el = map.getContainer().querySelector(selector) as HTMLElement;
+        expect(parseFloat(el.style.width) <= maxWidth).toBeTruthy();
+    });
 });
