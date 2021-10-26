a16b563165919213ded23bd6dfb39e99fa7d6d7d
astridx <info@astrid-guenther.de>
2021-10-26 14:03:25 +0200

    mauell changes

--- src/ui/map/requestRenderFrame.test.ts
+++ src/ui/map/requestRenderFrame.test.ts
@@ -1,35 +1,40 @@
-import '../../../stub_loader';
-import {test} from '../../../util/test';
-import {createMap} from '../../../util';
+import {createMap, setWebGlContext, setPerformance, setMatchMedia} from '../../util/test/util';
 
-describe('Map#_requestRenderFrame schedules a new render frame if necessary', done => {
-    const map = createMap(t);
-    t.stub(map, 'triggerRepaint');
-    map._requestRenderFrame(() => {});
-    expect(map.triggerRepaint.callCount).toBe(1);
-    map.remove();
-    done();
+beforeEach(() => {
+    setWebGlContext();
+    setPerformance();
+    setMatchMedia();
 });
 
-describe('Map#_requestRenderFrame queues a task for the next render frame', done => {
-    const map = createMap(t);
-    const cb = t.spy();
-    map._requestRenderFrame(cb);
-    map.once('render', () => {
-        expect(cb.callCount).toBe(1);
+describe('Map#', () => {
+    test('_requestRenderFrame schedules a new render frame if necessary', () => {
+        const map = createMap(undefined, undefined);
+        const triggerRepaint = jest.spyOn(map, 'triggerRepaint').mockImplementation(() => { });
+        map._requestRenderFrame(() => {});
+        expect(triggerRepaint).toHaveBeenCalledTimes(1);
         map.remove();
-        done();
     });
-});
 
-describe('Map#_cancelRenderFrame cancels a queued task', done => {
-    const map = createMap(t);
-    const cb = t.spy();
-    const id = map._requestRenderFrame(cb);
-    map._cancelRenderFrame(id);
-    map.once('render', () => {
-        expect(cb.callCount).toBe(0);
-        map.remove();
-        done();
+    test('_requestRenderFrame queues a task for the next render frame', done => {
+        const map = createMap(undefined, undefined);
+        const cb = jest.fn();
+        map._requestRenderFrame(cb);
+        map.once('render', () => {
+            expect(cb).toHaveBeenCalledTimes(1);
+            map.remove();
+            done();
+        });
+    });
+
+    test('_cancelRenderFrame cancels a queued task', done => {
+        const map = createMap(undefined, undefined);
+        const cb = jest.fn();
+        const id = map._requestRenderFrame(cb);
+        map._cancelRenderFrame(id);
+        map.once('render', () => {
+            expect(cb).not.toHaveBeenCalled();
+            map.remove();
+            done();
+        });
     });
 });
