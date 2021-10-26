395cea3b8449a028593f0bf3798704bc40f53622
astridx <info@astrid-guenther.de>
2021-10-26 13:11:55 +0200

    manuell changes

--- src/ui/map/isMoving.test.ts
+++ src/ui/map/isMoving.test.ts
@@ -1,164 +1,178 @@
-import '../../../stub_loader';
-import {test} from '../../../util/test';
 import browser from '../../util/browser';
-import Map from '../../ui/map';
+import Map from '../map';
 import DOM from '../../util/dom';
-import simulate from '../../../util/simulate_interaction';
+import simulate from '../../../test/util/simulate_interaction';
+import {setWebGlContext, setPerformance, setMatchMedia} from '../../util/test/util';
 
 function createMap() {
-    return new Map({container: DOM.create('div', '', window.document.body)});
+    return new Map({style: '', container: DOM.create('div', '', window.document.body)});
 }
 
-// MouseEvent.buttons
-const buttons = 1;
-
-describe('Map#isMoving returns false by default', () => {
-    const map = createMap(t);
-    expect(map.isMoving()).toBe(false);
-    map.remove();
+beforeEach(() => {
+    setWebGlContext();
+    setPerformance();
+    setMatchMedia();
 });
 
-describe('Map#isMoving returns true during a camera zoom animation', () => {
-    const map = createMap(t);
+describe('Map#isMoving', () => {
 
-    map.on('zoomstart', () => {
-        expect(map.isMoving()).toBe(true);
-    });
+    // MouseEvent.buttons
+    const buttons = 1;
 
-    map.on('zoomend', () => {
+    test('returns false by default', () => {
+        const map = createMap();
+
         expect(map.isMoving()).toBe(false);
         map.remove();
     });
 
-    map.zoomTo(5, {duration: 0});
-});
+    test('returns true during a camera zoom animation', done => {
+        const map = createMap();
 
-describe('Map#isMoving returns true when drag panning', () => {
-    const map = createMap(t);
+        map.on('zoomstart', () => {
+            expect(map.isMoving()).toBe(true);
+        });
 
-    map.on('movestart', () => {
-        expect(map.isMoving()).toBe(true);
-    });
-    map.on('dragstart', () => {
-        expect(map.isMoving()).toBe(true);
-    });
+        map.on('zoomend', () => {
+            expect(map.isMoving()).toBe(false);
+            map.remove();
+            done();
+        });
 
-    map.on('dragend', () => {
-        expect(map.isMoving()).toBe(false);
-    });
-    map.on('moveend', () => {
-        expect(map.isMoving()).toBe(false);
-        map.remove();
+        map.zoomTo(5, {duration: 0});
     });
 
-    simulate.mousedown(map.getCanvas());
-    map._renderTaskQueue.run();
+    test('returns true when drag panning', done => {
+        const map = createMap();
 
-    simulate.mousemove(map.getCanvas(), {buttons, clientX: 10, clientY: 10});
-    map._renderTaskQueue.run();
+        map.on('movestart', () => {
+            expect(map.isMoving()).toBe(true);
+        });
+        map.on('dragstart', () => {
+            expect(map.isMoving()).toBe(true);
+        });
 
-    simulate.mouseup(map.getCanvas());
-    map._renderTaskQueue.run();
-});
+        map.on('dragend', () => {
+            expect(map.isMoving()).toBe(false);
+        });
+        map.on('moveend', () => {
+            expect(map.isMoving()).toBe(false);
+            map.remove();
+            done();
+        });
 
-describe('Map#isMoving returns true when drag rotating', () => {
-    const map = createMap(t);
-
-    // Prevent inertial rotation.
-    t.stub(browser, 'now').returns(0);
-
-    map.on('movestart', () => {
-        expect(map.isMoving()).toBe(true);
-    });
-    map.on('rotatestart', () => {
-        expect(map.isMoving()).toBe(true);
-    });
-
-    map.on('rotateend', () => {
-        expect(map.isMoving()).toBe(false);
-    });
-    map.on('moveend', () => {
-        expect(map.isMoving()).toBe(false);
-        map.remove();
-    });
-
-    simulate.mousedown(map.getCanvas(), {buttons: 2, button: 2});
-    map._renderTaskQueue.run();
-
-    simulate.mousemove(map.getCanvas(), {buttons: 2, clientX: 10, clientY: 10});
-    map._renderTaskQueue.run();
-
-    simulate.mouseup(map.getCanvas(),   {buttons: 0, button: 2});
-    map._renderTaskQueue.run();
-});
-
-describe('Map#isMoving returns true when scroll zooming', () => {
-    const map = createMap(t);
-
-    map.on('zoomstart', () => {
-        expect(map.isMoving()).toBe(true);
-    });
-
-    map.on('zoomend', () => {
-        expect(map.isMoving()).toBe(false);
-        map.remove();
-    });
-
-    const browserNow = t.stub(browser, 'now');
-    let now = 0;
-    browserNow.callsFake(() => now);
-
-    simulate.wheel(map.getCanvas(), {type: 'wheel', deltaY: -simulate.magicWheelZoomDelta});
-    map._renderTaskQueue.run();
-
-    now += 400;
-    setTimeout(() => {
+        simulate.mousedown(map.getCanvas());
         map._renderTaskQueue.run();
-    }, 400);
-});
 
-describe('Map#isMoving returns true when drag panning and scroll zooming interleave', () => {
-    const map = createMap(t);
+        simulate.mousemove(map.getCanvas(), {buttons, clientX: 10, clientY: 10});
+        map._renderTaskQueue.run();
 
-    map.on('dragstart', () => {
-        expect(map.isMoving()).toBe(true);
-    });
-
-    map.on('zoomstart', () => {
-        expect(map.isMoving()).toBe(true);
-    });
-
-    map.on('zoomend', () => {
-        expect(map.isMoving()).toBe(true);
         simulate.mouseup(map.getCanvas());
+        map._renderTaskQueue.run();
+    });
+
+    test('returns true when drag rotating', done => {
+        const map = createMap();
+
+        // Prevent inertial rotation.
+        jest.spyOn(browser, 'now').mockImplementation(() => { return 0; });
+
+        map.on('movestart', () => {
+            expect(map.isMoving()).toBe(true);
+        });
+
+        map.on('rotatestart', () => {
+            expect(map.isMoving()).toBe(true);
+        });
+
+        map.on('rotateend', () => {
+            expect(map.isMoving()).toBe(false);
+        });
+
+        map.on('moveend', () => {
+            expect(map.isMoving()).toBe(false);
+            map.remove();
+            done();
+        });
+
+        simulate.mousedown(map.getCanvas(), {buttons: 2, button: 2});
+        map._renderTaskQueue.run();
+
+        simulate.mousemove(map.getCanvas(), {buttons: 2, clientX: 10, clientY: 10});
+        map._renderTaskQueue.run();
+
+        simulate.mouseup(map.getCanvas(),   {buttons: 0, button: 2});
+        map._renderTaskQueue.run();
+    });
+
+    test('returns true when scroll zooming', done => {
+        const map = createMap();
+
+        map.on('zoomstart', () => {
+            expect(map.isMoving()).toBe(true);
+        });
+
+        map.on('zoomend', () => {
+            expect(map.isMoving()).toBe(false);
+            map.remove();
+            done();
+        });
+
+        let now = 0;
+        jest.spyOn(browser, 'now').mockImplementation(() => { return now; });
+
+        simulate.wheel(map.getCanvas(), {type: 'wheel', deltaY: -simulate.magicWheelZoomDelta});
+        map._renderTaskQueue.run();
+
+        now += 400;
         setTimeout(() => {
             map._renderTaskQueue.run();
+        }, 400);
+    });
+
+    test('returns true when drag panning and scroll zooming interleave', done => {
+        const map = createMap();
+
+        map.on('dragstart', () => {
+            expect(map.isMoving()).toBe(true);
         });
-    });
 
-    map.on('dragend', () => {
-        expect(map.isMoving()).toBe(false);
-        map.remove();
-    });
+        map.on('zoomstart', () => {
+            expect(map.isMoving()).toBe(true);
+        });
 
-    // The following should trigger the above events, where a zoomstart/zoomend
-    // pair is nested within a dragstart/dragend pair.
+        map.on('zoomend', () => {
+            expect(map.isMoving()).toBe(true);
+            simulate.mouseup(map.getCanvas());
+            setTimeout(() => {
+                map._renderTaskQueue.run();
+                done();
+            });
+        });
 
-    simulate.mousedown(map.getCanvas());
-    map._renderTaskQueue.run();
+        map.on('dragend', () => {
+            expect(map.isMoving()).toBe(false);
+            map.remove();
+        });
 
-    simulate.mousemove(map.getCanvas(), {buttons, clientX: 10, clientY: 10});
-    map._renderTaskQueue.run();
+        // The following should trigger the above events, where a zoomstart/zoomend
+        // pair is nested within a dragstart/dragend pair.
 
-    const browserNow = t.stub(browser, 'now');
-    let now = 0;
-    browserNow.callsFake(() => now);
-
-    simulate.wheel(map.getCanvas(), {type: 'wheel', deltaY: -simulate.magicWheelZoomDelta});
-    map._renderTaskQueue.run();
-
-    now += 400;
-    setTimeout(() => {
+        simulate.mousedown(map.getCanvas());
         map._renderTaskQueue.run();
-    }, 400);
+
+        simulate.mousemove(map.getCanvas(), {buttons, clientX: 10, clientY: 10});
+        map._renderTaskQueue.run();
+
+        let now = 0;
+        jest.spyOn(browser, 'now').mockImplementation(() => { return now; });
+
+        simulate.wheel(map.getCanvas(), {type: 'wheel', deltaY: -simulate.magicWheelZoomDelta});
+        map._renderTaskQueue.run();
+
+        now += 400;
+        setTimeout(() => {
+            map._renderTaskQueue.run();
+        }, 400);
+    });
 });
