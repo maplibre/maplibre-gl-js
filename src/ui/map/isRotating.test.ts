bd03b24acc2e5a2e04ebf39e2fae234026827da6
astridx <info@astrid-guenther.de>
2021-10-26 13:34:54 +0200

    manuell changes

--- src/ui/map/isRotating.test.ts
+++ src/ui/map/isRotating.test.ts
@@ -1,59 +1,66 @@
-import '../../../stub_loader';
-import {test} from '../../../util/test';
 import Map from '../../ui/map';
 import DOM from '../../util/dom';
-import simulate from '../../../util/simulate_interaction';
+import simulate from '../../../test/util/simulate_interaction';
 import browser from '../../util/browser';
+import {setWebGlContext, setPerformance, setMatchMedia} from '../../util/test/util';
 
 function createMap() {
-    return new Map({container: DOM.create('div', '', window.document.body)});
+    return new Map({style: '', container: DOM.create('div', '', window.document.body)});
 }
 
-describe('Map#isRotating returns false by default', done => {
-    const map = createMap(t);
-    expect(map.isRotating()).toBe(false);
-    map.remove();
-    done();
+beforeEach(() => {
+    setWebGlContext();
+    setPerformance();
+    setMatchMedia();
 });
 
-describe('Map#isRotating returns true during a camera rotate animation', done => {
-    const map = createMap(t);
-
-    map.on('rotatestart', () => {
-        expect(map.isRotating()).toBe(true);
-    });
-
-    map.on('rotateend', () => {
+describe('Map#isRotating', () => {
+    test('returns false by default', done => {
+        const map = createMap();
         expect(map.isRotating()).toBe(false);
         map.remove();
         done();
     });
 
-    map.rotateTo(5, {duration: 0});
-});
+    test('returns true during a camera rotate animation', done => {
+        const map = createMap();
 
-describe('Map#isRotating returns true when drag rotating', done => {
-    const map = createMap(t);
+        map.on('rotatestart', () => {
+            expect(map.isRotating()).toBe(true);
+        });
 
-    // Prevent inertial rotation.
-    t.stub(browser, 'now').returns(0);
+        map.on('rotateend', () => {
+            expect(map.isRotating()).toBe(false);
+            map.remove();
+            done();
+        });
 
-    map.on('rotatestart', () => {
-        expect(map.isRotating()).toBe(true);
+        map.rotateTo(5, {duration: 0});
     });
 
-    map.on('rotateend', () => {
-        expect(map.isRotating()).toBe(false);
-        map.remove();
-        done();
+    test('returns true when drag rotating', done => {
+        const map = createMap();
+
+        // Prevent inertial rotation.
+        jest.spyOn(browser, 'now').mockImplementation(() => { return 0; });
+
+        map.on('rotatestart', () => {
+            expect(map.isRotating()).toBe(true);
+        });
+
+        map.on('rotateend', () => {
+            expect(map.isRotating()).toBe(false);
+            map.remove();
+            done();
+        });
+
+        simulate.mousedown(map.getCanvas(), {buttons: 2, button: 2});
+        map._renderTaskQueue.run();
+
+        simulate.mousemove(map.getCanvas(), {buttons: 2, clientX: 10, clientY: 10});
+        map._renderTaskQueue.run();
+
+        simulate.mouseup(map.getCanvas(),   {buttons: 0, button: 2});
+        map._renderTaskQueue.run();
     });
-
-    simulate.mousedown(map.getCanvas(), {buttons: 2, button: 2});
-    map._renderTaskQueue.run();
-
-    simulate.mousemove(map.getCanvas(), {buttons: 2, clientX: 10, clientY: 10});
-    map._renderTaskQueue.run();
-
-    simulate.mouseup(map.getCanvas(),   {buttons: 0, button: 2});
-    map._renderTaskQueue.run();
 });
