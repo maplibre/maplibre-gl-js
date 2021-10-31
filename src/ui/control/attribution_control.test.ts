324169e8186ef4ce3e62e131b3ef60c0936a684b
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2021-10-31 14:12:27 +0100

    Increase jest timeout from 10 seconds to 60 seconds

--- src/ui/control/attribution_control.test.ts
+++ src/ui/control/attribution_control.test.ts
@@ -23,7 +23,7 @@ beforeEach(() => {
 });
 
 describe('AttributionControl', () => {
-    jest.setTimeout(10000);
+    jest.setTimeout(60000);
     test('AttributionControl appears in bottom-right by default', () => {
         const map = createMap();
         map.addControl(new AttributionControl());
