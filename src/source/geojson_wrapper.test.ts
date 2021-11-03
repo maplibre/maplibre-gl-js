62d4e22281a2ba56bfde67ad44f1906eee3fb2ec
astridx <info@astrid-guenther.de>
2021-11-04 00:13:49 +0100

    manuell changes

--- src/source/geojson_wrapper.test.ts
+++ src/source/geojson_wrapper.test.ts
@@ -1,15 +1,14 @@
-import Wrapper from '../source/geojson_wrapper';
+import Wrapper from './geojson_wrapper';
 
 describe('geojsonwrapper', () => {
-
-    test('linestring', () => {
+    it('linestring', () => {
         const features = [{
             type: 2,
             geometry: [[[0, 0], [10, 10]]],
             tags: {hello: 'world'}
         }];
 
-        const wrap = new Wrapper(features);
+        const wrap = new Wrapper(features as any);
         const feature = wrap.feature(0);
 
         expect(feature).toBeTruthy();
@@ -19,16 +18,15 @@ describe('geojsonwrapper', () => {
 
     });
 
-    test('point', () => {
+    it('point', () => {
         const features = [{
             type: 1,
             geometry: [[0, 1]],
             tags: {}
         }];
 
-        const wrap = new Wrapper(features);
+        const wrap = new Wrapper(features as any);
         const feature = wrap.feature(0);
         expect(feature.loadGeometry()).toEqual([[{x: 0, y: 1}]]);
     });
-
 });
