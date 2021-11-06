9015ed3e7ed3e067fc4e91c6a596226e3ee09e52
Astrid <astridx@users.noreply.github.com>
2021-11-06 07:54:23 +0100

    delete none typed code (#621)

--- src/types/non-typed-modules.d.ts
+++ src/types/non-typed-modules.d.ts
@@ -16,37 +16,6 @@ declare module '@mapbox/mapbox-gl-supported' {
     export = __exports
 }
 
-declare module '@mapbox/unitbezier' {
-    class UnitBezier {
-        constructor(p1x: number, p1y: number, p2x: number, p2y: number);
-        sampleCurveX(t: number): number;
-        sampleCurveY(t: number): number;
-        sampleCurveDerivativeX(t: number): number;
-        solveCurveX(x: number, epsilon: number | void): number;
-        solve(x: number, epsilon: number | void): number;
-    }
-
-    let __exports: typeof UnitBezier;
-    export = __exports
-}
-declare module 'potpack' {
-    type Bin = {
-        x: number;
-        y: number;
-        w: number;
-        h: number;
-    };
-
-    function potpack(bins: Array<Bin>): {
-        w: number;
-        h: number;
-        fill: number;
-    }
-
-    let __exports: typeof potpack;
-    export = __exports
-}
-
 declare global {
     declare interface VectorTile {
         layers: {[_: string]: VectorTileLayer};
