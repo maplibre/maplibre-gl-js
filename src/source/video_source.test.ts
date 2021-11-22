e014e86890c0dd3e94606534cc6004c03959fc6a
astridx <info@astrid-guenther.de>
2021-11-22 10:11:08 +0100

    Run custom codemods and lint

--- src/source/video_source.test.ts
+++ src/source/video_source.test.ts
@@ -1,7 +1,6 @@
 import '../../stub_loader';
-import {test} from '../../util/test';
-import VideoSource from '../../../rollup/build/tsc/src/source/video_source';
-import {extend} from '../../../rollup/build/tsc/src/util/util';
+import VideoSource from '../source/video_source';
+import {extend} from '../util/util';
 
 function createSource(options) {
 
@@ -15,11 +14,11 @@ function createSource(options) {
     return source;
 }
 
-test('VideoSource', (t) => {
+describe('VideoSource', () => {
 
     const source = createSource({
         type: 'video',
-        urls : [ "cropped.mp4", "https://static-assets.mapbox.com/mapbox-gl-js/drone.webm" ],
+        urls : [ 'cropped.mp4', 'https://static-assets.mapbox.com/mapbox-gl-js/drone.webm' ],
         coordinates: [
             [-76.54, 39.18],
             [-76.52, 39.18],
@@ -28,32 +27,30 @@ test('VideoSource', (t) => {
         ]
     });
 
-    t.test('constructor', (t) => {
+    test('constructor', () => {
         expect(source.minzoom).toBe(0);
         expect(source.maxzoom).toBe(22);
         expect(source.tileSize).toBe(512);
-        t.end();
     });
 
-    t.test('sets coordinates', (t) => {
+    test('sets coordinates', () => {
 
         const newCoordinates = [[0, 0], [-1, 0], [-1, -1], [0, -1]];
         source.setCoordinates(newCoordinates);
         const serialized = source.serialize();
 
         expect(serialized.coordinates).toEqual(newCoordinates);
-        t.end();
 
     });
 
     //test video retrieval by first supplying the video element directly
-    t.test('gets video', (t) => {
+    test('gets video', () => {
 
         const el = window.document.createElement('video');
         const source = createSource({
             type: 'video',
             video: el,
-            urls : [ "cropped.mp4", "https://static-assets.mapbox.com/mapbox-gl-js/drone.webm" ],
+            urls : [ 'cropped.mp4', 'https://static-assets.mapbox.com/mapbox-gl-js/drone.webm' ],
             coordinates: [
                 [-76.54, 39.18],
                 [-76.52, 39.18],
@@ -63,8 +60,6 @@ test('VideoSource', (t) => {
         });
 
         expect(source.getVideo()).toBe(el);
-        t.end();
     });
 
-    t.end();
 });
