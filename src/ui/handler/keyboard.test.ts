29552ec331865a463ba9365fa33b9f2e684ce148
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2021-12-11 21:34:01 +0100

    Manual edits

--- src/ui/handler/keyboard.test.ts
+++ src/ui/handler/keyboard.test.ts
@@ -1,230 +1,235 @@
-import '../../../stub_loader';
-import {test} from '../../../util/test';
 import Map from '../../ui/map';
 import DOM from '../../util/dom';
-import simulate from '../../../util/simulate_interaction';
+import simulate from '../../../test/util/simulate_interaction';
 import {extend} from '../../util/util';
+import {setMatchMedia, setPerformance, setWebGlContext} from '../../util/test/util';
 
-function createMap(options) {
+function createMap(options?) {
     return new Map(extend({
         container: DOM.create('div', '', window.document.body),
     }, options));
 }
 
-describe('KeyboardHandler responds to keydown events', () => {
+beforeEach(() => {
+    setPerformance();
+    setWebGlContext();
+    setMatchMedia();
+});
+
+test('KeyboardHandler responds to keydown events', () => {
     const map = createMap();
     const h = map.keyboard;
-    t.spy(h, 'keydown');
+    const spy = jest.spyOn(h, 'keydown');
 
     simulate.keydown(map.getCanvas(), {keyCode: 32, key: ' '});
-    expect(h.keydown.called).toBeTruthy();
-    expect(h.keydown.getCall(0).args[0].keyCode).toBe(32);
+    expect(h.keydown).toHaveBeenCalled();
+    expect(spy.mock.calls[0][0].keyCode).toBe(32);
 });
 
-describe('KeyboardHandler pans map in response to arrow keys', () => {
+test('KeyboardHandler pans map in response to arrow keys', () => {
     const map = createMap({zoom: 10, center: [0, 0]});
-    t.spy(map, 'easeTo');
+    const spy = jest.spyOn(map, 'easeTo');
 
     simulate.keydown(map.getCanvas(), {keyCode: 32, key: ' '});
-    expect(map.easeTo.called).toBeFalsy();
+    expect(map.easeTo).not.toHaveBeenCalled();
 
     simulate.keydown(map.getCanvas(), {keyCode: 37, key: 'ArrowLeft'});
-    expect(map.easeTo.called).toBeTruthy();
-    let easeToArgs = map.easeTo.getCall(0).args[0];
+    expect(map.easeTo).toHaveBeenCalled();
+    let easeToArgs = spy.mock.calls[0][0];
     expect(easeToArgs.offset[0]).toBe(100);
-    expect(easeToArgs.offset[1]).toBe(0);
+    expect(easeToArgs.offset[1]).toBe(-0);
 
     simulate.keydown(map.getCanvas(), {keyCode: 39, key: 'ArrowRight'});
-    expect(map.easeTo.callCount === 2).toBeTruthy();
-    easeToArgs = map.easeTo.getCall(1).args[0];
+    expect(spy).toHaveBeenCalledTimes(2);
+    easeToArgs = spy.mock.calls[1][0];
     expect(easeToArgs.offset[0]).toBe(-100);
-    expect(easeToArgs.offset[1]).toBe(0);
+    expect(easeToArgs.offset[1]).toBe(-0);
 
     simulate.keydown(map.getCanvas(), {keyCode: 40, key: 'ArrowDown'});
-    expect(map.easeTo.callCount === 3).toBeTruthy();
-    easeToArgs = map.easeTo.getCall(2).args[0];
-    expect(easeToArgs.offset[0]).toBe(0);
+    expect(spy).toHaveBeenCalledTimes(3);
+    easeToArgs = spy.mock.calls[2][0];
+    expect(easeToArgs.offset[0]).toBe(-0);
     expect(easeToArgs.offset[1]).toBe(-100);
 
     simulate.keydown(map.getCanvas(), {keyCode: 38, key: 'ArrowUp'});
-    expect(map.easeTo.callCount === 4).toBeTruthy();
-    easeToArgs = map.easeTo.getCall(3).args[0];
-    expect(easeToArgs.offset[0]).toBe(0);
+    expect(spy).toHaveBeenCalledTimes(4);
+    easeToArgs = spy.mock.calls[3][0];
+    expect(easeToArgs.offset[0]).toBe(-0);
     expect(easeToArgs.offset[1]).toBe(100);
 
 });
 
-describe('KeyboardHandler pans map in response to arrow keys when disableRotation has been called', () => {
+test('KeyboardHandler pans map in response to arrow keys when disableRotation has been called', () => {
     const map = createMap({zoom: 10, center: [0, 0]});
-    t.spy(map, 'easeTo');
+    const spy = jest.spyOn(map, 'easeTo');
     map.keyboard.disableRotation();
 
     simulate.keydown(map.getCanvas(), {keyCode: 32, key: ' '});
-    expect(map.easeTo.called).toBeFalsy();
+    expect(map.easeTo).not.toHaveBeenCalled();
 
     simulate.keydown(map.getCanvas(), {keyCode: 37, key: 'ArrowLeft'});
-    expect(map.easeTo.called).toBeTruthy();
-    let easeToArgs = map.easeTo.getCall(0).args[0];
+    expect(map.easeTo).toHaveBeenCalled();
+    let easeToArgs = spy.mock.calls[0][0];
     expect(easeToArgs.offset[0]).toBe(100);
-    expect(easeToArgs.offset[1]).toBe(0);
+    expect(easeToArgs.offset[1]).toBe(-0);
 
     simulate.keydown(map.getCanvas(), {keyCode: 39, key: 'ArrowRight'});
-    expect(map.easeTo.callCount === 2).toBeTruthy();
-    easeToArgs = map.easeTo.getCall(1).args[0];
+    expect(spy).toHaveBeenCalledTimes(2);
+    easeToArgs = spy.mock.calls[1][0];
     expect(easeToArgs.offset[0]).toBe(-100);
-    expect(easeToArgs.offset[1]).toBe(0);
+    expect(easeToArgs.offset[1]).toBe(-0);
 
     simulate.keydown(map.getCanvas(), {keyCode: 40, key: 'ArrowDown'});
-    expect(map.easeTo.callCount === 3).toBeTruthy();
-    easeToArgs = map.easeTo.getCall(2).args[0];
-    expect(easeToArgs.offset[0]).toBe(0);
+    expect(spy).toHaveBeenCalledTimes(3);
+    easeToArgs = spy.mock.calls[2][0];
+    expect(easeToArgs.offset[0]).toBe(-0);
     expect(easeToArgs.offset[1]).toBe(-100);
 
     simulate.keydown(map.getCanvas(), {keyCode: 38, key: 'ArrowUp'});
-    expect(map.easeTo.callCount === 4).toBeTruthy();
-    easeToArgs = map.easeTo.getCall(3).args[0];
-    expect(easeToArgs.offset[0]).toBe(0);
+    expect(spy).toHaveBeenCalledTimes(4);
+    easeToArgs = spy.mock.calls[3][0];
+    expect(easeToArgs.offset[0]).toBe(-0);
     expect(easeToArgs.offset[1]).toBe(100);
 
 });
 
-describe('KeyboardHandler rotates map in response to Shift+left/right arrow keys', async () => {
+test('KeyboardHandler rotates map in response to Shift+left/right arrow keys', async () => {
     const map = createMap({zoom: 10, center: [0, 0], bearing: 0});
-    t.spy(map, 'easeTo');
+    const spy = jest.spyOn(map, 'easeTo');
 
     simulate.keydown(map.getCanvas(), {keyCode: 32, key: ' '});
-    expect(map.easeTo.called).toBeFalsy();
+    expect(map.easeTo).not.toHaveBeenCalled();
 
     simulate.keydown(map.getCanvas(), {keyCode: 37, key: 'ArrowLeft', shiftKey: true});
-    expect(map.easeTo.called).toBeTruthy();
-    let easeToArgs = map.easeTo.getCall(0).args[0];
+    expect(map.easeTo).toHaveBeenCalled();
+    let easeToArgs = spy.mock.calls[0][0];
     expect(easeToArgs.bearing).toBe(-15);
-    expect(easeToArgs.offset[0]).toBe(0);
+    expect(easeToArgs.offset[0]).toBe(-0);
 
     map.setBearing(0);
     simulate.keydown(map.getCanvas(), {keyCode: 39, key: 'ArrowRight', shiftKey: true});
-    expect(map.easeTo.callCount === 2).toBeTruthy();
-    easeToArgs = map.easeTo.getCall(1).args[0];
+    expect(spy).toHaveBeenCalledTimes(2);
+    easeToArgs = spy.mock.calls[1][0];
     expect(easeToArgs.bearing).toBe(15);
-    expect(easeToArgs.offset[0]).toBe(0);
+    expect(easeToArgs.offset[0]).toBe(-0);
 
 });
 
-describe('KeyboardHandler does not rotate map in response to Shift+left/right arrow keys when disableRotation has been called', async () => {
+test('KeyboardHandler does not rotate map in response to Shift+left/right arrow keys when disableRotation has been called', async () => {
     const map = createMap({zoom: 10, center: [0, 0], bearing: 0});
-    t.spy(map, 'easeTo');
+    const spy = jest.spyOn(map, 'easeTo');
     map.keyboard.disableRotation();
 
     simulate.keydown(map.getCanvas(), {keyCode: 32, key: ' '});
-    expect(map.easeTo.called).toBeFalsy();
+    expect(map.easeTo).not.toHaveBeenCalled();
 
     simulate.keydown(map.getCanvas(), {keyCode: 37, key: 'ArrowLeft', shiftKey: true});
-    expect(map.easeTo.called).toBeTruthy();
-    let easeToArgs = map.easeTo.getCall(0).args[0];
+    expect(map.easeTo).toHaveBeenCalled();
+    let easeToArgs = spy.mock.calls[0][0];
     expect(easeToArgs.bearing).toBe(0);
-    expect(easeToArgs.offset[0]).toBe(0);
+    expect(easeToArgs.offset[0]).toBe(-0);
 
     map.setBearing(0);
     simulate.keydown(map.getCanvas(), {keyCode: 39, key: 'ArrowRight', shiftKey: true});
-    expect(map.easeTo.callCount === 2).toBeTruthy();
-    easeToArgs = map.easeTo.getCall(1).args[0];
+    expect(spy).toHaveBeenCalledTimes(2);
+    easeToArgs = spy.mock.calls[1][0];
     expect(easeToArgs.bearing).toBe(0);
-    expect(easeToArgs.offset[0]).toBe(0);
+    expect(easeToArgs.offset[0]).toBe(-0);
 
 });
 
-describe('KeyboardHandler pitches map in response to Shift+up/down arrow keys', async () => {
+test('KeyboardHandler pitches map in response to Shift+up/down arrow keys', async () => {
     const map = createMap({zoom: 10, center: [0, 0], pitch: 30});
-    t.spy(map, 'easeTo');
+    const spy = jest.spyOn(map, 'easeTo');
 
     simulate.keydown(map.getCanvas(), {keyCode: 32, key: ' '});
-    expect(map.easeTo.called).toBeFalsy();
+    expect(map.easeTo).not.toHaveBeenCalled();
 
     simulate.keydown(map.getCanvas(), {keyCode: 40, key: 'ArrowDown', shiftKey: true});
-    expect(map.easeTo.called).toBeTruthy();
-    let easeToArgs = map.easeTo.getCall(0).args[0];
+    expect(map.easeTo).toHaveBeenCalled();
+    let easeToArgs = spy.mock.calls[0][0];
     expect(easeToArgs.pitch).toBe(20);
-    expect(easeToArgs.offset[1]).toBe(0);
+    expect(easeToArgs.offset[1]).toBe(-0);
 
     map.setPitch(30);
     simulate.keydown(map.getCanvas(), {keyCode: 38, key: 'ArrowUp', shiftKey: true});
-    expect(map.easeTo.callCount === 2).toBeTruthy();
-    easeToArgs = map.easeTo.getCall(1).args[0];
+    expect(spy).toHaveBeenCalledTimes(2);
+    easeToArgs = spy.mock.calls[1][0];
     expect(easeToArgs.pitch).toBe(40);
-    expect(easeToArgs.offset[1]).toBe(0);
+    expect(easeToArgs.offset[1]).toBe(-0);
 
 });
 
-describe('KeyboardHandler does not pitch map in response to Shift+up/down arrow keys when disableRotation has been called', async () => {
+test('KeyboardHandler does not pitch map in response to Shift+up/down arrow keys when disableRotation has been called', async () => {
     const map = createMap({zoom: 10, center: [0, 0], pitch: 30});
-    t.spy(map, 'easeTo');
+    const spy = jest.spyOn(map, 'easeTo');
     map.keyboard.disableRotation();
 
     simulate.keydown(map.getCanvas(), {keyCode: 32, key: ' '});
-    expect(map.easeTo.called).toBeFalsy();
+    expect(map.easeTo).not.toHaveBeenCalled();
 
     simulate.keydown(map.getCanvas(), {keyCode: 40, key: 'ArrowDown', shiftKey: true});
-    expect(map.easeTo.called).toBeTruthy();
-    let easeToArgs = map.easeTo.getCall(0).args[0];
+    expect(map.easeTo).toHaveBeenCalled();
+    let easeToArgs = spy.mock.calls[0][0];
     expect(easeToArgs.pitch).toBe(30);
-    expect(easeToArgs.offset[1]).toBe(0);
+    expect(easeToArgs.offset[1]).toBe(-0);
 
     map.setPitch(30);
     simulate.keydown(map.getCanvas(), {keyCode: 38, key: 'ArrowUp', shiftKey: true});
-    expect(map.easeTo.callCount === 2).toBeTruthy();
-    easeToArgs = map.easeTo.getCall(1).args[0];
+    expect(spy).toHaveBeenCalledTimes(2);
+    easeToArgs = spy.mock.calls[1][0];
     expect(easeToArgs.pitch).toBe(30);
-    expect(easeToArgs.offset[1]).toBe(0);
+    expect(easeToArgs.offset[1]).toBe(-0);
 
 });
 
-describe('KeyboardHandler zooms map in response to -/+ keys', () => {
+test('KeyboardHandler zooms map in response to -/+ keys', () => {
     const map = createMap({zoom: 10, center: [0, 0]});
-    t.spy(map, 'easeTo');
+    const spy = jest.spyOn(map, 'easeTo');
 
     simulate.keydown(map.getCanvas(), {keyCode: 187, key: 'Equal'});
-    expect(map.easeTo.callCount).toBe(1);
-    expect(map.easeTo.getCall(0).args[0].zoom).toBe(11);
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy.mock.calls[0][0].zoom).toBe(11);
 
     map.setZoom(10);
     simulate.keydown(map.getCanvas(), {keyCode: 187, key: 'Equal', shiftKey: true});
-    expect(map.easeTo.callCount).toBe(2);
-    expect(map.easeTo.getCall(1).args[0].zoom).toBe(12);
+    expect(spy).toHaveBeenCalledTimes(2);
+    expect(spy.mock.calls[1][0].zoom).toBe(12);
 
     map.setZoom(10);
     simulate.keydown(map.getCanvas(), {keyCode: 189, key: 'Minus'});
-    expect(map.easeTo.callCount).toBe(3);
-    expect(map.easeTo.getCall(2).args[0].zoom).toBe(9);
+    expect(spy).toHaveBeenCalledTimes(3);
+    expect(spy.mock.calls[2][0].zoom).toBe(9);
 
     map.setZoom(10);
     simulate.keydown(map.getCanvas(), {keyCode: 189, key: 'Minus', shiftKey: true});
-    expect(map.easeTo.callCount).toBe(4);
-    expect(map.easeTo.getCall(3).args[0].zoom).toBe(8);
+    expect(spy).toHaveBeenCalledTimes(4);
+    expect(spy.mock.calls[3][0].zoom).toBe(8);
 
 });
 
-describe('KeyboardHandler zooms map in response to -/+ keys when disableRotation has been called', () => {
+test('KeyboardHandler zooms map in response to -/+ keys when disableRotation has been called', () => {
     const map = createMap({zoom: 10, center: [0, 0]});
-    t.spy(map, 'easeTo');
+    const spy = jest.spyOn(map, 'easeTo');
     map.keyboard.disableRotation();
 
     simulate.keydown(map.getCanvas(), {keyCode: 187, key: 'Equal'});
-    expect(map.easeTo.callCount).toBe(1);
-    expect(map.easeTo.getCall(0).args[0].zoom).toBe(11);
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy.mock.calls[0][0].zoom).toBe(11);
 
     map.setZoom(10);
     simulate.keydown(map.getCanvas(), {keyCode: 187, key: 'Equal', shiftKey: true});
-    expect(map.easeTo.callCount).toBe(2);
-    expect(map.easeTo.getCall(1).args[0].zoom).toBe(12);
+    expect(spy).toHaveBeenCalledTimes(2);
+    expect(spy.mock.calls[1][0].zoom).toBe(12);
 
     map.setZoom(10);
     simulate.keydown(map.getCanvas(), {keyCode: 189, key: 'Minus'});
-    expect(map.easeTo.callCount).toBe(3);
-    expect(map.easeTo.getCall(2).args[0].zoom).toBe(9);
+    expect(spy).toHaveBeenCalledTimes(3);
+    expect(spy.mock.calls[2][0].zoom).toBe(9);
 
     map.setZoom(10);
     simulate.keydown(map.getCanvas(), {keyCode: 189, key: 'Minus', shiftKey: true});
-    expect(map.easeTo.callCount).toBe(4);
-    expect(map.easeTo.getCall(3).args[0].zoom).toBe(8);
+    expect(spy).toHaveBeenCalledTimes(4);
+    expect(spy.mock.calls[3][0].zoom).toBe(8);
 
 });
