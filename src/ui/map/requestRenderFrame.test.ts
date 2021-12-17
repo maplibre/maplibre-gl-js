7a08efae052451a718c26521b0bbb66b8852db99
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2021-12-17 18:06:42 +0100

    Wrap in describe

--- src/ui/map/requestRenderFrame.test.ts
+++ src/ui/map/requestRenderFrame.test.ts
@@ -6,33 +6,36 @@ beforeEach(() => {
     setMatchMedia();
 });
 
-test('Map#_requestRenderFrame schedules a new render frame if necessary', () => {
-    const map = createMap();
-    const spy = jest.spyOn(map, 'triggerRepaint');
-    map._requestRenderFrame(() => {});
-    expect(spy).toHaveBeenCalledTimes(1);
-    map.remove();
-});
+describe('requestRenderFrame', () => {
 
-test('Map#_requestRenderFrame queues a task for the next render frame', done => {
-    const map = createMap();
-    const cb = jest.fn();
-    map._requestRenderFrame(cb);
-    map.once('render', () => {
-        expect(cb).toHaveBeenCalledTimes(1);
+    test('Map#_requestRenderFrame schedules a new render frame if necessary', () => {
+        const map = createMap();
+        const spy = jest.spyOn(map, 'triggerRepaint');
+        map._requestRenderFrame(() => {});
+        expect(spy).toHaveBeenCalledTimes(1);
         map.remove();
-        done();
     });
-});
 
-test('Map#_cancelRenderFrame cancels a queued task', done => {
-    const map = createMap();
-    const cb = jest.fn();
-    const id = map._requestRenderFrame(cb);
-    map._cancelRenderFrame(id);
-    map.once('render', () => {
-        expect(cb).toHaveBeenCalledTimes(0);
-        map.remove();
-        done();
+    test('Map#_requestRenderFrame queues a task for the next render frame', done => {
+        const map = createMap();
+        const cb = jest.fn();
+        map._requestRenderFrame(cb);
+        map.once('render', () => {
+            expect(cb).toHaveBeenCalledTimes(1);
+            map.remove();
+            done();
+        });
+    });
+
+    test('Map#_cancelRenderFrame cancels a queued task', done => {
+        const map = createMap();
+        const cb = jest.fn();
+        const id = map._requestRenderFrame(cb);
+        map._cancelRenderFrame(id);
+        map.once('render', () => {
+            expect(cb).toHaveBeenCalledTimes(0);
+            map.remove();
+            done();
+        });
     });
 });
