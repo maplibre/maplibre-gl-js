1c3814bc72d2ec7bf3764a559875643f1d5255aa
acalcutt <acalcutt@techidiots.net>
2022-01-18 21:50:21 -0500

    clean up logo_control.ts

--- src/ui/control/logo_control.ts
+++ src/ui/control/logo_control.ts
@@ -12,7 +12,8 @@ type LogoOptions = {
  * A `LogoControl` is a control that adds the watermark.
  *
  * @implements {IControl}
- * @private
+ * @param {Object} [options]
+ * @param {boolean} [options.compact] If `true`, force a compact logo. If `false`, force the full logo. The default is a responsive logo that collapses when the map is less than 640 pixels wide.
 **/
 
 class LogoControl implements IControl {
@@ -23,8 +24,15 @@ class LogoControl implements IControl {
 
     constructor(options: LogoOptions = {}) {
         this.options = options;
-        bindAll(['_updateLogo'], this);
-        bindAll(['_updateCompact'], this);
+
+        bindAll([
+            '_updateLogo',
+            '_updateCompact'
+        ], this);
+    }
+
+    getDefaultPosition(): ControlPosition {
+        return 'bottom-left';
     }
 
     onAdd(map: Map) {
@@ -51,10 +59,6 @@ class LogoControl implements IControl {
         this._map.off('resize', this._updateCompact);
         this._map = undefined;
         this._compact = undefined;
-    }
-
-    getDefaultPosition(): ControlPosition {
-        return 'bottom-left';
     }
 
     _updateCompact() {
