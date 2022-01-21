2525247f2b80290a4ec9571628940bc98a8a57cd
Birk Skyum <birk.skyum@pm.me>
2022-01-21 19:19:51 +0100

    Wrap in describe

--- src/style-spec/validate_spec.test.ts
+++ src/style-spec/validate_spec.test.ts
@@ -5,13 +5,15 @@ import validate from '../style-spec/validate_style';
 
 const UPDATE = !!process.env.UPDATE;
 
-glob.sync('src/style-spec/fixture/*.input.json').forEach((file) => {
-    test(path.basename(file), () => {
-        const outputfile = file.replace('.input', '.output');
-        const style = fs.readFileSync(file);
-        const result = validate(style);
-        if (UPDATE) fs.writeFileSync(outputfile, JSON.stringify(result, null, 2));
-        const expectedOutput = JSON.parse(fs.readFileSync(outputfile).toString());
-        expect(result).toEqual(expectedOutput);
+describe('validate_spec', () => {
+    glob.sync('src/style-spec/fixture/*.input.json').forEach((file) => {
+        test(path.basename(file), () => {
+            const outputfile = file.replace('.input', '.output');
+            const style = fs.readFileSync(file);
+            const result = validate(style);
+            if (UPDATE) fs.writeFileSync(outputfile, JSON.stringify(result, null, 2));
+            const expectedOutput = JSON.parse(fs.readFileSync(outputfile).toString());
+            expect(result).toEqual(expectedOutput);
+        });
     });
 });
