88b0b97d8f115d3c92a13122a9764141a6fb9326
Birk Skyum <birk.skyum@pm.me>
2022-01-21 17:04:29 +0100

    Manual edits

--- src/style-spec/migrate.test.ts
+++ src/style-spec/migrate.test.ts
@@ -1,34 +1,23 @@
-import fs from 'fs';
-import glob from 'glob';
-import path, {dirname} from 'path';
-import validate from '../style-spec/validate_style';
-/* eslint-disable import/no-unresolved */
-import v8 from '../style-spec/reference/v8';
 import migrate from '../style-spec/migrate';
-import {fileURLToPath} from 'url';
-/* eslint-disable import/namespace */
 import * as spec from '../style-spec/style-spec';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const UPDATE = !!process.env.UPDATE;
-
-describe('does not migrate from version 5', () => {
+test('does not migrate from version 5', () => {
     expect(() => {
         migrate({version: 5, layers: []});
     }).toThrow(new Error('Cannot migrate from 5'));
 });
 
-describe('does not migrate from version 6', () => {
+test('does not migrate from version 6', () => {
     expect(() => {
         migrate({version: 6, layers: []});
     }).toThrow(new Error('Cannot migrate from 6'));
 });
 
-describe('migrates to latest version from version 7', () => {
+test('migrates to latest version from version 7', () => {
     expect(migrate({version: 7, layers: []}).version).toEqual(spec.latest.$version);
 });
 
-describe('converts token strings to expressions', () => {
+test('converts token strings to expressions', () => {
     const migrated = migrate({
         version: 8,
         layers: [{
@@ -36,12 +25,12 @@ describe('converts token strings to expressions', () => {
             type: 'symbol',
             layout: {'text-field': 'a{x}', 'icon-image': '{y}'}
         }]
-    }, spec.latest.$version);
+    });
     expect(migrated.layers[0].layout['text-field']).toEqual(['concat', 'a', ['get', 'x']]);
     expect(migrated.layers[0].layout['icon-image']).toEqual(['to-string', ['get', 'y']]);
 });
 
-describe('converts stop functions to expressions', () => {
+test('converts stop functions to expressions', () => {
     const migrated = migrate({
         version: 8,
         layers: [{
@@ -63,7 +52,7 @@ describe('converts stop functions to expressions', () => {
                 }
             }
         }]
-    }, spec.latest.$version);
+    });
     expect(migrated.layers[0].paint['background-opacity']).toEqual([
         'interpolate',
         ['linear'],
@@ -84,7 +73,7 @@ describe('converts stop functions to expressions', () => {
     ]);
 });
 
-describe('converts categorical function on resolvedImage type to valid expression', () => {
+test('converts categorical function on resolvedImage type to valid expression', () => {
     const migrated = migrate({
         version: 8,
         sources: {
@@ -107,7 +96,7 @@ describe('converts categorical function on resolvedImage type to valid expressio
                 }
             }
         }]
-    }, spec.latest.$version);
+    });
     expect(migrated.layers[0].layout['icon-image']).toEqual([
         'match',
         ['get', 'type' ],
@@ -115,17 +104,4 @@ describe('converts categorical function on resolvedImage type to valid expressio
         'some-icon',
         ''
     ]);
-    expect(validate.parsed(migrated, v8)).toEqual([]);
-});
-
-glob.sync(`${__dirname}/fixture/v7-migrate/*.input.json`).forEach((file) => {
-    test(path.basename(file), () => {
-        const outputfile = file.replace('.input', '.output');
-        const style = JSON.parse(fs.readFileSync(file));
-        const result = migrate(style);
-        expect(validate.parsed(result, v8)).toEqual([]);
-        if (UPDATE) fs.writeFileSync(outputfile, JSON.stringify(result, null, 2));
-        const expect = JSON.parse(fs.readFileSync(outputfile));
-        expect(result).toEqual(expect);
-    });
 });
