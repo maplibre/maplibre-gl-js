8f6f0238388fe6f08d2ad05389991fb9e85e41f2
Birk Skyum <birk.skyum@pm.me>
2022-01-21 19:12:57 +0100

    Run custom codemods

--- src/style-spec/validate_spec.test.ts
+++ src/style-spec/validate_spec.test.ts
@@ -1,47 +1,41 @@
-import {test} from '../../util/test';
 import glob from 'glob';
 import fs from 'fs';
 import path, {dirname} from 'path';
-import validate from '../../../rollup/build/tsc/src/style-spec/validate_style';
+import validate from '../style-spec/validate_style';
 import {fileURLToPath} from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const UPDATE = !!process.env.UPDATE;
 
 glob.sync(`${__dirname}/fixture/*.input.json`).forEach((file) => {
-    test(path.basename(file), (t) => {
+    test(path.basename(file), () => {
         const outputfile = file.replace('.input', '.output');
         const style = fs.readFileSync(file);
         const result = validate(style);
         if (UPDATE) fs.writeFileSync(outputfile, JSON.stringify(result, null, 2));
         const expect = JSON.parse(fs.readFileSync(outputfile));
         expect(result).toEqual(expect);
-        t.end();
     });
 });
 
 const fixtures = glob.sync(`${__dirname}/fixture/*.input.json`);
 const style = JSON.parse(fs.readFileSync(fixtures[0]));
-import reference from '../../../rollup/build/tsc/src/style-spec/reference/latest';
+import reference from '../style-spec/reference/latest';
 
-test('validate.parsed exists', (t) => {
+describe('validate.parsed exists', () => {
     expect(typeof validate.parsed).toBe('function');
-    t.end();
 });
 
-test('errors from validate.parsed do not contain line numbers', (t) => {
+describe('errors from validate.parsed do not contain line numbers', () => {
     const result = validate.parsed(style, reference);
-    expect(result[0].line).toBe(undefined);
-    t.end();
+    expect(result[0].line).toBeUndefined();
 });
 
-test('validate.latest exists', (t) => {
+describe('validate.latest exists', () => {
     expect(typeof validate.latest).toBe('function');
-    t.end();
 });
 
-test('errors from validate.latest do not contain line numbers', (t) => {
+describe('errors from validate.latest do not contain line numbers', () => {
     const result = validate.latest(style);
-    expect(result[0].line).toBe(undefined);
-    t.end();
+    expect(result[0].line).toBeUndefined();
 });
