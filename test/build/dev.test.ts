8f9dd8072ab7d465c57d2e7dcd450b36ec89f76b
Birk Skyum <birk.skyum@pm.me>
2022-01-22 23:26:33 +0100

    Run jest-codemods

--- test/build/dev.test.ts
+++ test/build/dev.test.ts
@@ -2,7 +2,11 @@ import {test} from '../util/test';
 import fs from 'fs';
 
 test('dev build contains asserts', (t) => {
-    t.assert(fs.readFileSync('dist/maplibre-gl-dev.js', 'utf8').indexOf('canary assert') !== -1);
-    t.assert(fs.readFileSync('dist/maplibre-gl-dev.js', 'utf8').indexOf('canary debug run') !== -1);
+    expect(
+        fs.readFileSync('dist/maplibre-gl-dev.js', 'utf8').indexOf('canary assert') !== -1
+    ).toBeTruthy();
+    expect(
+        fs.readFileSync('dist/maplibre-gl-dev.js', 'utf8').indexOf('canary debug run') !== -1
+    ).toBeTruthy();
     t.end();
 });
