2f6bdba7055e62d2372ff073d2914ea8605fa22a
Birk Skyum <birk.skyum@pm.me>
2022-01-22 23:27:28 +0100

    Run custom codemods

--- test/build/dev.test.ts
+++ test/build/dev.test.ts
@@ -1,12 +1,10 @@
-import {test} from '../util/test';
 import fs from 'fs';
 
-test('dev build contains asserts', (t) => {
+describe('dev build contains asserts', () => {
     expect(
         fs.readFileSync('dist/maplibre-gl-dev.js', 'utf8').indexOf('canary assert') !== -1
     ).toBeTruthy();
     expect(
         fs.readFileSync('dist/maplibre-gl-dev.js', 'utf8').indexOf('canary debug run') !== -1
     ).toBeTruthy();
-    t.end();
 });
