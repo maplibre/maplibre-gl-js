d57d15337816b68360189d1a17f6530b0aa5e09f
Birk Skyum <birk.skyum@pm.me>
2022-01-23 12:02:41 +0100

    Run jest-codemods

--- test/build/min.test.ts
+++ test/build/min.test.ts
@@ -10,8 +10,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const minBundle = fs.readFileSync('dist/maplibre-gl.js', 'utf8');
 
 test('production build removes asserts', (t) => {
-    t.assert(minBundle.indexOf('canary assert') === -1);
-    t.assert(minBundle.indexOf('canary debug run') === -1);
+    expect(minBundle.indexOf('canary assert') === -1).toBeTruthy();
+    expect(minBundle.indexOf('canary debug run') === -1).toBeTruthy();
     t.end();
 });
 
@@ -19,20 +19,20 @@ test('trims package.json assets', (t) => {
     // confirm that the entire package.json isn't present by asserting
     // the absence of each of our script strings
     for (const name in packageJson.scripts) {
-        t.assert(minBundle.indexOf(packageJson.scripts[name]) === -1);
+        expect(minBundle.indexOf(packageJson.scripts[name]) === -1).toBeTruthy();
     }
     t.end();
 });
 
 test('trims reference.json fields', (t) => {
-    t.assert(reference.$root.version.doc);
-    t.assert(minBundle.indexOf(reference.$root.version.doc) === -1);
+    expect(reference.$root.version.doc).toBeTruthy();
+    expect(minBundle.indexOf(reference.$root.version.doc) === -1).toBeTruthy();
     t.end();
 });
 
 test('can be browserified', (t) => {
     browserify(path.join(__dirname, 'browserify-test-fixture.js')).bundle((err) => {
-        t.ifError(err);
+        expect(err).toBeFalsy();
         t.end();
     });
 });
@@ -49,7 +49,7 @@ test('evaluates without errors', async (t) => {
     try {
         await import('../../dist/maplibre-gl.js');
     } catch (e) {
-        t.error(e);
+        expect(e).toBeFalsy();
     }
     t.end();
 });
