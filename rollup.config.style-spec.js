12baeac20cf1be3dccc006b7701d59bf420e23b4
Birk Skyum <birk.skyum@pm.me>
2022-01-23 19:37:50 +0100

    Replace require with import

--- rollup.config.style-spec.js
+++ rollup.config.style-spec.js
@@ -4,7 +4,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import unassert from 'rollup-plugin-unassert';
 import json from '@rollup/plugin-json';
-import {fileURLToPath} from 'url';
+import {fileURLToPath, pathToFileURL} from 'url';
 
 const esm = 'esm' in process.env;
 
@@ -25,7 +25,7 @@ const config = [{
                 // This check will cause the build to fail on CI allowing these issues to be caught.
                 if (importer && !importer.includes('node_modules')) {
                     const resolvedPath = path.join(importer, source);
-                    const importMetaUrl = require('url').pathToFileURL(__filename).toString();
+                    const importMetaUrl = pathToFileURL(__filename).toString();
                     const fromRoot = path.relative(dirname(fileURLToPath(importMetaUrl)), resolvedPath);
                     if (fromRoot.length > 2 && fromRoot.slice(0, 2) === '..') {
                         throw new Error(`Module ${importer} imports ${source} from outside the style-spec package root directory.`);
