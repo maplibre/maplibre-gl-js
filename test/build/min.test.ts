27bf0b9aa20256a3acca8c2f1edbb7e946e52cfd
Birk Skyum <birk.skyum@pm.me>
2022-01-23 12:04:48 +0100

    Wrap in describe

--- test/build/min.test.ts
+++ test/build/min.test.ts
@@ -1,4 +1,3 @@
-import {test} from '../util/test';
 import fs from 'fs';
 import path, {dirname} from 'path';
 import reference from '../../rollup/build/tsc/src/style-spec/reference/latest';
@@ -9,48 +8,50 @@ import {fileURLToPath} from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const minBundle = fs.readFileSync('dist/maplibre-gl.js', 'utf8');
 
-describe('production build removes asserts', done => {
-    expect(minBundle.indexOf('canary assert') === -1).toBeTruthy();
-    expect(minBundle.indexOf('canary debug run') === -1).toBeTruthy();
-    done();
-});
-
-describe('trims package.json assets', done => {
-    // confirm that the entire package.json isn't present by asserting
-    // the absence of each of our script strings
-    for (const name in packageJson.scripts) {
-        expect(minBundle.indexOf(packageJson.scripts[name]) === -1).toBeTruthy();
-    }
-    done();
-});
-
-describe('trims reference.json fields', done => {
-    expect(reference.$root.version.doc).toBeTruthy();
-    expect(minBundle.indexOf(reference.$root.version.doc) === -1).toBeTruthy();
-    done();
-});
-
-describe('can be browserified', done => {
-    browserify(path.join(__dirname, 'browserify-test-fixture.js')).bundle((err) => {
-        expect(err).toBeFalsy();
+describe('test min build', () => {
+    test('production build removes asserts', done => {
+        expect(minBundle.indexOf('canary assert') === -1).toBeTruthy();
+        expect(minBundle.indexOf('canary debug run') === -1).toBeTruthy();
         done();
     });
-});
 
-describe('evaluates without errors', async done => {
-    global.window = {
-        URL: {
-            createObjectURL: () => {}
+    test('trims package.json assets', done => {
+    // confirm that the entire package.json isn't present by asserting
+    // the absence of each of our script strings
+        for (const name in packageJson.scripts) {
+            expect(minBundle.indexOf(packageJson.scripts[name]) === -1).toBeTruthy();
         }
-    };
-    global.Blob = function() {};
-    global.performance = {};
-    global.navigator = {};
-    try {
-        await import('../../dist/maplibre-gl.js');
-    } catch (e) {
-        expect(e).toBeFalsy();
-    }
-    done();
-});
+        done();
+    });
 
+    test('trims reference.json fields', done => {
+        expect(reference.$root.version.doc).toBeTruthy();
+        expect(minBundle.indexOf(reference.$root.version.doc) === -1).toBeTruthy();
+        done();
+    });
+
+    test('can be browserified', done => {
+        browserify(path.join(__dirname, 'browserify-test-fixture.js')).bundle((err) => {
+            expect(err).toBeFalsy();
+            done();
+        });
+    });
+
+    test('evaluates without errors', async done => {
+        global.window = {
+            URL: {
+                createObjectURL: () => {}
+            }
+        };
+        global.Blob = function() {};
+        global.performance = {};
+        global.navigator = {};
+        try {
+            await import('../../dist/maplibre-gl.js');
+        } catch (e) {
+            expect(e).toBeFalsy();
+        }
+        done();
+    });
+
+});
