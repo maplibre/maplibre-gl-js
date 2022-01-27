9fc9cf8a4f2f27b132640bbb487d6b35168cf5a9
Birk Skyum <birk.skyum@pm.me>
2022-01-27 14:02:56 +0100

    Revert change to test

--- src/symbol/grid_index.test.ts
+++ src/symbol/grid_index.test.ts
@@ -4,10 +4,10 @@ import type {GridKey} from './grid_index';
 describe('GridIndex', () => {
 
     test('indexes features', () => {
-        const grid = new GridIndex<Number>(100, 100, 10);
-        grid.insert(0, 4, 10, 6, 30);
-        grid.insert(1, 4, 10, 30, 12);
-        grid.insert(2, -10, 30, 5, 35);
+        const grid = new GridIndex(100, 100, 10);
+        grid.insert(0 as GridKey, 4, 10, 6, 30);
+        grid.insert(1 as GridKey, 4, 10, 30, 12);
+        grid.insert(2 as GridKey, -10, 30, 5, 35);
 
         expect(grid.query(4, 10, 5, 11).map(x => x.key).sort()).toEqual([0, 1]);
         expect(grid.query(24, 10, 25, 11).map(x => x.key).sort()).toEqual([1]);
@@ -19,8 +19,8 @@ describe('GridIndex', () => {
     });
 
     test('returns multiple copies of a key if multiple boxes were inserted with the same key', () => {
-        const grid = new GridIndex<Number>(100, 100, 10);
-        const key = 123;
+        const grid = new GridIndex(100, 100, 10);
+        const key = 123 as GridKey;
         grid.insert(key, 3, 3, 4, 4);
         grid.insert(key, 13, 13, 14, 14);
         grid.insert(key, 23, 23, 24, 24);
@@ -28,22 +28,22 @@ describe('GridIndex', () => {
     });
 
     test('circle-circle intersection', () => {
-        const grid = new GridIndex<Number>(100, 100, 10);
-        grid.insertCircle(0, 50, 50, 10);
-        grid.insertCircle(1, 60, 60, 15);
-        grid.insertCircle(2, -10, 110, 20);
+        const grid = new GridIndex(100, 100, 10);
+        grid.insertCircle(0 as GridKey, 50, 50, 10);
+        grid.insertCircle(1 as GridKey, 60, 60, 15);
+        grid.insertCircle(2 as GridKey, -10, 110, 20);
 
-        expect(grid.hitTestCircle(55, 55, 2)).toBeTruthy();
-        expect(grid.hitTestCircle(10, 10, 10)).toBeFalsy();
-        expect(grid.hitTestCircle(0, 100, 10)).toBeTruthy();
-        expect(grid.hitTestCircle(80, 60, 10)).toBeTruthy();
+        expect(grid.hitTestCircle(55, 55, 2, 'never')).toBeTruthy();
+        expect(grid.hitTestCircle(10, 10, 10, 'never')).toBeFalsy();
+        expect(grid.hitTestCircle(0, 100, 10, 'never')).toBeTruthy();
+        expect(grid.hitTestCircle(80, 60, 10, 'never')).toBeTruthy();
     });
 
     test('circle-rectangle intersection', () => {
-        const grid = new GridIndex<Number>(100, 100, 10);
-        grid.insertCircle(0, 50, 50, 10);
-        grid.insertCircle(1, 60, 60, 15);
-        grid.insertCircle(2, -10, 110, 20);
+        const grid = new GridIndex(100, 100, 10);
+        grid.insertCircle(0 as GridKey, 50, 50, 10);
+        grid.insertCircle(1 as GridKey, 60, 60, 15);
+        grid.insertCircle(2 as GridKey, -10, 110, 20);
 
         expect(grid.query(45, 45, 55, 55).map(x => x.key)).toEqual([0, 1]);
         expect(grid.query(0, 0, 30, 30).map(x => x.key)).toEqual([]);
