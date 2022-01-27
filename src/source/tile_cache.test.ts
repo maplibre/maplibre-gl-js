3bf19d0e085e241134f5213392e46992bdce0756
astridx <info@astrid-guenther.de>
2022-01-27 22:09:48 +0100

    wip

--- src/source/tile_cache.test.ts
+++ src/source/tile_cache.test.ts
@@ -32,7 +32,7 @@ describe('TileCache', () => {
 
     test('get without removing', () => {
         const cache = new TileCache(10, () => {
-            done('test "get without removing" failed');
+            expect(false).toBeTruthy();
         });
         expect(cache.add(idA, tileA)).toBe(cache);
         expect(cache.get(idA)).toBe(tileA);
@@ -42,7 +42,7 @@ describe('TileCache', () => {
 
     test('duplicate add', () => {
         const cache = new TileCache(10, () => {
-            done('test "duplicate add" failed');
+            expect(false).toBeTruthy();
         });
 
         cache.add(idA, tileA);
