a452c5cb882baa082123daaec98458e17d54fd08
Birk Skyum <74932975+birkskyum@users.noreply.github.com>
2022-01-28 13:33:17 +0100

    fix camera tests on mac runners (#910)

--- src/ui/camera.test.ts
+++ src/ui/camera.test.ts
@@ -1545,7 +1545,7 @@ describe('#flyTo', () => {
             .on('moveend', () => {
                 endTime = new Date();
                 timeDiff = endTime - startTime;
-                expect(timeDiff).toBeLessThan(10);
+                expect(timeDiff).toBeLessThan(20);
                 done();
             });
 
