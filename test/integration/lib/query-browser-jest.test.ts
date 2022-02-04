b787279ff2c0b71af9c6c6911fc1dd1bca9ff63e
Birk Skyum <birk.skyum@pm.me>
2022-02-04 19:05:02 +0100

    Remove ignored test

--- test/integration/lib/query-browser-jest.test.ts
+++ test/integration/lib/query-browser-jest.test.ts
@@ -2,7 +2,6 @@
 // fixtures.json is automatically generated before this file gets built
 // refer build/generate-query-test-fixtures.ts
 import fixtures from '../dist/fixtures.json';
-import ignores from '../../ignores.json';
 import {deepEqual} from './json-diff';
 import st from 'st';
 import http from 'http';
@@ -42,147 +41,144 @@ describe('query tests', () => {
 
     Object.keys(fixtures).forEach((testName, testindex) => {
 
-        if (!(testName in ignores)) {
+        test(testName, async () => {
+            console.log((testindex + 1), ' / ', Object.keys(fixtures).length);
 
-            test(testName, async () => {
-                console.log((testindex + 1), ' / ', Object.keys(fixtures).length);
+            await page.goto(`file:${path.join(__dirname, 'fixtures/loadMap.html')}`);
 
-                await page.goto(`file:${path.join(__dirname, 'fixtures/loadMap.html')}`);
+            const currentTestName = testName;
+            const fixture = fixtures[currentTestName];
 
-                const currentTestName = testName;
-                const fixture = fixtures[currentTestName];
+            const style = fixture.style;
+            const options = style.metadata.test;
 
-                const style = fixture.style;
-                const options = style.metadata.test;
+            await page.evaluate((options) => {
+                const container = document.querySelector('#map');
+                // @ts-ignore
+                container.style.position = 'fixed';
+                // @ts-ignore
+                container.style.bottom = '10px';
+                // @ts-ignore
+                container.style.right = '10px';
+                // @ts-ignore
+                container.style.width = `${options.width}px`;
+                // @ts-ignore
+                container.style.height = `${options.height}px`;
+            }, options);
 
-                await page.evaluate((options) => {
-                    const container = document.querySelector('#map');
-                    // @ts-ignore
-                    container.style.position = 'fixed';
-                    // @ts-ignore
-                    container.style.bottom = '10px';
-                    // @ts-ignore
-                    container.style.right = '10px';
-                    // @ts-ignore
-                    container.style.width = `${options.width}px`;
-                    // @ts-ignore
-                    container.style.height = `${options.height}px`;
-                }, options);
+            const actual = await page.evaluate((fixture, _testName) => {
 
-                const actual = await page.evaluate((fixture, _testName) => {
+                return new Promise((resolve, _reject) => {
 
-                    return new Promise((resolve, _reject) => {
-
-                        function handleOperation(map, operations, opIndex, done) {
-                            const operation = operations[opIndex];
-                            const opName = operation[0];
-                            //Delegate to special handler if one is available
-                            if (opName in operationHandlers) {
-                                operationHandlers[opName](map, operation.slice(1), () => {
-                                    done(opIndex);
-                                });
-                            } else {
-                                map[opName](...operation.slice(1));
+                    function handleOperation(map, operations, opIndex, done) {
+                        const operation = operations[opIndex];
+                        const opName = operation[0];
+                        //Delegate to special handler if one is available
+                        if (opName in operationHandlers) {
+                            operationHandlers[opName](map, operation.slice(1), () => {
                                 done(opIndex);
-                            }
+                            });
+                        } else {
+                            map[opName](...operation.slice(1));
+                            done(opIndex);
+                        }
+                    }
+
+                    const operationHandlers = {
+                        wait(map, params, done) {
+                            const wait = function() {
+                                if (map.loaded()) {
+                                    done();
+                                } else {
+                                    map.once('render', wait);
+                                }
+                            };
+                            wait();
+                        },
+                        idle(map, params, done) {
+                            const idle = function() {
+                                if (!map.isMoving()) {
+                                    done();
+                                } else {
+                                    map.once('render', idle);
+                                }
+                            };
+                            idle();
+                        }
+                    };
+
+                    function applyOperations(map, operations, done) {
+                        // No operations specified, end immediately and invoke done.
+                        if (!operations || operations.length === 0) {
+                            done();
+                            return;
                         }
 
-                        const operationHandlers = {
-                            wait(map, params, done) {
-                                const wait = function() {
-                                    if (map.loaded()) {
-                                        done();
-                                    } else {
-                                        map.once('render', wait);
-                                    }
-                                };
-                                wait();
-                            },
-                            idle(map, params, done) {
-                                const idle = function() {
-                                    if (!map.isMoving()) {
-                                        done();
-                                    } else {
-                                        map.once('render', idle);
-                                    }
-                                };
-                                idle();
-                            }
-                        };
-
-                        function applyOperations(map, operations, done) {
-                            // No operations specified, end immediately and invoke done.
-                            if (!operations || operations.length === 0) {
+                        // Start recursive chain
+                        const scheduleNextOperation = (lastOpIndex) => {
+                            if (lastOpIndex === operations.length - 1) {
+                                // Stop recusive chain when at the end of the operations
                                 done();
                                 return;
                             }
 
-                            // Start recursive chain
-                            const scheduleNextOperation = (lastOpIndex) => {
-                                if (lastOpIndex === operations.length - 1) {
-                                    // Stop recusive chain when at the end of the operations
-                                    done();
-                                    return;
-                                }
+                            handleOperation(map, operations, ++lastOpIndex, scheduleNextOperation);
+                        };
+                        scheduleNextOperation(-1);
+                    }
 
-                                handleOperation(map, operations, ++lastOpIndex, scheduleNextOperation);
-                            };
-                            scheduleNextOperation(-1);
-                        }
+                    const style = fixture.style;
+                    const options = style.metadata.test;
+                    const skipLayerDelete = style.metadata.skipLayerDelete;
 
-                        const style = fixture.style;
-                        const options = style.metadata.test;
-                        const skipLayerDelete = style.metadata.skipLayerDelete;
-
+                    // @ts-ignore
+                    const map =  new maplibregl.Map({
+                        container: 'map',
+                        style,
                         // @ts-ignore
-                        const map =  new maplibregl.Map({
-                            container: 'map',
-                            style,
-                            // @ts-ignore
-                            classes: options.classes,
-                            interactive: false,
-                            attributionControl: false,
-                            pixelRatio: options.pixelRatio,
-                            preserveDrawingBuffer: true,
-                            axonometric: options.axonometric || false,
-                            skew: options.skew || [0, 0],
-                            fadeDuration: options.fadeDuration || 0,
-                            localIdeographFontFamily: options.localIdeographFontFamily || false,
-                            crossSourceCollisions: typeof options.crossSourceCollisions === 'undefined' ? true : options.crossSourceCollisions
-                        });
-
-                        map.repaint = true;
-                        map.once('load', () => {
-                            console.log('load', map);
-                            // Run the operations on the map
-                            applyOperations(map, options.operations, () => {
-                                console.log('operation', map.queryRenderedFeatures);
-
-                                // Perform query operation and compare results from expected values
-                                const results = options.queryGeometry ?
-                                    map.queryRenderedFeatures(options.queryGeometry, options.queryOptions || {}) :
-                                    [];
-                                console.log('results', results);
-
-                                const actual = results.map((feature) => {
-                                    const featureJson = JSON.parse(JSON.stringify(feature.toJSON()));
-                                    if (!skipLayerDelete) delete featureJson.layer;
-                                    return featureJson;
-                                });
-
-                                resolve(actual);
-
-                            });
-                        });
-
+                        classes: options.classes,
+                        interactive: false,
+                        attributionControl: false,
+                        pixelRatio: options.pixelRatio,
+                        preserveDrawingBuffer: true,
+                        axonometric: options.axonometric || false,
+                        skew: options.skew || [0, 0],
+                        fadeDuration: options.fadeDuration || 0,
+                        localIdeographFontFamily: options.localIdeographFontFamily || false,
+                        crossSourceCollisions: typeof options.crossSourceCollisions === 'undefined' ? true : options.crossSourceCollisions
                     });
-                }, fixture, testName);
 
-                expect(deepEqual(actual, fixture.expected)).toBeTruthy();
+                    map.repaint = true;
+                    map.once('load', () => {
+                        console.log('load', map);
+                        // Run the operations on the map
+                        applyOperations(map, options.operations, () => {
+                            console.log('operation', map.queryRenderedFeatures);
 
-            }, 10000);
+                            // Perform query operation and compare results from expected values
+                            const results = options.queryGeometry ?
+                                map.queryRenderedFeatures(options.queryGeometry, options.queryOptions || {}) :
+                                [];
+                            console.log('results', results);
 
-        }
+                            const actual = results.map((feature) => {
+                                const featureJson = JSON.parse(JSON.stringify(feature.toJSON()));
+                                if (!skipLayerDelete) delete featureJson.layer;
+                                return featureJson;
+                            });
+
+                            resolve(actual);
+
+                        });
+                    });
+
+                });
+            }, fixture, testName);
+
+            expect(deepEqual(actual, fixture.expected)).toBeTruthy();
+
+        }, 10000);
+
     });
 
 });
