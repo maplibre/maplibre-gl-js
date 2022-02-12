1e7569c7c9e63211b50cf69b2c21e23ff3367e8a
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2022-02-12 21:51:11 +0100

    Bump to v2.1.5-pre.1 - Publish empty `postinstall.js` file

--- postinstall.js
+++ postinstall.js
@@ -1,5 +1,3 @@
 import {execSync} from 'child_process';
 
-if (process.env.NODE_ENV !== 'production') {
-    execSync('npm run codegen && npm run generate-query-test-fixtures && husky install');
-}
+execSync('npm run codegen && npm run generate-query-test-fixtures && husky install');
