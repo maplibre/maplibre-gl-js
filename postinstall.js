a10aadf93ab87c0713deb7f30b1acd5783d84fed
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2022-02-12 20:19:22 +0100

    Use production env var value

--- postinstall.js
+++ postinstall.js
@@ -1,5 +1,5 @@
 import {execSync} from 'child_process';
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV !== 'production') {
     execSync('npm run codegen && npm run generate-query-test-fixtures && husky install');
 }
