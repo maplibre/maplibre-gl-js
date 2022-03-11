b00e38a5d58e59d2a26e65d411a1ac037f21aed7
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2022-03-11 13:59:23 +0100

    Fix logic

--- src/render/draw_symbol.ts
+++ src/render/draw_symbol.ts
@@ -269,7 +269,7 @@ function drawLayerSymbols(
     // Line label rotation happens in `updateLineLabels`
     // Pitched point labels are automatically rotated by the labelPlaneMatrix projection
     // Unpitched point labels need to have their rotation applied after projection
-    const rotateInShader = rotateWithMap && !pitchWithMap && (!alongLine || rotationAlignment === 'viewport-glyph');
+    const rotateInShader = rotateWithMap && !pitchWithMap && !alongLine;
 
     const hasSortKey = !layer.layout.get('symbol-sort-key').isConstant();
     let sortFeaturesByKey = false;
