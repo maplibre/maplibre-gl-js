dcf687604c92d6b003fcae6a204eadfdbcb48e7c
Timo Korkalainen <timo@ctrlreality.fi>
2022-03-18 13:50:46 +0200

    Replace vec3.fromValues with plain array

--- src/geo/transform.ts
+++ src/geo/transform.ts
@@ -724,10 +724,10 @@ class Transform {
 
         // The mercatorMatrix can be used to transform points from mercator coordinates
         // ([0, 0] nw, [1, 1] se) to GL coordinates.
-        this.mercatorMatrix = mat4.scale([] as any, m, vec3.fromValues(this.worldSize, this.worldSize, this.worldSize));
+        this.mercatorMatrix = mat4.scale([] as any, m, [this.worldSize, this.worldSize, this.worldSize]);
 
         // scale vertically to meters per pixel (inverse of ground resolution):
-        mat4.scale(m, m, vec3.fromValues(1, 1, mercatorZfromAltitude(1, this.center.lat) * this.worldSize));
+        mat4.scale(m, m, [1, 1, mercatorZfromAltitude(1, this.center.lat) * this.worldSize]);
 
         this.projMatrix = m;
         this.invProjMatrix = mat4.invert([] as any, this.projMatrix);
