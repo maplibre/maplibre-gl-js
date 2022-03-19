c6ce8fe95d4f7fc66cd666243f7133890a523a05
Timo Korkalainen <timo@ctrlreality.fi>
2022-03-19 12:21:29 +0200

    Correct lint

--- src/symbol/collision_index.test.ts
+++ src/symbol/collision_index.test.ts
@@ -1,14 +1,14 @@
-import CollisionIndex from "./collision_index";
+import CollisionIndex from './collision_index';
 import {mat4} from 'gl-matrix';
 
 import Transform from '../geo/transform';
 
-describe("CollisionIndex", () => {
+describe('CollisionIndex', () => {
 
-    test("floating point precision", () => {
+    test('floating point precision', () => {
         const posMatrix = mat4.create();
         const x = 100000.123456, y = 0;
-        let transform = new Transform(0, 22, 0, 60, true);
+        const transform = new Transform(0, 22, 0, 60, true);
         transform.resize(200, 200);
 
         const ci = new CollisionIndex(transform);
--- src/symbol/projection.test.ts
+++ src/symbol/projection.test.ts
@@ -1,12 +1,12 @@
-import { project } from "./projection";
+import {project} from './projection';
 
 import Point from '@mapbox/point-geometry';
-import {mat4, vec4} from 'gl-matrix';
+import {mat4} from 'gl-matrix';
 
 describe('Projection', () => {
     test('matrix float precision', () => {
-        let point = new Point(10.000000005 , 0);
-        let matrix = mat4.create();
-        expect(project(point,matrix).point.x).toBeCloseTo(point.x, 10);
-    })
+        const point = new Point(10.000000005, 0);
+        const matrix = mat4.create();
+        expect(project(point, matrix).point.x).toBeCloseTo(point.x, 10);
+    });
 });
