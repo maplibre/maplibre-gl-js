441ed0c64b7c609e54616a011fddafc8efc635c1
Andrew Chou <andrewchou@fastmail.com>
2022-03-21 13:39:58 -0400

    Add build step for generating types

---
+++ src/style-spec/build/generate-typings.ts
@@ -0,0 +1,18 @@
+import fs from "fs";
+import childProcess from "child_process";
+
+if (!fs.existsSync("dist")) {
+  fs.mkdirSync("dist");
+}
+
+console.log("Starting bundling types");
+const outputFile = "./dist/index.d.ts";
+childProcess.execSync(
+  `dts-bundle-generator --no-check -o ${outputFile} ./style-spec.ts`
+);
+let types = fs.readFileSync(outputFile, "utf8");
+// Classes are not exported but should be since this is exported as UMD - fixing...
+types = types.replace(/declare class/g, "export declare class");
+
+fs.writeFileSync(outputFile, types);
+console.log("Finished bundling types");
