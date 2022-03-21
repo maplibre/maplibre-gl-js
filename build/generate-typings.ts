fa745254e910e66ec7db9f7469fbe783b4e43865
Andrew Chou <andrewchou@fastmail.com>
2022-03-21 14:23:51 -0400

    Use shared generate-typings script to build types instead

--- build/generate-typings.ts
+++ build/generate-typings.ts
@@ -2,15 +2,38 @@
 import fs from 'fs';
 import childProcess from 'child_process';
 
-if (!fs.existsSync('dist')) {
-    fs.mkdirSync('dist');
+console.log('Starting bundling types');
+
+function generateGLTypes() {
+    if (!fs.existsSync('dist')) {
+        fs.mkdirSync('dist');
+    }
+    const outputFile = './dist/maplibre-gl.d.ts';
+    childProcess.execSync(`dts-bundle-generator --no-check --umd-module-name=maplibregl -o ${outputFile} ./src/index.ts`);
+    let types = fs.readFileSync(outputFile, 'utf8');
+    // Classes are not exported but should be since this is exported as UMD - fixing...
+    types = types.replace(/declare class/g, 'export declare class');
+    fs.writeFileSync(outputFile, types);
+    console.log("Finished building maplibre-gl")
 }
 
-console.log('Starting bundling types');
-const outputFile = './dist/maplibre-gl.d.ts';
-childProcess.execSync(`dts-bundle-generator --no-check --umd-module-name=maplibregl -o ${outputFile} ./src/index.ts`);
-let types = fs.readFileSync(outputFile, 'utf8');
-// Classes are not exported but should be since this is exported as UMD - fixing...
-types = types.replace(/declare class/g, 'export declare class');
-fs.writeFileSync(outputFile, types);
+function generateStyleSpecTypes() {
+    const moduleBasePath = './src/style-spec'
+    const outputPath = `${moduleBasePath}/dist`
+    if (!fs.existsSync(outputPath)) {
+        fs.mkdirSync(outputPath);
+    }
+    const outputFile = `${outputPath}/index.d.ts`;
+    childProcess.execSync(`dts-bundle-generator --no-check -o ${outputFile} ${moduleBasePath}/style-spec.ts`);
+    let types = fs.readFileSync(outputFile, 'utf8');
+    // Classes are not exported but should be since this is exported as UMD - fixing...
+    types = types.replace(/declare class/g, 'export declare class');
+    fs.writeFileSync(outputFile, types);
+    console.log("Finished building style-spec")
+}
+
+
+generateGLTypes()
+generateStyleSpecTypes()
+
 console.log('Finished bundling types');
