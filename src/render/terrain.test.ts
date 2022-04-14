c4d0b1c396ff525db7001e03824fcfbf41ca0863
HarelM <harel.mazor@gmail.com>
2022-04-14 22:21:21 +0300

    Added -1 for terrain calculation, added a test, fixed public field

---
+++ src/render/terrain.test.ts
@@ -0,0 +1,49 @@
+import Point from '@mapbox/point-geometry';
+import Terrain from './terrain';
+import gl from 'gl';
+import Context from '../gl/context';
+import {RGBAImage} from '../util/image';
+import Texture from './texture';
+
+describe('Terrain', () => {
+    test('pointCoordiate should not return null', () => {
+        const style = {
+            map: {
+                painter: {
+                    context: new Context(gl(1, 1)),
+                    width: 1,
+                    height: 1
+                }
+            }
+        } as any;
+        const sourceCache = {
+            getTileByID: (tileID) => {
+                if (tileID !== 'abcd') {
+                    return null;
+                }
+                return {
+                    tileID: {
+                        canonical: {
+                            x: 0,
+                            y: 0,
+                            z: 0
+                        }
+                    }
+                };
+            }
+        } as any;
+        const terrain = new Terrain(style, {} as any, {} as any);
+        terrain.sourceCache = sourceCache;
+        const context = style.map.painter.context as Context;
+        const pixels = new Uint8Array([0, 0, 255, 255]);
+        const image = new RGBAImage({width: 1, height: 1}, pixels);
+        const imageTexture = new Texture(context, image, context.gl.RGBA);
+        terrain.getFramebuffer('coords'); // allow init of frame buffers
+        terrain._fboCoordsTexture.texture = imageTexture.texture;
+        terrain.coordsIndex.push('abcd');
+
+        const coordinate = terrain.pointCoordinate(new Point(0, 0));
+
+        expect(coordinate).not.toBeNull();
+    });
+});
