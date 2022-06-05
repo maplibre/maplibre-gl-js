a3d95d81cf953b2751c69f29a1638360c2d14d02
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2022-06-05 14:33:04 +0200

    Fix benchmark MapStub class (#1297)

--- test/bench/lib/tile_parser.ts
+++ test/bench/lib/tile_parser.ts
@@ -4,6 +4,7 @@ import assert from 'assert';
 
 import deref from '../../../src/style-spec/deref';
 import Style from '../../../src/style/style';
+import Transform from '../../../src/geo/transform';
 import {Evented} from '../../../src/util/evented';
 import {RequestManager} from '../../../src/util/request_manager';
 import WorkerTile from '../../../src/source/worker_tile';
@@ -18,10 +19,12 @@ import type Map from '../../../src/ui/map';
 class StubMap extends Evented {
     style: Style;
     _requestManager: RequestManager;
+    transform: Transform;
 
     constructor() {
         super();
         this._requestManager = new RequestManager();
+        this.transform = new Transform();
     }
 
     getPixelRatio() {
