fdc5b5c660f80a043e7da9176b88541aa66121a8
Kanahiro <mediterranean1769@gmail.com>
2022-06-11 21:03:26 +0900

    set threshold to kill pan-inertia

--- src/ui/handler_inertia.ts
+++ src/ui/handler_inertia.ts
@@ -87,9 +87,13 @@ export default class HandlerInertia {
             deltas.zoom += settings.zoomDelta || 0;
             deltas.bearing += settings.bearingDelta || 0;
             deltas.pitch += settings.pitchDelta || 0;
-            if (settings.panDelta) deltas.pan._add(settings.panDelta);
             if (settings.around) deltas.around = settings.around;
             if (settings.pinchAround) deltas.pinchAround = settings.pinchAround;
+            if (settings.panDelta) {
+                deltas.pan._add(settings.panDelta);
+                const panDistance = ((settings.panDelta as Point).x ** 2 + (settings.panDelta as Point).y ** 2) ** 0.5;
+                if  (panDistance < 3) deltas.pan = new Point(0, 0) // the number is a threshold to kill inertia, in xy-distance
+            }
         }
 
         const lastEntry = this._inertiaBuffer[this._inertiaBuffer.length - 1];
