d1c6144669081ac6d35b9aefbb47046907718f11
David Weber | geOps <david.weber@geops.de>
2022-06-12 15:06:24 +0200

    fix comment

--- src/shaders/raster.vertex.glsl
+++ src/shaders/raster.vertex.glsl
@@ -12,7 +12,7 @@ varying vec2 v_pos1;
 void main() {
     gl_Position = u_matrix * vec4(a_pos, 0, 1);
     // We are using Int16 for texture position coordinates to give us enough precision for
-    // fractional coordinates. We use 8192 to scale the texture coordinates in the buffer
+    // fractional coordinates. We use 16384 to scale the texture coordinates in the buffer
     // as an arbitrarily high number to preserve adequate precision when rendering.
     // This is also the same value as the EXTENT we are using for our tile buffer pos coordinates,
     // so math for modifying either is consistent.
