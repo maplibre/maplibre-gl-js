23134e7801a508b88f3bfe8a2a50de4ea4c889d7
Kanahiro <mediterranean1769@gmail.com>
2022-06-17 23:56:20 +0900

    format

--- test/integration/render/custom_layer_implementations.ts
+++ test/integration/render/custom_layer_implementations.ts
@@ -1,3 +1,5 @@
+import maplibregl from '../../../src/index';
+
 class NullIsland {
     id: string;
     type: string;
@@ -139,37 +141,44 @@ class TileSource {
     minzoom?: number;
     maxzoom?:number;
 
-    program: WebGLProgram & {
-        a_pos?: number;
-        aPos?: number;
-        uMatrix?:  WebGLUniformLocation;
-    };
+    program: WebGLProgram;
     vertexBuffer: WebGLBuffer;
-    indexBuffer: WebGLBuffer;
-    
+    texCoordBuffer: WebGLBuffer;
+
+    aPos?: number;
+    aTexCoord?: number;
+    uMatrix?:  WebGLUniformLocation;
+    uTexture?:  WebGLUniformLocation;
+
     constructor() {
         this.id = 'tile-source';
         this.type = 'custom';
         this.renderingMode = '3d';
-        this.source = 'gsi';
+        this.source = 'satellite';
         this.minzoom = 2;
-        this.maxzoom = 5;
+        this.maxzoom = 17;
     }
 
-    onAdd(map, gl: WebGLRenderingContext) {
+    onAdd(map, gl) {
         const vertexSource = `
 
         attribute vec3 aPos;
+        attribute vec2 aTexCoord;
         uniform mat4 uMatrix;
+        varying vec2 vTexCoord;
 
         void main() {
             gl_Position = uMatrix * vec4(aPos, 1.0);
+            vTexCoord = aTexCoord;
         }
         `;
 
         const fragmentSource = `
+        precision mediump float;
+        uniform sampler2D uTexture;
+        varying vec2 vTexCoord;
         void main() {
-            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+            gl_FragColor = texture2D(uTexture, vTexCoord);
         }
         `;
 
@@ -184,46 +193,78 @@ class TileSource {
         gl.attachShader(this.program, vertexShader);
         gl.attachShader(this.program, fragmentShader);
         gl.linkProgram(this.program);
-        gl.validateProgram(this.program);
 
-        this.program.aPos = gl.getAttribLocation(this.program, 'aPos');
-        this.program.uMatrix = gl.getUniformLocation(this.program, 'uMatrix');
+        this.aPos = gl.getAttribLocation(this.program, 'aPos');
+        this.aTexCoord = gl.getAttribLocation(
+            this.program,
+            'aTexCoord',
+        );
+        this.uMatrix = gl.getUniformLocation(
+            this.program,
+            'uMatrix',
+        );
+        this.uTexture = gl.getUniformLocation(
+            this.program,
+            'uTexture',
+        );
 
-        const x = 0.5 - 0.015;
-        const y = 0.5 - 0.01;
-        const z = 0.01;
-        const d = 0.01;
-
+        const {x, y} = maplibregl.MercatorCoordinate.fromLngLat({
+            lng: 13.418056,
+            lat: 52.499167,
+        });
         const vertexArray = new Float32Array([
-            x, y, 0,
-            x + d, y, 0,
-            x, y + d, z,
-            x + d, y + d, z,
-            x, y + d + d, 0,
-            x + d, y + d + d, 0]);
-        const indexArray = new Uint16Array([
-            0, 1, 2,
-            1, 2, 3,
-            2, 3, 4,
-            3, 4, 5
+            x - 0.000005,
+            y - 0.000003,
+            0,
+            x + 0.000005,
+            y - 0.000003,
+            0,
+            x,
+            y + 0.000003,
+            0,
+        ]);
+        const texCoordArray = new Float32Array([
+            0.0, 0.0, 1.0, 0.0, 0.0, 1.0,
         ]);
 
+        // bind vertex
         this.vertexBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, vertexArray, gl.STATIC_DRAW);
-        this.indexBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
-        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indexArray, gl.STATIC_DRAW);
+        gl.enableVertexAttribArray(this.aPos);
+        gl.vertexAttribPointer(this.aPos, 3, gl.FLOAT, false, 0, 0);
+
+        // bind texCoord
+        this.texCoordBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.texCoordBuffer);
+        gl.bufferData(
+            gl.ARRAY_BUFFER,
+            texCoordArray,
+            gl.STATIC_DRAW,
+        );
+        gl.enableVertexAttribArray(this.aTexCoord);
+        gl.vertexAttribPointer(
+            this.aTexCoord,
+            2,
+            gl.FLOAT,
+            false,
+            0,
+            0,
+        );
     }
 
     render(gl: WebGLRenderingContext, matrix, tiles) {
+        if (tiles.length === 0) return;
         gl.useProgram(this.program);
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
-        gl.enableVertexAttribArray(this.program.a_pos);
-        gl.vertexAttribPointer(this.program.aPos, 3, gl.FLOAT, false, 0, 0);
-        gl.uniformMatrix4fv(this.program.uMatrix, false, matrix);
-        gl.drawElements(gl.TRIANGLES, 12, gl.UNSIGNED_SHORT, 0);
+
+        // bind texture
+        const {texture} = tiles[0];
+        gl.bindTexture(gl.TEXTURE_2D, texture.texture);
+        gl.uniform1i(this.uTexture, 0);
+
+        // render
+        gl.uniformMatrix4fv(this.uMatrix, false, matrix);
+        gl.drawElements(gl.TRIANGLES, 3, gl.UNSIGNED_SHORT, 0);
     }
 }
 
