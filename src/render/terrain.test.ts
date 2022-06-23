a28a22831463d6f543dfbc23b1105de0e074feaf
Matt Riggott <flother@users.noreply.github.com>
2022-06-23 05:31:39 +0000

    Add unit test for calculating min/max elevation (#1316)
    * Add unit test for calculating min/max elevation
    
    * Use correct method name
    
    * Move min/max elevation value calc to Terrain class
    
    * Alter Terrain.getMinMaxElevation to take tileID arg

--- src/render/terrain.test.ts
+++ src/render/terrain.test.ts
@@ -7,7 +7,10 @@ import Texture from './texture';
 import type Style from '../style/style';
 import type SourceCache from '../source/source_cache';
 import type TerrainSourceCache from '../source/terrain_source_cache';
+import {OverscaledTileID} from '../source/tile_id';
 import type {TerrainSpecification} from '../style-spec/types.g';
+import type DEMData from '../data/dem_data';
+import Tile from '../source/tile';
 
 describe('Terrain', () => {
     test('pointCoordiate should not return null', () => {
@@ -49,5 +52,100 @@ describe('Terrain', () => {
         const coordinate = terrain.pointCoordinate(new Point(0, 0));
 
         expect(coordinate).not.toBeNull();
+    });
+
+    test('Calculate tile minimum and maximum elevation', () => {
+        const tileID = new OverscaledTileID(5, 0, 5, 17, 11);
+        const tile = new Tile(tileID, 256);
+        tile.dem = {
+            min: 0,
+            max: 100,
+            getPixels: () => new RGBAImage({width: 1, height: 1}, new Uint8Array(1 * 4)),
+            getUnpackVector: () => [6553.6, 25.6, 0.1, 10000.0],
+        } as any as DEMData;
+        const style = {
+            map: {
+                painter: {
+                    context: new Context(gl(1, 1)),
+                    width: 1,
+                    height: 1,
+                    getTileTexture: () => null
+                }
+            }
+        } as any as Style;
+        const sourceCache = {
+            _source: {maxzoom: 12},
+            getTileByID: () => {
+                return tile;
+            },
+        } as any as SourceCache;
+        const terrain = new Terrain(
+            style,
+            sourceCache,
+            {exaggeration: 2, elevationOffset: 50} as any as TerrainSpecification,
+        );
+
+        terrain.sourceCache._tiles[tileID.key] = tile;
+        const {minElevation, maxElevation} = terrain.getMinMaxElevation(tileID);
+
+        expect(minElevation).toBe(100);
+        expect(maxElevation).toBe(300);
+    });
+
+    test('Return null elevation values when no tile', () => {
+        const tileID = new OverscaledTileID(5, 0, 5, 17, 11);
+        const style = {
+            map: {
+                painter: {
+                    context: new Context(gl(1, 1)),
+                    width: 1,
+                    height: 1,
+                }
+            }
+        } as any as Style;
+        const sourceCache = {
+            _source: {maxzoom: 12},
+            getTileByID: () => null,
+        } as any as SourceCache;
+        const terrain = new Terrain(
+            style,
+            sourceCache,
+            {exaggeration: 2, elevationOffset: 50} as any as TerrainSpecification,
+        );
+
+        const minMaxNoTile = terrain.getMinMaxElevation(tileID);
+
+        expect(minMaxNoTile.minElevation).toBeNull();
+        expect(minMaxNoTile.maxElevation).toBeNull();
+    });
+
+    test('Return null elevation values when no DEM', () => {
+        const tileID = new OverscaledTileID(5, 0, 5, 17, 11);
+        const tile = new Tile(tileID, 256);
+        tile.dem = null as any as DEMData;
+        const style = {
+            map: {
+                painter: {
+                    context: new Context(gl(1, 1)),
+                    width: 1,
+                    height: 1,
+                }
+            }
+        } as any as Style;
+        const sourceCache = {
+            _source: {maxzoom: 12},
+            getTileByID: () => {
+                return tile;
+            },
+        } as any as SourceCache;
+        const terrain = new Terrain(
+            style,
+            sourceCache,
+            {exaggeration: 2, elevationOffset: 50} as any as TerrainSpecification,
+        );
+        const minMaxNoDEM = terrain.getMinMaxElevation(tileID);
+
+        expect(minMaxNoDEM.minElevation).toBeNull();
+        expect(minMaxNoDEM.maxElevation).toBeNull();
     });
 });
