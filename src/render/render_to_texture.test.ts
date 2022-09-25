c444a79324b7a35caf4ff342c1475c698a2941aa
HarelM <harel.mazor@gmail.com>
2022-09-25 15:00:01 +0300

    Fix some more tests

--- src/render/render_to_texture.test.ts
+++ src/render/render_to_texture.test.ts
@@ -3,6 +3,7 @@ import type Painter from './painter';
 import type LineStyleLayer from '../style/style_layer/line_style_layer';
 import type SymbolStyleLayer from '../style/style_layer/symbol_style_layer';
 import type Terrain from './terrain';
+import type Style from '../style/style';
 
 describe('render to texture', () => {
     test('should render text after a line by not adding the text to the stack', () => {
@@ -13,20 +14,26 @@ describe('render to texture', () => {
             },
             clearRerenderCache: () => {}
         } as any as Terrain;
+        const style = {
+            map: {
+                terrain
+            },
+            _order: []
+        } as any as Style;
         const painterMock = {
-            style: {
-                map: {
-                    terrain
-                },
-                _order: []
+            style,  
+            transform: {
+                zoom: 10
             }
         } as any as Painter;
         const uut = new RenderToTexture(painterMock, terrain);
+        uut.initialize(style, 10);
         const lineLayer = {
             id: 'maine-line',
             type: 'line',
-            source: 'maine'
+            source: 'maine',
         } as LineStyleLayer;
+        lineLayer.isHidden = () => false;
         const symbolLayer = {
             id: 'maine-text',
             type: 'symbol',
@@ -34,9 +41,9 @@ describe('render to texture', () => {
             layout: {
                 'text-field': 'maine',
                 'symbol-placement': 'line'
-            }
+            },
+            isHidden: () => false
         } as any as SymbolStyleLayer;
-
         expect(uut.renderLayer(lineLayer)).toBeTruthy();
         painterMock.style._order = ['maine-line', 'maine-text'];
         painterMock.currentLayer = 1;
