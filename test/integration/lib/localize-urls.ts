e50e8151b523ccab393a6d7bc99c4d0588c5e42f
Matthew Mamonov <g.smellyshovel@gmail.com>
2022-11-04 19:09:10 +0400

    Revert "Try without removing, but only adapting for the new sprite spec"
    This reverts commit 17931fc36d9d47ba3c113314f056bd46403d0cc6.

--- test/integration/lib/localize-urls.ts
+++ test/integration/lib/localize-urls.ts
@@ -43,35 +43,16 @@ function localizeURL(url: string, port: number) {
     return url.replace(/^local:\/\//, `http://localhost:${port}/`);
 }
 
-function localizeMapboxSpriteURL(url: string, port: number) {
-    return url.replace(/^mapbox:\/\//, `http://localhost:${port}/`);
-}
-
-function localizeMapboxFontsURL(url: string, port: number) {
-    return url.replace(/^mapbox:\/\/fonts/, `http://localhost:${port}/glyphs`);
-}
-
-function localizeMapboxTilesURL(url: string, port: number) {
-    return url.replace(/^mapbox:\/\//, `http://localhost:${port}/tiles/`);
-}
-
-function localizeMapboxTilesetURL(url: string, port: number) {
-    return url.replace(/^mapbox:\/\//, `http://localhost:${port}/tilesets/`);
-}
-
 function localizeSourceURLs(source: any, port: number) {
     for (const tile in source.tiles) {
-        source.tiles[tile] = localizeMapboxTilesURL(source.tiles[tile], port);
         source.tiles[tile] = localizeURL(source.tiles[tile], port);
     }
 
     if (source.urls) {
-        source.urls = source.urls.map((url) => localizeMapboxTilesetURL(url, port));
         source.urls = source.urls.map((url) => localizeURL(url, port));
     }
 
     if (source.url) {
-        source.url = localizeMapboxTilesetURL(source.url, port);
         source.url = localizeURL(source.url, port);
     }
 
@@ -87,19 +68,15 @@ function localizeStyleURLs(style: StyleSpecification, port: number) {
 
     if (style.sprite) {
         if (typeof style.sprite === 'string') {
-            style.sprite = localizeMapboxSpriteURL(style.sprite, port);
             style.sprite = localizeURL(style.sprite, port);
         } else if (Array.isArray(style.sprite)) {
             for (const sprite of style.sprite) {
-                sprite.url = localizeMapboxSpriteURL(sprite.url, port);
                 sprite.url = localizeURL(sprite.url, port);
             }
         }
-
     }
 
     if (style.glyphs) {
-        style.glyphs = localizeMapboxFontsURL(style.glyphs, port);
         style.glyphs = localizeURL(style.glyphs, port);
     }
 }
