607ec52cb0988c1a6c2ae45525f1b4912bf21db2
Yi Zhang (BING) <zhangyi@microsoft.com>
2022-12-05 14:09:15 -0800

    make sure reset

--- test/integration/render/render.test.ts
+++ test/integration/render/render.test.ts
@@ -496,11 +496,11 @@ function getImageFromStyle(style: StyleWithTestData): Promise<Uint8Array> {
  */
 function printProgress(test: TestData, total: number, index: number) {
     if (test.error) {
-        console.log('\x1b[31m', `${index}/${total}: errored ${test.id} ${test.error.message}`);
+        console.log('\x1b[31m', `${index}/${total}: errored ${test.id} ${test.error.message}`, '\x1b[0m');
     } else if (!test.ok) {
-        console.log('\x1b[31m', `${index}/${total}: failed ${test.id} ${test.difference}`);
+        console.log('\x1b[31m', `${index}/${total}: failed ${test.id} ${test.difference}`, '\x1b[0m');
     } else {
-        console.log('\x1b[0m', `${index}/${total}: passed ${test.id}`);
+        console.log(`${index}/${total}: passed ${test.id}`);
     }
 }
 
