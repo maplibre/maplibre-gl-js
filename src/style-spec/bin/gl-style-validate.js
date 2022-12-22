0e4bfc006fb0fde32b3fbe4720395ad248583e21
acalcutt <acalcutt@techidiots.net>
2022-12-21 20:14:31 -0500

    fix call to minimist

--- src/style-spec/bin/gl-style-validate.js
+++ src/style-spec/bin/gl-style-validate.js
@@ -5,7 +5,7 @@ import minimist from 'minimist';
 import rw from 'rw';
 import {validate} from '@maplibre/maplibre-gl-style-spec';
 
-const argv = minimist('minimist')(process.argv.slice(2), {
+const argv = minimist(process.argv.slice(2), {
     boolean: 'json',
 });
 let status = 0;
