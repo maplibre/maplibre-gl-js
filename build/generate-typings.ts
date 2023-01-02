5b260d23e11dbbff742fd9f9188ebdea8e71b6a3
Dan <DanOfTheRoses@gmail.com>
2023-01-01 23:26:48 -0600

    inline types into d.ts
    also move all non-runtime dependencies to devDependencies

--- build/generate-typings.ts
+++ build/generate-typings.ts
@@ -8,11 +8,11 @@ if (!fs.existsSync('dist')) {
 
 console.log('Starting bundling types');
 const outputFile = './dist/maplibre-gl.d.ts';
-childProcess.execSync(`dts-bundle-generator --umd-module-name=maplibregl -o ${outputFile} ./src/index.ts`);
+childProcess.execSync(`dts-bundle-generator --umd-module-name=maplibregl -o ${outputFile} --config ./build/dts-bundle-generator-config.json ./src/index.ts`);
 let types = fs.readFileSync(outputFile, 'utf8');
 // Classes are not exported but should be since this is exported as UMD - fixing...
 types = types.replace(/declare class/g, 'export declare class');
 fs.writeFileSync(outputFile, types);
 
-console.log('Finifhed bundling types for maplibre-gl ');
+console.log('Finished bundling types for maplibre-gl ');
 
