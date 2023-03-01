498f72cafea526bcbf59ffe39ad9e200ce34a2bb
David Schlosnagle <davids@palantir.com>
2023-03-01 16:58:18 -0500

    shouldPausePlacement short circuits when _forceFullPlacement

--- src/style/pauseable_placement.ts
+++ src/style/pauseable_placement.ts
@@ -97,8 +97,10 @@ class PauseablePlacement {
         const startTime = browser.now();
 
         const shouldPausePlacement = () => {
-            const elapsedTime = browser.now() - startTime;
-            return this._forceFullPlacement ? false : elapsedTime > 2;
+            if (this._forceFullPlacement) {
+                return false;
+            }
+            return browser.now() - startTime > 2;
         };
 
         while (this._currentPlacementIndex >= 0) {
