2ea92d66ce79be34681a13be75f782f69ead4490
Yi Zhang <zhangyi@microsoft.com>
2023-04-04 12:16:52 -0700

    coerceSpriteToArray should dedup

--- src/util/style.ts
+++ src/util/style.ts
@@ -3,10 +3,26 @@ import {SpriteSpecification} from '@maplibre/maplibre-gl-style-spec';
 /**
  * Takes a SpriteSpecification value and returns it in its array form. If `undefined` is passed as an input value, an
  * empty array is returned.
- *
+ * duplicated entries with identical id/url will be removed in returned array
  * @param [sprite] {SpriteSpecification} optional sprite to coerce
  * @returns {Array} an empty array in case `undefined` is passed; id-url pairs otherwise
  */
 export function coerceSpriteToArray(sprite?: SpriteSpecification): {id: string; url: string}[] {
-    return typeof sprite === 'string' ? [{id: 'default', url: sprite}] : (sprite ?? []);
+    const resultArray: {id: string; url: string}[]  = [];
+
+    if (typeof sprite === 'string') {
+        resultArray.push({id: 'default', url: sprite});
+    } else if (sprite && sprite.length > 0) {
+        const dedupArray: string[] = [];
+        for (const {id, url} of sprite) {
+            const key = `${id}${url}`;
+            if (dedupArray.indexOf(key) === -1) {
+                dedupArray.push(key);
+                resultArray.push({id, url});
+            }
+        }
+    }
+
+    return resultArray;
+
 }
