eab560659d77875936e7869c9ac3ec9d887b8393
Yi Zhang (BING) <zhangyi@microsoft.com>
2023-04-04 19:27:28 -0700

    save

--- timerStart.ts
+++ timerStart.ts
@@ -3,9 +3,12 @@ import fs from 'fs';
 const timingFile = './staging/.timing';
 const start = Date.now();
 
-if (!fs.existsSync('./staing/')) {
+if (!fs.existsSync('./staging')) {
     fs.mkdirSync('./staging');
 }
 
+if (fs.existsSync(timingFile)) {
+    fs.unlinkSync(timingFile);
+}
 fs.writeFileSync(timingFile, `${start}`, 'utf8');
 console.log('Start time:', (new Date()).toLocaleTimeString());
