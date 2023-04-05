9829d2a4e78f8284a49eaa2be62507444aacd11b
Yi Zhang <zhangyi@microsoft.com>
2023-04-04 21:37:03 -0700

    override

--- timerStart.ts
+++ timerStart.ts
@@ -7,8 +7,5 @@ if (!fs.existsSync('./staging')) {
     fs.mkdirSync('./staging');
 }
 
-if (fs.existsSync(timingFile)) {
-    fs.unlinkSync(timingFile);
-}
 fs.writeFileSync(timingFile, `${start}`, 'utf8');
 console.log('Start time:', (new Date()).toLocaleTimeString());
