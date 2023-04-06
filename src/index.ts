7de36ad524a3caf14515a2753366d75591e84be0
Pramil Kuchhal <pramik@microsoft.com>
2023-04-06 15:15:20 -0700

    Update Comments

--- src/index.ts
+++ src/index.ts
@@ -109,7 +109,7 @@ const exported = {
 
     /**
      * Gets and sets the number of web workers instantiated on a page with GL JS maps.
-     * By default, it is set to half the number of CPU cores (capped at 6).
+     * By default, it is set to half the number of CPU cores (capped at 2).
      * Make sure to set this property before creating any map instances for it to have effect.
      *
      * @var {string} workerCount
