c471afe0f79acf6bbcda1907f28d66033e37bc10
Birk Skyum <birk.skyum@pm.me>
2023-05-26 21:48:53 +0200

    Lint circle

--- src/shaders/circle.fragment.glsl
+++ src/shaders/circle.fragment.glsl
@@ -9,8 +9,6 @@ in float v_visibility;
 #pragma mapbox: define mediump float stroke_width
 #pragma mapbox: define lowp float stroke_opacity
 
-
-
 void main() {
     #pragma mapbox: initialize highp vec4 color
     #pragma mapbox: initialize mediump float radius
@@ -28,11 +26,7 @@ void main() {
 
     float opacity_t = smoothstep(0.0, antialiased_blur, extrude_length - 1.0);
 
-    float color_t = stroke_width < 0.01 ? 0.0 : smoothstep(
-        antialiased_blur,
-        0.0,
-        extrude_length - radius / (radius + stroke_width)
-    );
+    float color_t = stroke_width < 0.01 ? 0.0 : smoothstep(antialiased_blur, 0.0, extrude_length - radius / (radius + stroke_width));
 
     fragColor = v_visibility * opacity_t * mix(color * opacity, stroke_color * stroke_opacity, color_t);
 
