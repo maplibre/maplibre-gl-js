834f5d57d2b5a5a6fed70a81a291fe99e9d51b80
HarelM <harel.mazor@gmail.com>
2023-07-05 21:56:04 +0300

    Fix typo

--- src/source/video_source.ts
+++ src/source/video_source.ts
@@ -37,7 +37,7 @@ import type {VideoSourceSpecification} from '@maplibre/maplibre-gl-style-spec';
  * });
  *
  * // update
- * letmySource = map.getSource('some id');
+ * let mySource = map.getSource('some id');
  * mySource.setCoordinates([
  *     [-76.54335737228394, 39.18579907229748],
  *     [-76.52803659439087, 39.1838364847587],
