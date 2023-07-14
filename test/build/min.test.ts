8c9fc1b3fa82bfee681c51c8b533fca07756ce0e
HarelM <harel.mazor@gmail.com>
2023-07-14 22:56:05 +0300

    Fix build test

--- test/build/min.test.ts
+++ test/build/min.test.ts
@@ -36,7 +36,7 @@ describe('test min build', () => {
         const decreaseQuota = 4096;
 
         // feel free to update this value after you've checked that it has changed on purpose :-)
-        const expectedBytes = 761600;
+        const expectedBytes = 762000;
 
         expect(actualBytes - expectedBytes).toBeLessThan(increaseQuota);
         expect(expectedBytes - actualBytes).toBeLessThan(decreaseQuota);
