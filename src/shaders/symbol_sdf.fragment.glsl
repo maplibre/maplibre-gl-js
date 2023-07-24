eb8e1a1fd14b155bc8195b65342e575c2403a61c
Chris Loer <chris@felt.com>
2023-07-24 15:54:08 -0700

    Move the halo "in" a bit so that the edge of the text has a solid color to draw on.

--- src/shaders/symbol_sdf.fragment.glsl
+++ src/shaders/symbol_sdf.fragment.glsl
@@ -33,15 +33,16 @@ void main() {
 
     lowp vec4 color = fill_color;
     highp float gamma = EDGE_GAMMA / (fontScale * u_gamma_scale);
-    lowp float text_edge = (256.0 - 64.0) / 256.0;
+    lowp float inner_edge = (256.0 - 64.0) / 256.0;
     if (u_is_halo) {
         color = halo_color;
         gamma = (halo_blur * 1.19 / SDF_PX + EDGE_GAMMA) / (fontScale * u_gamma_scale);
+        inner_edge = inner_edge + gamma * gamma_scale;
     }
 
     lowp float dist = texture(u_texture, tex).a;
     highp float gamma_scaled = gamma * gamma_scale;
-    highp float alpha = smoothstep(text_edge - gamma_scaled, text_edge + gamma_scaled, dist);
+    highp float alpha = smoothstep(inner_edge - gamma_scaled, inner_edge + gamma_scaled, dist);
     if (u_is_halo) {
         // When drawing halos, we want the inside of the halo to be transparent as well
         // in case the text fill is transparent.
