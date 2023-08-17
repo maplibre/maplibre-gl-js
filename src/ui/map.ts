39f4b16cf27303aa995c50defe066768d9893981
Craig O'Connor <5249140+CraigglesO@users.noreply.github.com>
2023-08-17 06:37:43 -0400

    Update map.ts
    switch from undefined to null

--- src/ui/map.ts
+++ src/ui/map.ts
@@ -1963,13 +1963,13 @@ export class Map extends Camera {
      * map.setTerrain({ source: 'terrain' });
      * ```
      */
-    setTerrain(options?: TerrainSpecification): this {
+    setTerrain(options: TerrainSpecification | null): this {
         this.style._checkLoaded();
 
         // clear event handlers
         if (this._terrainDataCallback) this.style.off('data', this._terrainDataCallback);
 
-        if (options === undefined) {
+        if (options === null) {
             // remove terrain
             if (this.terrain) this.terrain.sourceCache.destruct();
             this.terrain = null;
