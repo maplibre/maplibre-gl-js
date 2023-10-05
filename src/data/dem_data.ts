f44ebefcf750d6c2e7619ce4ff827141581db3ad
Isaac Besora <isaac.besora@gmail.com>
2023-10-05 11:57:32 +0200

    Remove rounding

--- src/data/dem_data.ts
+++ src/data/dem_data.ts
@@ -112,9 +112,7 @@ export class DEMData {
     }
 
     unpack(r: number, g: number, b: number) {
-        const value = (r * this.redFactor + g * this.greenFactor + b * this.blueFactor - this.baseShift);
-        const significantDigits = 5;
-        return Math.round(value * Math.pow(10, significantDigits) / Math.pow(10, significantDigits));
+        return (r * this.redFactor + g * this.greenFactor + b * this.blueFactor - this.baseShift);
     }
 
     getPixels() {
