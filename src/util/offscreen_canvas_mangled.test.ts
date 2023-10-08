7edf4051239304bca9ecbbf30e6fd36d415f4306
Mike Barry <msb5014@gmail.com>
2023-10-07 20:57:05 -0400

    fix mangle test

--- src/util/offscreen_canvas_mangled.test.ts
+++ src/util/offscreen_canvas_mangled.test.ts
@@ -1,10 +1,13 @@
 import {offscreenCanvasMangled} from './offscreen_canvas_mangled';
 import {Canvas} from 'canvas';
+import {offscreenCanvasSupported} from './offscreen_canvas_supported';
 
 test('normal operation does not mangle canvas', () => {
     const OffscreenCanvas = (window as any).OffscreenCanvas = jest.fn((width:number, height: number) => {
         return new Canvas(width, height);
     });
+    expect(offscreenCanvasSupported()).toBeTruthy();
+    OffscreenCanvas.mockClear();
     expect(offscreenCanvasMangled()).toBeFalsy();
-    expect(OffscreenCanvas).toHaveBeenCalledTimes(2);
+    expect(OffscreenCanvas).toHaveBeenCalledTimes(1);
 });
