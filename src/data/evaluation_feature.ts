76164a89b732d6d51344c577f76a4d9be3ac3723
Harel M <harel.mazor@gmail.com>
2023-10-23 15:51:08 +0300

    Improve geojson source typings (#3184)
    * Add geojsonvt types package
    
    * improve typings
    
    * Fix lint
    
    * Update packages to fix types

--- src/data/evaluation_feature.ts
+++ src/data/evaluation_feature.ts
@@ -1,21 +1,9 @@
 import {loadGeometry} from './load_geometry';
 import type Point from '@mapbox/point-geometry';
 import type {VectorTileFeature} from '@mapbox/vector-tile';
+import type {Feature} from '@maplibre/maplibre-gl-style-spec';
 
-type EvaluationFeature = {
-    readonly type: 1 | 2 | 3 | 'Unknown' | 'Point' | 'MultiPoint' | 'LineString' | 'MultiLineString' | 'Polygon' | 'MultiPolygon';
-    readonly id?: any;
-    readonly properties: {[_: string]: any};
-    readonly patterns?: {
-        [_: string]: {
-            'min': string;
-            'mid': string;
-            'max': string;
-        };
-    };
-    geometry: Array<Array<Point>>;
-};
-
+type EvaluationFeature = Feature & { geometry: Array<Array<Point>> };
 /**
  * Construct a new feature based on a VectorTileFeature for expression evaluation, the geometry of which
  * will be loaded based on necessity.
