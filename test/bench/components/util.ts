727e9deadd499b740832189efa884922f1056f03
Harel M <harel.mazor@gmail.com>
2023-11-07 11:34:57 +0000

    Added missing type

--- test/bench/components/util.ts
+++ test/bench/components/util.ts
@@ -1,5 +1,6 @@
 import * as d3 from 'd3';
 import {RegressionResults, Summary} from '../lib/statistics';
+import Benchmark from '../lib/benchmark';
 
 export type Version = {
     name: string;
@@ -9,6 +10,7 @@ export type Version = {
     summary: Summary;
     regression: RegressionResults;
     error: Error;
+    bench: Benchmark;
 }
 
 export const versionColor = d3.scaleOrdinal(['#1b9e77', '#7570b3', '#d95f02']);
