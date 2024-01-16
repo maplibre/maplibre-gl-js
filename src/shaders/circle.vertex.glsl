bf1c6edb7779784d8556a86a56083952f118e7b2
Jakub Pelc <jakub.pelc@email.cz>
2024-01-16 21:48:53 +0100

    Circle layer: proper handling of all 4 alignemnt+scale settings combinations

--- src/shaders/circle.vertex.glsl
+++ src/shaders/circle.vertex.glsl
@@ -18,6 +18,23 @@ out float v_visibility;
 #pragma mapbox: define mediump float stroke_width
 #pragma mapbox: define lowp float stroke_opacity
 
+// Interaction of globe (3D) with circle-pitch-alignment + circle-pitch-scale.
+// This is a summary of behaviour, where "v" stands for "viewport" and "m" for map.
+// First letter is alignment, second is scale.
+// v+v:
+//   2D: circles have constant screenspace size
+//   3D: same as 2D
+// v+m:
+//   2D: circles have constant screenspace size, far away circles are shrunk, like with perspective projection (only has effect if map is pitched)
+//   3D: same as 2D
+// m+v:
+//   2D: circles "printed" onto map surface, far away circles are enlarged (counteracts shirnking due to perspective projection)
+//   3D: circles "printed" onto map surface, far away circles are enlarged (implemented but questionable whether it does anything)
+// m+m:
+//   2D: circles "printed" onto map surface, far away circles are naturally smaller due to perspective projection
+//   3D: circles "printed" onto globe surface, far away circles are naturally smaller due to perspective projectionp
+// JP: TODO: put this into style specs
+
 void main(void) {
     #pragma mapbox: initialize highp vec4 color
     #pragma mapbox: initialize mediump float radius
@@ -37,16 +54,9 @@ void main(void) {
     v_visibility = calculate_visibility(projectTileWithElevation(vec3(circle_center, ele)));
 
     if (u_pitch_with_map) {
-#ifdef GLOBE
         vec3 center_vector = projectToSphere(circle_center);
 
-        // Default axis for vertical rotation
-        vec3 axis = vec3(-center_vector.z, 0.0, center_vector.x); // Equivalent to cross(center_vector, vec3(0.0, 1.0, 0.0))
-        if ((extrude.x > 0.0) != (extrude.y > 0.0)) {
-            // Move corner horizontally instead of vertically
-            axis = cross(center_vector, axis);
-        }
-        axis = normalize(axis);
+        // This var is only used when globe is enabled and defined.
         float angle = (extrude.x > 0.0) ? u_globe_extrude_scale : -u_globe_extrude_scale; // TODO maybe wrong sign
 
         // Keep track of "2D" corner position to allow smooth interpolation between globe and mercator
@@ -59,25 +69,23 @@ void main(void) {
             // To counteract the effect for pitch-scale: viewport, we rescale the
             // whole circle based on the pitch scaling effect at its central point
             vec4 projected_center = interpolateProjection(circle_center, center_vector);
-            angle *= (radius + stroke_width) * (projected_center.w / u_camera_to_center_distance);
             corner_position += extrude * u_extrude_scale * (radius + stroke_width) * (projected_center.w / u_camera_to_center_distance);
+            angle *= (radius + stroke_width) * (projected_center.w / u_camera_to_center_distance);
         }
 
+#ifdef GLOBE
+        // Default axis for vertical rotation
+        vec3 axis = vec3(-center_vector.z, 0.0, center_vector.x); // Equivalent to cross(center_vector, vec3(0.0, 1.0, 0.0))
+        if ((extrude.x > 0.0) != (extrude.y > 0.0)) {
+            // Move corner horizontally instead of vertically
+            axis = cross(center_vector, axis);
+        }
+        axis = normalize(axis);
+        
         mat3 m = rotationMatrixFromAxisAngle(axis, angle);
         vec3 corner_vector = m * center_vector;
-
         gl_Position = interpolateProjection(circle_center, corner_vector);
 #else
-        vec2 corner_position = circle_center;
-        if (u_scale_with_map) {
-            corner_position += extrude * u_extrude_scale * (radius + stroke_width);
-        } else {
-            // Pitching the circle with the map effectively scales it with the map
-            // To counteract the effect for pitch-scale: viewport, we rescale the
-            // whole circle based on the pitch scaling effect at its central point
-            vec4 projected_center = projectTile(circle_center);
-            corner_position += extrude * u_extrude_scale * (radius + stroke_width) * (projected_center.w / u_camera_to_center_distance);
-        }
         gl_Position = projectTileWithElevation(vec3(corner_position, ele));
 #endif
     } else {
