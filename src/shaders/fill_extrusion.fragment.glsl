4559ac4403410deae21ac289796254ce8a41ba22
Jakub Pelc <jakub.pelc@email.cz>
2024-02-16 13:34:21 +0100

    Fill extrusion: add comment in fragment shader about planet occlusion

--- src/shaders/fill_extrusion.fragment.glsl
+++ src/shaders/fill_extrusion.fragment.glsl
@@ -13,7 +13,19 @@ void main() {
 #endif
 
 #ifdef GLOBE
-    // Discard fragments that are occluded by the planet
+    // We want extruded geometry to be occluded by the planet.
+    // This would be trivial in any traditional 3D renderer with Z-buffer,
+    // but not in MapLibre, since Z-buffer is used to mask certain layers
+    // and optimize overdraw.
+    // One solution would be to draw the planet into Z-buffer just before
+    // rendering fill-extrusion layers, but what if another layer
+    // is drawn after that which makes use of this Z-buffer mask?
+    // We can't just trash the mask with out own Z values.
+    // So instead, the "Z-test" against the planet is done here,
+    // in the pixel shader.
+    // Luckily the planet is (assumed to be) a perfect sphere,
+    // so the ray-planet intersection test is quite simple.
+    // We discard any fragments that are occluded by the planet.
     vec3 toPlanetCenter = -v_sphere_pos;
     vec3 toCameraNormalized = normalize(u_camera_pos_globe - v_sphere_pos);
     float t = dot(toPlanetCenter, toCameraNormalized);
