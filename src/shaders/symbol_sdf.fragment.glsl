9c3ebd385fc4f1a71123a6855715506ae3c111ed
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2024-02-29 13:54:07 +0100

    Add icon example

--- src/shaders/symbol_sdf.fragment.glsl
+++ src/shaders/symbol_sdf.fragment.glsl
@@ -59,13 +59,13 @@ void main() {
     
     vec3 s = texture(u_texture, tex).rgb;
     float sd = median(s.r, s.g, s.b);
-    vec4 sdfParams = vec4(1024.0, 1024.0, 32.0, 8.0);
-    vec2 dxdy = fwidth(tex) * sdfParams.xy;
-    float dist = sd + min(0.001, 0.5 - 1.0 / sdfParams.w) - 0.5;
-    float opacity_openglobus = clamp(dist * sdfParams.w / length(dxdy) + 0.5, 0.0, 1.0);
-    if(opacity_openglobus <= 0.1){
-        discard;
-    }
+    // vec4 sdfParams = vec4(1024.0, 1024.0, 32.0, 8.0);
+    // vec2 dxdy = fwidth(tex) * sdfParams.xy;
+    float dist = (sd - 0.5) * 6.0 * size;
+    float opacity_openglobus = clamp(dist + 0.5, 0.0, 1.0);
+    // if(opacity_openglobus <= 0.1){
+    //     discard;
+    // }
 
     float alpha = opacity_openglobus; //clamp(sigDist * 100.0, 0.0, 1.0);
 
