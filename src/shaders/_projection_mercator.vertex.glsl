3cc71ca392f71d16b5414d3cf82fd7f27679a124
Jakub Pelc <jakub.pelc@email.cz>
2024-03-06 14:00:35 +0100

    Cleanup vertex shader projection interface

--- src/shaders/_projection_mercator.vertex.glsl
+++ src/shaders/_projection_mercator.vertex.glsl
@@ -6,6 +6,7 @@ float projectCircleRadius(float tileY) {
     return 1.0;
 }
 
+// Projects a point in tile-local coordinates (usually 0..EXTENT) to screen.
 vec4 projectTile(vec2 p) {
     // Kill pole vertices and triangles by placing the pole vertex so far in Z that
     // the clipping hardware kills the entire triangle.
@@ -21,18 +22,3 @@ vec4 projectTileWithElevation(vec2 posInTile, float elevation) {
     // so no need to detect them.
     return u_projection_matrix * vec4(posInTile, elevation, 1.0);
 }
-
-vec4 projectTileFor3D(vec2 posInTile, float elevation) {
-    return projectTileWithElevation(posInTile, elevation);
-}
-
-vec4 interpolateProjection(vec2 posInTile, vec3 spherePos, float elevation) {
-    return projectTileFor3D(posInTile, elevation);
-}
-
-vec4 interpolateProjectionFor3D(vec2 posInTile, vec3 spherePos, float elevation) {
-    return interpolateProjection(posInTile, spherePos, elevation);
-}
-
-#define projectToSphere(p) (vec3(0.0, 1.0, 0.0))
-#define getDebugColor(p) (vec4(1.0, 0.0, 1.0, 1.0))
