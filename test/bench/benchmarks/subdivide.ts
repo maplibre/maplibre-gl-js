66576dcd3d14e8e26c3b111886fa0c846093df59
Jakub Pelc <jakub.pelc@email.cz>
2024-03-25 09:30:44 +0100

    Fix subdivision benchmark

--- test/bench/benchmarks/subdivide.ts
+++ test/bench/benchmarks/subdivide.ts
@@ -1,14 +1,13 @@
-import {subdivideFill} from '../../../src/render/subdivision';
 import {CanonicalTileID} from '../../../src/source/tile_id';
 import Benchmark from '../lib/benchmark';
 import {EXTENT} from '../../../src/data/extent';
+import {subdivideFill} from '../../../src/render/subdivision';
+import Point from '@mapbox/point-geometry';
 
 export default class Subdivide extends Benchmark {
-    flattened: Array<number>;
-    holeIndices: Array<number>;
-    lineList: Array<Array<number>>;
     tileID: CanonicalTileID;
     granularity: number;
+    polygon: Array<Array<Point>>;
 
     async setup(): Promise<void> {
         await super.setup();
@@ -18,54 +17,43 @@ export default class Subdivide extends Benchmark {
         // granularity = 64
 
         const vertexCountMultiplier = 11;
-
         this.granularity = 64;
 
         // Use web mercator base tile, as it borders both north and south poles,
         // so we also benchmark pole geometry generation.
         this.tileID = new CanonicalTileID(2, 1, 1); // tile avoids north and south mercator edges
 
-        const vertices = [];
-        const holeIndices = [];
-        const lineList = [];
+        const polygon = [];
 
-        lineList.push(generateRing(EXTENT / 2, EXTENT / 2, EXTENT * 1.1 / 2, 81 * vertexCountMultiplier, vertices));
+        polygon.push(generateRing(EXTENT / 2, EXTENT / 2, EXTENT * 1.1 / 2, 81 * vertexCountMultiplier));
 
         // this function takes arguments in range 0..1, where 0 maps to 0 and 1 to EXTENT
         // this makes placing holes by hand easier
         function generateHole(cx: number, cy: number, r: number, vertexCount: number) {
-            holeIndices.push(vertices.length / 2);
-            lineList.push(generateRing(cx * EXTENT, cy * EXTENT, r * EXTENT, vertexCount, vertices));
+            polygon.push(generateRing(cx * EXTENT, cy * EXTENT, r * EXTENT, vertexCount));
         }
 
         generateHole(0.25, 0.5, 0.15, 16 * vertexCountMultiplier);
         generateHole(0.75, 0.5, 0.15, 2 * vertexCountMultiplier);
         generateHole(0.5, 0.1, 0.05, 4 * vertexCountMultiplier);
-
-        this.flattened = vertices;
-        this.holeIndices = holeIndices;
-        this.lineList = lineList;
     }
 
-    // async bench() {
-    //     subdivideFill(this.flattened, this.holeIndices, this.lineList, this.tileID, this.granularity);
-    // }
+    async bench() {
+        subdivideFill(this.polygon, this.tileID, this.granularity, true);
+    }
 }
 
-// Returns line indices for this ring
-function generateRing(cx: number, cy: number, radius: number, vertexCount: number, target: Array<number>): Array<number> {
-    const lineIndices = [];
-    const baseVertex = target.length / 2;
+function generateRing(cx: number, cy: number, radius: number, vertexCount: number): Array<Point> {
+    const ring = [];
 
     for (let i = 0; i < vertexCount; i++) {
         const angle = i / vertexCount * 2.0 * Math.PI;
         // round to emulate integer vertex coordinates
-        target.push(Math.round(cx + Math.cos(angle) * radius));
-        target.push(Math.round(cy + Math.sin(angle) * radius));
-
-        lineIndices.push(i + baseVertex);
-        lineIndices.push(((i + 1) % vertexCount) + baseVertex);
+        ring.push(new Point(
+            Math.round(cx + Math.cos(angle) * radius),
+            Math.round(cy + Math.sin(angle) * radius)
+        ));
     }
 
-    return lineIndices;
+    return ring;
 }
