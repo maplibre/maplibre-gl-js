830409e653273aa4136d4e7e5d8eae0a89130b4b
Jakub Pelc <jakub.pelc@email.cz>
2024-04-25 10:58:13 +0200

    Symbols: fix collision boxes with anchor behind horizon disappearing altogether

--- src/shaders/collision_box.vertex.glsl
+++ src/shaders/collision_box.vertex.glsl
@@ -10,6 +10,9 @@ out float v_notUsed;
 void main() {
     gl_Position = projectTileWithElevation(a_anchor_pos, get_elevation(a_anchor_pos));
     gl_Position.xy += (a_box_real * u_pixel_extrude_scale * 2.0) * vec2(1.0, -1.0) * gl_Position.w;
+    // Globe projection would set Z beyond visible range if the anchor point gets hidden behind the planet's horizon.
+    // Thus we need to force it to a visible value here.
+    gl_Position.z = 0.5;
 
     v_placed = a_placed.x;
     v_notUsed = a_placed.y;
