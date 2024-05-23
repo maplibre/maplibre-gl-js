f02103e9d2f87e4c7c8dc6d6d515570757309019
Jakub Pelc <jakub.pelc@email.cz>
2024-05-23 12:17:03 +0200

    Fix some failing unit tests

--- src/render/draw_symbol.test.ts
+++ src/render/draw_symbol.test.ts
@@ -15,6 +15,7 @@ import type {EvaluationParameters} from '../style/evaluation_parameters';
 import type {SymbolLayerSpecification} from '@maplibre/maplibre-gl-style-spec';
 import {Style} from '../style/style';
 import {MercatorProjection} from '../geo/projection/mercator';
+import {translatePosition} from '../geo/projection/mercator_transform';
 
 jest.mock('./painter');
 jest.mock('./program');
@@ -43,7 +44,7 @@ describe('drawSymbol', () => {
             }
         } as any;
         painterMock.renderPass = 'translucent';
-        painterMock.transform = {pitch: 0, labelPlaneMatrix: mat4.create()} as any as Transform;
+        painterMock.transform = createMockTransform();
         painterMock.options = {} as any;
         painterMock.style = {
             map: createMockMap()
@@ -81,12 +82,17 @@ describe('drawSymbol', () => {
         };
         const tile = new Tile(tileId, 256);
         tile.tileID = tileId;
+        console.log(tile.tileSize);
         tile.imageAtlasTexture = {
             bind: () => { }
         } as any;
         (tile.getBucket as jest.Mock).mockReturnValue(bucketMock);
+        tile.tileSize = 256;
+        console.log(tile.tileSize);
         const sourceCacheMock = new SourceCache(null, null, null);
+        console.log(tile.tileSize);
         (sourceCacheMock.getTile as jest.Mock).mockReturnValue(tile);
+        console.log(tile.tileSize);
         sourceCacheMock.map = {showCollisionBoxes: false} as any as Map;
 
         drawSymbols(painterMock, sourceCacheMock, layer, [tileId], null);
@@ -104,7 +110,7 @@ describe('drawSymbol', () => {
             }
         } as any;
         painterMock.renderPass = 'translucent';
-        painterMock.transform = {pitch: 0, labelPlaneMatrix: mat4.create()} as any as Transform;
+        painterMock.transform = createMockTransform();
         painterMock.options = {} as any;
 
         const layerSpec = {
@@ -170,7 +176,7 @@ describe('drawSymbol', () => {
             }
         } as any;
         painterMock.renderPass = 'translucent';
-        painterMock.transform = {pitch: 0, labelPlaneMatrix: mat4.create()} as any as Transform;
+        painterMock.transform = createMockTransform();
         painterMock.options = {} as any;
         painterMock.style = {
             map: createMockMap()
@@ -227,4 +233,26 @@ function createMockMap() {
     return {
         projection: new MercatorProjection()
     };
+}
+
+function createMockTransform() {
+    return {
+        pitch: 0,
+        labelPlaneMatrix: mat4.create(),
+        getCircleRadiusCorrection: () => 1,
+        angle: 0,
+        zoom: 0,
+        getProjectionData(_canonical, fallback) {
+            return {
+                'u_projection_matrix': fallback,
+                'u_projection_tile_mercator_coords': [0, 0, 1, 1],
+                'u_projection_clipping_plane': [0, 0, 0, 0],
+                'u_projection_transition': 0.0,
+                'u_projection_fallback_matrix': fallback,
+            };
+        },
+        translatePosition(tile: Tile, translate: [number, number], translateAnchor: 'map' | 'viewport'): [number, number] {
+            return translatePosition({angle: 0, zoom: 0}, tile, translate, translateAnchor);
+        }
+    } as any as Transform;
 }
