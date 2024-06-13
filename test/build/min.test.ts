48cdd323643add15bb3d2c738bda0f7653794884
HarelM <harel.mazor@gmail.com>
2024-06-13 22:43:14 +0300

    Increase build size

--- test/build/min.test.ts
+++ test/build/min.test.ts
@@ -36,7 +36,7 @@ describe('test min build', () => {
         const decreaseQuota = 4096;
 
         // feel free to update this value after you've checked that it has changed on purpose :-)
-        const expectedBytes = 833333;
+        const expectedBytes = 834444;
 
         expect(actualBytes).toBeLessThan(expectedBytes + increaseQuota);
         expect(actualBytes).toBeGreaterThan(expectedBytes - decreaseQuota);
