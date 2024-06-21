2cf86c329ff1c145aaa445f67f90686780c51fda
Jakub Pelc <jakub.pelc@email.cz>
2024-06-21 13:32:06 +0200

    Incorporate my own feedback

--- src/shaders/terrain.fragment.glsl
+++ src/shaders/terrain.fragment.glsl
@@ -11,9 +11,9 @@ in float v_fog_depth;
 void main() {
     vec4 color = texture2D(u_texture, v_texture_pos);
     if (v_fog_depth > u_fog_ground_blend) {
-        vec4 fog_horizon_color = mix(u_horizon_color, u_fog_color, 1.0 - u_horizon_fog_blend - v_fog_depth);
-        float a = (v_fog_depth - u_fog_ground_blend) / (1.0 - u_fog_ground_blend);
-        gl_FragColor = mix(color, fog_horizon_color, pow(a * u_fog_ground_blend_opacity, 2.0));
+        vec4 fog_horizon_color = mix(u_fog_color, u_horizon_color, max((v_fog_depth - u_horizon_fog_blend) / (1.0 - u_fog_ground_blend), 0));
+        float a = max((v_fog_depth - u_fog_ground_blend) / (1.0 - u_fog_ground_blend), 0);
+        gl_FragColor = mix(color, fog_horizon_color, pow(a, 2.0) * u_fog_ground_blend_opacity);
     } else {
         gl_FragColor = color;
     }
