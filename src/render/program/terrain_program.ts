fe973a579555d5ce9475ec7ab66dbcd1e809741d
HarelM <harel.mazor@gmail.com>
2024-06-21 10:20:09 +0300

    Add support for blend in fog and horizon

--- src/render/program/terrain_program.ts
+++ src/render/program/terrain_program.ts
@@ -28,6 +28,8 @@ export type TerrainUniformsType = {
     'u_fog_color': UniformColor;
     'u_fog_ground_blend': Uniform1f;
     'u_fog_ground_blend_opacity': Uniform1f;
+    'u_horizon_color': UniformColor;
+    'u_horizon_fog_blend': Uniform1f;
 };
 
 export type TerrainDepthUniformsType = {
@@ -58,7 +60,9 @@ const terrainUniforms = (context: Context, locations: UniformLocations): Terrain
     'u_fog_matrix': new UniformMatrix4f(context, locations.u_fog_matrix),
     'u_fog_color': new UniformColor(context, locations.u_fog_color),
     'u_fog_ground_blend': new Uniform1f(context, locations.u_fog_ground_blend),
-    'u_fog_ground_blend_opacity': new Uniform1f(context, locations.u_fog_ground_blend_opacity)
+    'u_fog_ground_blend_opacity': new Uniform1f(context, locations.u_fog_ground_blend_opacity),
+    'u_horizon_color': new UniformColor(context, locations.u_horizon_color),
+    'u_horizon_fog_blend': new Uniform1f(context, locations.u_horizon_fog_blend)
 });
 
 const terrainDepthUniforms = (context: Context, locations: UniformLocations): TerrainDepthUniformsType => ({
@@ -85,7 +89,9 @@ const terrainUniformValues = (
     'u_fog_matrix': fogMatrix,
     'u_fog_color': sky ? sky.properties.get('fog-color') : Color.white,
     'u_fog_ground_blend': sky ? sky.properties.get('fog-ground-blend') : 1,
-    'u_fog_ground_blend_opacity': sky ? sky.calculateFogBlendOpacity(pitch) : 0
+    'u_fog_ground_blend_opacity': sky ? sky.calculateFogBlendOpacity(pitch) : 0,
+    'u_horizon_color': sky ? sky.properties.get('horizon-color') : Color.white,
+    'u_horizon_fog_blend': sky ? sky.properties.get('horizon-fog-blend') : 1
 });
 
 const terrainDepthUniformValues = (
--- src/shaders/terrain.fragment.glsl
+++ src/shaders/terrain.fragment.glsl
@@ -1,7 +1,9 @@
 uniform sampler2D u_texture;
 uniform vec4 u_fog_color;
+uniform vec4 u_horizon_color;
 uniform float u_fog_ground_blend;
 uniform float u_fog_ground_blend_opacity;
+uniform float u_horizon_fog_blend;
 
 in vec2 v_texture_pos;
 in float v_fog_depth;
@@ -9,8 +11,9 @@ in float v_fog_depth;
 void main() {
     vec4 color = texture2D(u_texture, v_texture_pos);
     if (v_fog_depth > u_fog_ground_blend) {
+        vec4 fog_horizon_color = mix(u_horizon_color, u_fog_color, 1.0 - u_horizon_fog_blend - v_fog_depth);
         float a = (v_fog_depth - u_fog_ground_blend) / (1.0 - u_fog_ground_blend);
-        gl_FragColor = mix(color, u_fog_color, pow(a * u_fog_ground_blend_opacity, 2.0));
+        gl_FragColor = mix(color, fog_horizon_color, pow(a * u_fog_ground_blend_opacity, 2.0));
     } else {
         gl_FragColor = color;
     }
