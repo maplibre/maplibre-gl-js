18bdfd4145b0fd520178c9a8341ad2d044c8327c
Jakub Pelc <jakub.pelc@email.cz>
2024-06-21 19:05:51 +0200

    Fix shader

--- src/shaders/terrain.fragment.glsl
+++ src/shaders/terrain.fragment.glsl
@@ -11,8 +11,8 @@ in float v_fog_depth;
 void main() {
     vec4 color = texture2D(u_texture, v_texture_pos);
     if (v_fog_depth > u_fog_ground_blend) {
-        vec4 fog_horizon_color = mix(u_fog_color, u_horizon_color, max((v_fog_depth - u_horizon_fog_blend) / (1.0 - u_fog_ground_blend), 0));
-        float a = max((v_fog_depth - u_fog_ground_blend) / (1.0 - u_fog_ground_blend), 0);
+        vec4 fog_horizon_color = mix(u_fog_color, u_horizon_color, max((v_fog_depth - u_horizon_fog_blend) / (1.0 - u_horizon_fog_blend), 0.0));
+        float a = max(v_fog_depth - u_fog_ground_blend, 0.0) / (1.0 - u_fog_ground_blend);
         gl_FragColor = mix(color, fog_horizon_color, pow(a, 2.0) * u_fog_ground_blend_opacity);
     } else {
         gl_FragColor = color;
