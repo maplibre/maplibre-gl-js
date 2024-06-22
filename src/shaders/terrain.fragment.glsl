267fa9062d9cbf1268a558f342a3eb17c3f9a694
Jakub Pelc <jakub.pelc@email.cz>
2024-06-22 09:43:53 +0200

    Make terrain fog shader blending gamma-correct, update render test

--- src/shaders/terrain.fragment.glsl
+++ src/shaders/terrain.fragment.glsl
@@ -8,13 +8,25 @@ uniform float u_horizon_fog_blend;
 in vec2 v_texture_pos;
 in float v_fog_depth;
 
+const float gamma = 2.2;
+
+vec4 gammaToLinear(vec4 color) {
+    return pow(color, vec4(gamma));
+}
+
+vec4 linearToGamma(vec4 color) {
+    return pow(color, vec4(1.0 / gamma));
+}
+
 void main() {
-    vec4 color = texture2D(u_texture, v_texture_pos);
+    vec4 surface_color = texture2D(u_texture, v_texture_pos);
+    vec4 surface_color_linear = gammaToLinear(surface_color);
     if (v_fog_depth > u_fog_ground_blend) {
-        vec4 fog_horizon_color = mix(u_fog_color, u_horizon_color, max((v_fog_depth - u_horizon_fog_blend) / (1.0 - u_horizon_fog_blend), 0.0));
-        float a = max(v_fog_depth - u_fog_ground_blend, 0.0) / (1.0 - u_fog_ground_blend);
-        gl_FragColor = mix(color, fog_horizon_color, pow(a, 2.0) * u_fog_ground_blend_opacity);
+        float blend_color = smoothstep(0.0, 1.0, max((v_fog_depth - u_horizon_fog_blend) / (1.0 - u_horizon_fog_blend), 0.0));
+        vec4 fog_horizon_color_linear = mix(gammaToLinear(u_fog_color), gammaToLinear(u_horizon_color), blend_color);
+        float factor_fog = max(v_fog_depth - u_fog_ground_blend, 0.0) / (1.0 - u_fog_ground_blend);
+        gl_FragColor = linearToGamma(mix(surface_color_linear, fog_horizon_color_linear, pow(factor_fog, 2.0) * u_fog_ground_blend_opacity));
     } else {
-        gl_FragColor = color;
+        gl_FragColor = surface_color;
     }
 }
