5aceaed18e092cb6567def218dd1211bc7672174
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2024-07-09 17:57:10 +0200

    Add same property test

--- test/integration/browser/browser.test.ts
+++ test/integration/browser/browser.test.ts
@@ -426,4 +426,30 @@ describe('Browser tests', () => {
         expect(name).toBe('eman');
 
     }, 20000);
+
+    test('Set Feature Properties Transform should update feature properties in existing property', async () => {
+
+        const name = await page.evaluate(async () => {
+            const transform = `
+                const featurePropertiesTransform = ({_,__,___,____,_____,properties}) => {
+                    if (properties === null) return;
+                    if ('NAME' in properties) {
+                        properties['NAME'] = properties['NAME'].split('').reverse().join('');
+                    }
+                };
+                self.setFeaturePropertiesTransform(featurePropertiesTransform);
+            `;
+            const decodedString = decodeURIComponent(transform);
+            const blob = new Blob([decodedString], {type: 'application/javascript'});
+            const workerUrl = URL.createObjectURL(blob);
+            maplibregl.importScriptInWorkers(workerUrl);
+            await map.once('idle');
+            map.style.sourceCaches['land'].reload();
+            const features = map.queryRenderedFeatures({layers: ['land']});
+            return features[0].properties.NAME;
+        });
+
+        expect(name).toBe('eman');
+
+    }, 20000);
 });
