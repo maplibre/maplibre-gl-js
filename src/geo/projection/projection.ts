c3bd00ef12380275ce1c1e7e233e98d57f3dcc6d
Jakub Pelc <jakub.pelc@email.cz>
2024-08-12 15:47:14 +0200

    Add missing tsdoc param

--- src/geo/projection/projection.ts
+++ src/geo/projection/projection.ts
@@ -115,6 +115,7 @@ export interface Projection {
      * @param tileID - The tile coordinates for which to return a mesh. Meshes for tiles that border the top/bottom mercator edge might include extra geometry for the north/south pole.
      * @param hasBorder - When true, the mesh will also include a small border beyond the 0..EXTENT range.
      * @param allowPoles - When true, the mesh will also include geometry to cover the north (south) pole, if the given tileID borders the mercator range's top (bottom) edge.
+     * @param usage - Specify the usage of the tile mesh, as different usages might use different levels of subdivision.
      */
     getMeshFromTileID(context: Context, tileID: CanonicalTileID, hasBorder: boolean, allowPoles: boolean, usage: TileMeshUsage): Mesh;
 }
