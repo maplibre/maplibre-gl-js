458fef80cc7f3ea0c4bd2b88d6a0a5c88189367b
Isaac Besora <isaac.besora@gmail.com>
2024-08-23 11:18:46 +0200

    Force SDF on icons

--- src/style/load_sprite.ts
+++ src/style/load_sprite.ts
@@ -61,9 +61,9 @@ async function doOnceCompleted(
         const json = (await jsonsMap[spriteName]).data;
 
         for (const id in json) {
-            const {width, height, x, y, sdf, pixelRatio, stretchX, stretchY, content, textFitWidth, textFitHeight} = json[id];
+            const {width, height, x, y, pixelRatio, stretchX, stretchY, content, textFitWidth, textFitHeight} = json[id];
             const spriteData = {width, height, x, y, context};
-            result[spriteName][id] = {data: null, pixelRatio, sdf, stretchX, stretchY, content, textFitWidth, textFitHeight, spriteData};
+            result[spriteName][id] = {data: null, pixelRatio, sdf: true, stretchX, stretchY, content, textFitWidth, textFitHeight, spriteData};
         }
     }
 
