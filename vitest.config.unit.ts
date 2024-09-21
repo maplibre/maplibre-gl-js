860d51cb0d8323b58eca50df1a49ea9aa008ce51
Birk Skyum <birk.skyum@pm.me>
2024-09-21 21:29:48 +0200

    don't set port

--- vitest.config.unit.ts
+++ vitest.config.unit.ts
@@ -1,9 +1,6 @@
 import {defineConfig} from 'vitest/config';
 
 export default defineConfig({
-    server: {
-        port: 80
-    },
     test: {
         name: 'unit',
         environment: 'jsdom',
