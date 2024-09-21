277c9f81e79ca3e0ec2f542862dc1627a05fc8ea
Birk Skyum <birk.skyum@pm.me>
2024-09-21 21:29:28 +0200

    fix hash test

--- vitest.config.unit.ts
+++ vitest.config.unit.ts
@@ -1,6 +1,9 @@
 import {defineConfig} from 'vitest/config';
 
 export default defineConfig({
+    server: {
+        port: 80
+    },
     test: {
         name: 'unit',
         environment: 'jsdom',
