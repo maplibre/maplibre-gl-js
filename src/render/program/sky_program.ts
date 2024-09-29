eda9933432a32d1672abe1987f8fd82bfc879468
Nathan <nathanmolson@gmail.com>
2024-09-29 10:54:26 -0700

    reduce repeat calls to potentially expensive functions

--- src/render/program/sky_program.ts
+++ src/render/program/sky_program.ts
@@ -21,13 +21,18 @@ const skyUniforms = (context: Context, locations: UniformLocations): SkyUniforms
     'u_sky_horizon_blend': new Uniform1f(context, locations.u_sky_horizon_blend),
 });
 
-const skyUniformValues = (sky: Sky, transform: IReadonlyTransform, pixelRatio: number): UniformValues<SkyUniformsType> => ({
-    'u_sky_color': sky.properties.get('sky-color'),
-    'u_horizon_color': sky.properties.get('horizon-color'),
-    'u_horizon': [(transform.width / 2 + getMercatorHorizon(transform) * Math.sin(transform.roll * Math.PI / 180.0))  * pixelRatio,
-        (transform.height / 2 + getMercatorHorizon(transform) * Math.cos(transform.roll * Math.PI / 180.0)) * pixelRatio],
-    'u_horizon_normal' : [Math.sin(transform.roll * Math.PI / 180.0), Math.cos(transform.roll * Math.PI / 180.0)],
-    'u_sky_horizon_blend': (sky.properties.get('sky-horizon-blend') * transform.height / 2) * pixelRatio,
-});
+const skyUniformValues = (sky: Sky, transform: IReadonlyTransform, pixelRatio: number): UniformValues<SkyUniformsType> => {
+    const cos_roll = Math.cos(transform.roll * Math.PI / 180.0);
+    const sin_roll = Math.sin(transform.roll * Math.PI / 180.0);
+    const mercator_horizon  = getMercatorHorizon(transform);
+    return {
+        'u_sky_color': sky.properties.get('sky-color'),
+        'u_horizon_color': sky.properties.get('horizon-color'),
+        'u_horizon': [(transform.width / 2 + mercator_horizon * sin_roll)  * pixelRatio,
+            (transform.height / 2 + mercator_horizon * cos_roll) * pixelRatio],
+        'u_horizon_normal' : [sin_roll, cos_roll],
+        'u_sky_horizon_blend': (sky.properties.get('sky-horizon-blend') * transform.height / 2) * pixelRatio,
+    };
+};
 
 export {skyUniforms, skyUniformValues};
