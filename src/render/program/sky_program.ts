2af62f3aed89a15ff1b75d95a56e801f1253bd51
Nathan <nathanmolson@gmail.com>
2024-09-29 09:20:28 -0700

    update sky shader to use roll angle

--- src/render/program/sky_program.ts
+++ src/render/program/sky_program.ts
@@ -1,4 +1,4 @@
-import {UniformColor, Uniform1f} from '../uniform_binding';
+import {UniformColor, Uniform1f, Uniform2f} from '../uniform_binding';
 import type {Context} from '../../gl/context';
 import type {UniformValues, UniformLocations} from '../uniform_binding';
 import {IReadonlyTransform} from '../../geo/transform_interface';
@@ -8,21 +8,24 @@ import {getMercatorHorizon} from '../../geo/projection/mercator_utils';
 export type SkyUniformsType = {
     'u_sky_color': UniformColor;
     'u_horizon_color': UniformColor;
-    'u_horizon': Uniform1f;
+    'u_horizon': Uniform2f;
+    'u_horizon_normal': Uniform2f;
     'u_sky_horizon_blend': Uniform1f;
 };
 
 const skyUniforms = (context: Context, locations: UniformLocations): SkyUniformsType => ({
     'u_sky_color': new UniformColor(context, locations.u_sky_color),
     'u_horizon_color': new UniformColor(context, locations.u_horizon_color),
-    'u_horizon': new Uniform1f(context, locations.u_horizon),
+    'u_horizon': new Uniform2f(context, locations.u_horizon),
+    'u_horizon_normal': new Uniform2f(context, locations.u_horizon_normal),
     'u_sky_horizon_blend': new Uniform1f(context, locations.u_sky_horizon_blend),
 });
 
 const skyUniformValues = (sky: Sky, transform: IReadonlyTransform, pixelRatio: number): UniformValues<SkyUniformsType> => ({
     'u_sky_color': sky.properties.get('sky-color'),
     'u_horizon_color': sky.properties.get('horizon-color'),
-    'u_horizon': (transform.height / 2 + getMercatorHorizon(transform)) * pixelRatio,
+    'u_horizon': [transform.width / 2, (transform.height / 2 + getMercatorHorizon(transform)) * pixelRatio],
+    'u_horizon_normal' : [Math.sin(transform.roll * Math.PI / 180.0), Math.cos(transform.roll * Math.PI / 180.0)],
     'u_sky_horizon_blend': (sky.properties.get('sky-horizon-blend') * transform.height / 2) * pixelRatio,
 });
 
