6318ceb6aefb9f4ef5656b316c9791e6db430f76
Jamie Popkin <popkinj@littleearth.ca>
2024-11-04 15:00:24 -0800

    Think I was trying to cover to much with this test

--- src/data/feature_index.test.ts
+++ src/data/feature_index.test.ts
@@ -6,25 +6,6 @@ import {log} from 'console';
 describe('FeatureIndex', () => {
     describe('getId', () => {
         const tileID = new OverscaledTileID(0, 0, 0, 0, 0);
-        
-        test('converts boolean ids to numbers', () => {
-            const featureIndex = new FeatureIndex(tileID, 'someProperty');
-            const feature = {
-                id: 1,
-                properties: {
-                    someProperty: true
-                },
-                geometry: {
-                    type: 'Point',
-                    coordinates: [0, 0]
-                },
-                extent: 4096,
-                type: 1,
-                loadGeometry: () => [],
-                toGeoJSON: () => ({})
-            } as unknown as VectorTileFeature;
-            expect(featureIndex.getId(feature, 'sourceLayer')).toBe(1);
-        });
 
         test('uses cluster_id when cluster is true and id is undefined', () => {
             const featureIndex = new FeatureIndex(tileID, 'someProperty');
