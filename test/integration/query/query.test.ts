758a5b15a8b8f6750a263b8e5d716b750d7662c0
Isaac Besora <isaac.besora@gmail.com>
2024-12-12 16:28:26 +0100

    Use default powerPreference on CI

--- test/integration/query/query.test.ts
+++ test/integration/query/query.test.ts
@@ -87,7 +87,7 @@ function performQueryOnFixture(fixture)  {
             interactive: false,
             attributionControl: false,
             pixelRatio: options.pixelRatio,
-            contextAttributes: {preserveDrawingBuffer: true},
+            contextAttributes: {preserveDrawingBuffer: true, powerPreference: 'default'},
             fadeDuration: options.fadeDuration || 0,
             localIdeographFontFamily: options.localIdeographFontFamily || false,
             crossSourceCollisions: typeof options.crossSourceCollisions === 'undefined' ? true : options.crossSourceCollisions
