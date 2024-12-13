9ca21fc3a66912cf6e9d88dbb22667fd7ad45b67
Fatih Mar <mfatihmar@gmail.com>
2024-12-13 17:10:02 +0300

    more shader tests

--- src/shaders/shaders.test.ts
+++ src/shaders/shaders.test.ts
@@ -1,7 +1,14 @@
 import {describe, test, expect} from 'vitest';
 import {transpileToWebGL1} from './shaders';
+import {globSync} from 'glob';
+import fs from 'fs';
 
 describe('Shaders', () => {
+    test('`transpileToWebGL1()` should throw for unexpected type', () => {
+        //@ts-expect-error
+        expect(transpileToWebGL1('invalid', '')).toThrowError();
+    });
+
     test('webgl2 to webgl1 transpiled shader should be identical', () => {
         const vertexSourceWebGL2 = `
             in vec3 aPos;
@@ -32,9 +39,30 @@ describe('Shaders', () => {
         expect(fragmentSourceTranspiled.trim()).equals(fragmentSourceWebGL1.trim());
     });
 
+    // reference: https://webgl2fundamentals.org/webgl/lessons/webgl1-to-webgl2.html
     test('built-in shaders should be written in WebGL2', () => {
+        const shaderFiles = globSync('./*.glsl');
+        for (const shaderFile of shaderFiles) {
+            const shaderSource = fs.readFileSync(shaderFile, 'utf8');
+            expect(shaderSource.includes('attribute')).toBe(false);
+            expect(shaderSource.includes('varying')).toBe(false);
+            expect(shaderSource.includes('gl_FragColor')).toBe(false);
+            expect(shaderSource.includes('texture2D')).toBe(false);
+        }
     });
 
-    test('built-in shaders should be able to transpiled to WebGL1', () => {
+    test('built-in shaders should be able to `transpileToWebGL1()`', () => {
+        const vertexShaderFiles = globSync('./*.vertex.glsl');
+        for (const vertexShaderFile of vertexShaderFiles) {
+            const vertexShaderSource = fs.readFileSync(vertexShaderFile, 'utf8');
+            const vertexShaderTranspiled = transpileToWebGL1('vertex', vertexShaderSource);
+            expect(vertexShaderSource.trim()).not.equals(vertexShaderTranspiled.trim());
+        }
+        const fragmentShaderFiles = globSync('./*.fragment.glsl');
+        for (const fragmentShaderFile of fragmentShaderFiles) {
+            const fragmentShaderSource = fs.readFileSync(fragmentShaderFile, 'utf8');
+            const fragmentShaderTranspiled = transpileToWebGL1('fragment', fragmentShaderSource);
+            expect(fragmentShaderSource.trim()).not.equals(fragmentShaderTranspiled.trim());
+        }
     });
 });
