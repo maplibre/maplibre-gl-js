ca68a047713c40b1b3138aaa459dafbffce61383
Abel Vázquez Montoro <abel.vm@gmail.com>
2025-03-03 19:11:07 +0100

    Changed expectedBytes

--- test/build/min.test.ts
+++ test/build/min.test.ts
@@ -38,7 +38,7 @@ describe('test min build', () => {
         const decreaseQuota = 4096;
 
         // feel free to update this value after you've checked that it has changed on purpose :-)
-        const expectedBytes = 909799;
+        const expectedBytes = 911457;
 
         expect(actualBytes).toBeLessThan(expectedBytes + increaseQuota);
         expect(actualBytes).toBeGreaterThan(expectedBytes - decreaseQuota);
