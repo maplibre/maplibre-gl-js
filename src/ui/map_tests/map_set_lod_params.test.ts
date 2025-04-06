43ef06229f083b52ca2138bdd1994fe855c27c96
Nathan <nathanmolson@gmail.com>
2025-04-06 10:20:57 -0700

    add unit tests for new map API function

---
+++ src/ui/map_tests/map_set_lod_params.test.ts
@@ -0,0 +1,54 @@
+import {beforeEach, test, expect} from 'vitest';
+import {createMap, beforeMapTest} from '../../util/test/util';
+
+beforeEach(() => {
+    beforeMapTest();
+    global.fetch = null;
+});
+
+test('set tile LOD params for a specific source', () => new Promise<void>(done => {
+    const map = createMap({interactive: false}, (err, map) => {
+        expect(err).toBeFalsy();
+        map.addSource('source-id1', {type: 'raster', url: ''});
+        map.addSource('source-id2', {type: 'raster', url: ''});
+
+        expect(map.getSource('source-id1').calculateTileZoom).toBeFalsy();
+        expect(map.setSourceTileLodParams(1, 1, 'source-id1')).toBe(true);
+        expect(map.getSource('source-id1').calculateTileZoom).toBeTruthy();
+        expect(map.getSource('source-id2').calculateTileZoom).toBeFalsy();
+
+        done();
+    });
+}));
+
+test('set tile LOD params for all sources', () => new Promise<void>(done => {
+    const map = createMap({interactive: false}, (err, map) => {
+        expect(err).toBeFalsy();
+        map.addSource('source-id1', {type: 'raster', url: ''});
+        map.addSource('source-id2', {type: 'raster', url: ''});
+
+        expect(map.getSource('source-id1').calculateTileZoom).toBeFalsy();
+        expect(map.getSource('source-id2').calculateTileZoom).toBeFalsy();
+        expect(map.setSourceTileLodParams(1, 1)).toBe(true);
+        expect(map.getSource('source-id1').calculateTileZoom).toBeTruthy();
+        expect(map.getSource('source-id2').calculateTileZoom).toBeTruthy();
+
+        done();
+    });
+}));
+
+test('set tile LOD params for a non-existent source', () => new Promise<void>(done => {
+    const map = createMap({interactive: false}, (err, map) => {
+        expect(err).toBeFalsy();
+        map.addSource('source-id1', {type: 'raster', url: ''});
+        map.addSource('source-id2', {type: 'raster', url: ''});
+
+        expect(map.getSource('source-id1').calculateTileZoom).toBeFalsy();
+        expect(map.getSource('source-id2').calculateTileZoom).toBeFalsy();
+        expect(map.setSourceTileLodParams(1, 1, 'non-existent-source-id')).toBe(false);
+        expect(map.getSource('source-id1').calculateTileZoom).toBeFalsy();
+        expect(map.getSource('source-id2').calculateTileZoom).toBeFalsy();
+
+        done();
+    });
+}));
