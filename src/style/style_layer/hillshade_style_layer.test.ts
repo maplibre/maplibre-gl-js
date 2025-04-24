f488aef2ccd21551b5d45e1fb00ba2c1115a2629
Nathan Olson <nathanmolson@gmail.com>
2025-04-24 11:09:36 -0700

    Additional Hillshade Methods (#5768)
    * inplement GDAL's basic hillshade algorithm
    
    * add new hillshade algorithms
    
    * set hillshade method based on map style
    
    * add illumination altitude and basic method
    
    * remove unrelated changes
    
    * support multidirectional hillshade
    
    * fix "combined"
    
    * improve how combined uses shadows and highlights
    
    * add uniform binding for float array and uniform binding unit tests
    
    * fix basic hillshade color blending
    
    * make hillshade properly depend on tileSize
    
    * fix lint
    
    * fix UniformFloatArray
    
    * support new style spec definitions
    
    * support new style spec definitions
    
    * update types to camelCase
    
    * hook up new hillshade style spec
    
    * throw error if hillshade illumination properties are empty array.
    
    * add hillshade style layer tests
    
    * add hilshade style layer tests
    
    * lint
    
    * fix test warnings
    
    * fix unit test
    
    * add render tests for new hillshade methods
    
    * multidirectional hillshade render test
    
    * hook up hillshade-exaggeration to hew hillshade methods
    
    * add hillshade example
    
    * update changelog
    
    * add comments in shader
    
    * update to latest version of style spec
    
    * fix style-spec integrity check
    
    * increase expected bytes
    
    * update expectedBytes
    
    * update failing render tests
    
    * change if/else to switch/case
    
    * remove terrain from hillshade example
    
    * change hillshade method to switch/case
    
    * split unit test
    
    * replace defines null option with default empty array.
    
    * Add new, simple hillshade demo.
    
    * Add GDAL links in hillshade comments
    
    * remove empty array check in HillshadeStyleLayer. Will be handled in style spec.
    
    * fix unit test
    
    * update changelog to highlight rendering change for hillshade layers with 512x512 tiles.
    
    * add multidirectional hillshade example
    
    * remove hillshade style exploration example
    
    * update style spec version
    
    * replace for loop with map
    
    * replace for loops with Array fill and concat

---
+++ src/style/style_layer/hillshade_style_layer.test.ts
@@ -0,0 +1,107 @@
+import {describe, test, expect} from 'vitest';
+import {HillshadeStyleLayer} from './hillshade_style_layer';
+import {Color, type LayerSpecification} from '@maplibre/maplibre-gl-style-spec';
+import {createStyleLayer} from '../create_style_layer';
+import {degreesToRadians, extend} from '../../util/util';
+
+function createLayerSpec(properties?): LayerSpecification {
+    return extend({
+        type: 'hillshade',
+        id: 'hillshade',
+        source: 'hillshadeSource'
+    }, properties);
+}
+
+describe('HillshadeStyleLayer', () => {
+
+    test('default', () => {
+        const layerSpec = createLayerSpec();
+        const layer = createStyleLayer(layerSpec);
+        expect(layer).toBeInstanceOf(HillshadeStyleLayer);
+        
+        const illumination = (layer as HillshadeStyleLayer).getIlluminationProperties();
+        expect(illumination.directionRadians).toEqual([degreesToRadians(335)]);
+        expect(illumination.altitudeRadians).toEqual([degreesToRadians(45)]);
+        expect(illumination.highlightColor).toEqual([Color.white]);
+        expect(illumination.shadowColor).toEqual([Color.black]);
+    });
+
+    test('single-value illumination parameters', () => {
+        const layerSpec = createLayerSpec({
+            paint: {
+                'hillshade-illumination-direction': 3,
+                'hillshade-illumination-altitude': 4,
+                'hillshade-highlight-color': '#FF0000',
+                'hillshade-shadow-color': '#FFFFFF',
+            }
+        });
+        
+        const layer = createStyleLayer(layerSpec);
+        expect(layer).toBeInstanceOf(HillshadeStyleLayer);
+        
+        const illumination = (layer as HillshadeStyleLayer).getIlluminationProperties();
+        expect(illumination.directionRadians).toEqual([degreesToRadians(3)]);
+        expect(illumination.altitudeRadians).toEqual([degreesToRadians(4)]);
+        expect(illumination.highlightColor).toEqual([Color.red]);
+        expect(illumination.shadowColor).toEqual([Color.white]);
+    });
+
+    test('array-value illumination parameters', () => {
+        const layerSpec = createLayerSpec({
+            paint: {
+                'hillshade-illumination-direction': [6, 7],
+                'hillshade-illumination-altitude': [8, 9],
+                'hillshade-highlight-color': ['#FF0000','#FF0000'],
+                'hillshade-shadow-color': ['#FF0000', '#FFFFFF'],
+            }
+        });
+        
+        const layer = createStyleLayer(layerSpec);
+        expect(layer).toBeInstanceOf(HillshadeStyleLayer);
+        
+        const illumination = (layer as HillshadeStyleLayer).getIlluminationProperties();
+        expect(illumination.directionRadians).toEqual([degreesToRadians(6), degreesToRadians(7)]);
+        expect(illumination.altitudeRadians).toEqual([degreesToRadians(8), degreesToRadians(9)]);
+        expect(illumination.highlightColor).toEqual([Color.red, Color.red]);
+        expect(illumination.shadowColor).toEqual([Color.red, Color.white]);
+    });
+
+    test('mixed illumination parameters: number, color, and arrays', () => {
+        const layerSpec = createLayerSpec({
+            paint: {
+                'hillshade-illumination-direction': [6, 7],
+                'hillshade-illumination-altitude': 23,
+                'hillshade-highlight-color': ['#FF0000'],
+                'hillshade-shadow-color': '#000000',
+            }
+        });
+        
+        const layer = createStyleLayer(layerSpec);
+        expect(layer).toBeInstanceOf(HillshadeStyleLayer);
+        
+        const illumination = (layer as HillshadeStyleLayer).getIlluminationProperties();
+        expect(illumination.directionRadians).toEqual([degreesToRadians(6), degreesToRadians(7)]);
+        expect(illumination.altitudeRadians).toEqual([degreesToRadians(23), degreesToRadians(23)]);
+        expect(illumination.highlightColor).toEqual([Color.red, Color.red]);
+        expect(illumination.shadowColor).toEqual([Color.black, Color.black]);
+    });
+
+    test('mixed illumination parameters: default, number, and arrays', () => {
+        const layerSpec = createLayerSpec({
+            paint: {
+                'hillshade-illumination-altitude': 23,
+                'hillshade-highlight-color': ['#FF0000'],
+                'hillshade-shadow-color': ['#000000', '#FFFFFF'],
+            }
+        });
+        
+        const layer = createStyleLayer(layerSpec);
+        expect(layer).toBeInstanceOf(HillshadeStyleLayer);
+        
+        const illumination = (layer as HillshadeStyleLayer).getIlluminationProperties();
+        expect(illumination.directionRadians).toEqual([degreesToRadians(335), degreesToRadians(335)]);
+        expect(illumination.altitudeRadians).toEqual([degreesToRadians(23), degreesToRadians(23)]);
+        expect(illumination.highlightColor).toEqual([Color.red, Color.red]);
+        expect(illumination.shadowColor).toEqual([Color.black, Color.white]);
+    });
+});
