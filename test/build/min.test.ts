6840b709df40c4ed215083b814104df72b1ca5cf
Nathan <nathanmolson@gmail.com>
2025-05-19 18:13:14 -0700

    update expectedBytes

--- test/build/min.test.ts
+++ test/build/min.test.ts
@@ -38,7 +38,7 @@ describe('test min build', () => {
         const decreaseQuota = 4096;
 
         // feel free to update this value after you've checked that it has changed on purpose :-)
-        const expectedBytes = 914955;
+        const expectedBytes = 932407;
 
         expect(actualBytes).toBeLessThan(expectedBytes + increaseQuota);
         expect(actualBytes).toBeGreaterThan(expectedBytes - decreaseQuota);
