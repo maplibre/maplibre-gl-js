3c1cfaf121de3e490d15010c884bc46cce8fde71
Nathan <nathanmolson@gmail.com>
2025-05-21 14:18:15 -0700

    fix lint

--- src/util/image.ts
+++ src/util/image.ts
@@ -1,4 +1,4 @@
-import {Color} from '@maplibre/maplibre-gl-style-spec';
+import {type Color} from '@maplibre/maplibre-gl-style-spec';
 import {register} from './web_worker_transfer';
 
 export type Size = {
