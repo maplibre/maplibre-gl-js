d381f443e313c33b422fb3d247830dece7499fbe
Birk Skyum <birk.skyum@pm.me>
2025-08-13 23:12:04 +0200

    keep the amd/umd bundle

--- rollup.config.ts
+++ rollup.config.ts
@@ -11,9 +11,54 @@ const outputFile = production ? 'dist/maplibre-gl.js' : 'dist/maplibre-gl-dev.js
 const esmOutputFile = production ? 'dist/maplibre-gl.mjs' : 'dist/maplibre-gl-dev.mjs';
 
 const config: RollupOptions[] = [
-// ESM build configuration
+    // Original AMD/UMD build configuration
     {
-    // First pass for ESM: Split into ES modules
+        // First pass: Bundle GL JS into three AMD chunks for UMD build
+        // - staging/maplibregl/index.js: the main module, plus all its dependencies not shared by the worker module
+        // - staging/maplibregl/worker.js: the worker module, plus all dependencies not shared by the main module
+        // - staging/maplibregl/shared.js: the set of modules that are dependencies of both the main module and the worker module
+        input: ['src/index.ts', 'src/source/worker.ts'],
+        output: {
+            dir: 'staging/maplibregl',
+            format: 'amd',
+            sourcemap: 'inline',
+            indent: false,
+            chunkFileNames: 'shared.js',
+            amd: {
+                autoId: true,
+            },
+            minifyInternalExports: production
+        },
+        onwarn: (message) => {
+            console.error(message);
+            throw message;
+        },
+        treeshake: production,
+        plugins: plugins(production)
+    }, {
+        // Second pass: Bundle together the three AMD chunks into a single UMD bundle
+        input: 'build/rollup/maplibregl.js',
+        output: {
+            name: 'maplibregl',
+            file: outputFile,
+            format: 'umd',
+            sourcemap: true,
+            indent: false,
+            intro: fs.readFileSync('build/rollup/bundle_prelude.js', 'utf8'),
+            banner
+        },
+        watch: {
+            buildDelay: 1000
+        },
+        treeshake: false,
+        plugins: [
+            sourcemaps(),
+            ...production ? [] : [watchStagingPlugin]
+        ],
+    },
+    // ESM build configuration
+    {
+        // First pass for ESM: Split into ES modules
         input: ['src/index.ts', 'src/source/worker.ts'],
         output: {
             dir: 'staging/esm',
