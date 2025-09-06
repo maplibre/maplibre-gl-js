93d00f0ce02371fcae711726707fcfd80e79add3
Lucas Wojciechowski <hello@lucaswoj.com>
2025-09-05 17:27:02 -0800

    Refactor

--- test/build/min.test.ts
+++ test/build/min.test.ts
@@ -38,7 +38,7 @@ describe('test min build', () => {
         const decreaseQuota = 4096;
 
         // feel free to update this value after you've checked that it has changed on purpose :-)
-        const expectedBytes = 944684;
+        const expectedBytes = 945707;
 
         expect(actualBytes).toBeLessThan(expectedBytes + increaseQuota);
         expect(actualBytes).toBeGreaterThan(expectedBytes - decreaseQuota);
