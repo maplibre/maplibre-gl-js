d893faefcb83ac0a1742e91dd1febdbcef55a322
wayofthefuture <wayofthefuture@users.noreply.github.com>
2025-09-30 13:50:39 -0500

    Build size

--- test/build/min.test.ts
+++ test/build/min.test.ts
@@ -38,7 +38,7 @@ describe('test min build', () => {
         const decreaseQuota = 4096;
 
         // feel free to update this value after you've checked that it has changed on purpose :-)
-        const expectedBytes = 945709;
+        const expectedBytes = 946860;
 
         expect(actualBytes).toBeLessThan(expectedBytes + increaseQuota);
         expect(actualBytes).toBeGreaterThan(expectedBytes - decreaseQuota);
