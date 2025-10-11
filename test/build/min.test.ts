8b7c8489256684428b04a8a69b113f24a2efb548
Lars Maxfield <maxfield.lars@gmail.com>
2025-10-11 22:38:37 +0200

    Increase min bundle expected bytes

--- test/build/min.test.ts
+++ test/build/min.test.ts
@@ -38,7 +38,7 @@ describe('test min build', () => {
         const decreaseQuota = 4096;
 
         // feel free to update this value after you've checked that it has changed on purpose :-)
-        const expectedBytes = 954720;
+        const expectedBytes = 954841;
 
         expect(actualBytes).toBeLessThan(expectedBytes + increaseQuota);
         expect(actualBytes).toBeGreaterThan(expectedBytes - decreaseQuota);
