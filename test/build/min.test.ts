4fdbad86217f63b1207e93face4af9387c63a6af
Lucas Wojciechowski <hello@lucaswoj.com>
2025-10-13 09:56:26 -0800

    Update min.test.ts

--- test/build/min.test.ts
+++ test/build/min.test.ts
@@ -38,7 +38,7 @@ describe('test min build', () => {
         const decreaseQuota = 4096;
 
         // feel free to update this value after you've checked that it has changed on purpose :-)
-        const expectedBytes = 956462;
+        const expectedBytes = 955458;
 
         expect(actualBytes).toBeLessThan(expectedBytes + increaseQuota);
         expect(actualBytes).toBeGreaterThan(expectedBytes - decreaseQuota);
