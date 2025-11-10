851ad205a453858afcd9b7af19342dd6da59aa98
Lucas Wojciechowski <hello@lucaswoj.com>
2025-11-10 09:55:54 -0800

    Fix runtime exception when calling GeoJSONSource#updateData after unloading a vector tile

--- src/source/geojson_source.ts
+++ src/source/geojson_source.ts
@@ -466,6 +466,9 @@ export class GeoJSONSource extends Evented implements Source {
      * @internal
      */
     shouldReloadTile(tile: Tile, {nextBounds, prevIds}: GeoJSONSourceShouldReloadTileOptions) : boolean {
+        // If the this.latestFeatureIndex is `null`, the tile has been unloaded, and doesn't need to be refreshed.
+        if (!tile.latestFeatureIndex) return false;
+
         // Update the tile if it PREVIOUSLY contained an updated feature.
         const layers = tile.latestFeatureIndex.loadVTLayers();
         for (let i = 0; i < tile.latestFeatureIndex.featureIndexArray.length; i++) {
