e81444554f467c1bf37d7ca0c66fd8453388ac65
HarelM <harel.mazor@gmail.com>
2025-11-11 20:28:42 +0200

    Remove comment

--- src/tile/tile_manager.ts
+++ src/tile/tile_manager.ts
@@ -582,7 +582,7 @@ export class TileManager extends Evented {
         }
 
         // When tilemanager is used for terrain also load parent tiles for complete rendering of 3d terrain levels
-        if (this.usedForTerrain) { // HM TODO: this probably needs to be part of covering tiles?
+        if (this.usedForTerrain) {
             idealTileIDs = this._addTerrainIdealTiles(idealTileIDs);
         }
 
