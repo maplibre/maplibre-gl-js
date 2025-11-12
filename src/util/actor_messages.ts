57c8eb1e039af76065874458f33198b696623aae
Lucas Wojciechowski <hello@lucaswoj.com>
2025-11-12 14:16:46 -0800

    Improve GeoJSON#updateData performance on small diffs by not sending entire "data" back from worker (#6668)
    * First prototype
    
    * Update ator_messages.ts
    
    * Work through misc exge cases
    
    * Remove exceptions
    
    * Don't send diff back at all
    
    * Fix edge cases
    
    * Add comment
    
    * Fix lint error
    
    * Add console message
    
    * Add unit test
    
    * WIP
    
    * WIP
    
    * Fix lint
    
    * Add better comments
    
    * Revise test name
    
    * Move 'data' case to top of if block
    
    * Revert example page
    
    * Update changelog
    
    * Remove comments
    
    * Typo
    
    * Convert GeoJSONSource#_data to object
    
    * Extract '_applyDiff' method
    
    * Fix CI
    
    * Add ExactlyOne type

--- src/util/actor_messages.ts
+++ src/util/actor_messages.ts
@@ -28,6 +28,7 @@ export type GetClusterLeavesParams = ClusterIDAndSource & { limit: number; offse
  */
 export type GeoJSONWorkerSourceLoadDataResult = {
     data?: GeoJSON.GeoJSON;
+    shouldApplyDiff?: boolean;
     resourceTiming?: {[_: string]: Array<PerformanceResourceTiming>};
     abandoned?: boolean;
 };
