64172a542437c4b96a25355f0ebe1060ef49005b
Lucas Wojciechowski <hello@lucaswoj.com>
2025-11-12 12:55:54 -0800

    Fix CI

--- test/build/min.test.ts
+++ test/build/min.test.ts
@@ -38,7 +38,7 @@ describe('test min build', () => {
         const decreaseQuota = 4096;
 
         // feel free to update this value after you've checked that it has changed on purpose :-)
-        const expectedBytes = 1014309;
+        const expectedBytes = 1015376;
 
         expect(actualBytes, `Consider changing expectedBytes to: ${actualBytes}`).toBeLessThan(expectedBytes + increaseQuota);
         expect(actualBytes).toBeGreaterThan(expectedBytes - decreaseQuota);
