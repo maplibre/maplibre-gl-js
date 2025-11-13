1884c17f149a1e9f7b6a8fcd05fb189a6f8d1b70
Lucas Wojciechowski <hello@lucaswoj.com>
2025-11-13 09:11:34 -0800

    Update feature_index.ts

--- src/data/feature_index.ts
+++ src/data/feature_index.ts
@@ -346,7 +346,7 @@ export function getFeatureId<T extends GeoJSON.Feature | VectorTileFeature>(feat
     let id: T['id'] = feature.id;
     if (promoteId) {
         const propName = typeof promoteId === 'string' ? promoteId : promoteId[sourceLayerId];
-        id = feature.properties[propName] as T['id'];
+        id = feature.properties[propName];
         if (typeof id === 'boolean') id = Number(id);
 
         // When cluster is true, the id is the cluster_id even though promoteId is set
