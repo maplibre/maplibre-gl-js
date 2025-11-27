eb6278bb62f8b28ce96694b71bd49f4496f71fa6
Lucas Wojciechowski <hello@lucaswoj.com>
2025-11-27 08:19:26 -0800

    Update vectortile_to_geojson.ts

--- src/util/vectortile_to_geojson.ts
+++ src/util/vectortile_to_geojson.ts
@@ -50,57 +50,60 @@ export class GeoJSONFeature {
         this.id = id;
     }
 
-    // Copied from https://github.com/mapbox/vector-tile-js/blob/f1457ee47d0a261e6246d68c959fbd12bf56aeeb/index.js
+    private projectPoint(p: Point, x0: number, y0: number, size: number): [number, number] {
+        return [
+            (p.x + x0) * 360 / size - 180,
+            360 / Math.PI * Math.atan(Math.exp((1 - (p.y + y0) * 2 / size) * Math.PI)) - 90
+        ];
+    }
+
+    private projectLine(line: Point[], x0: number, y0: number, size: number) {
+        return line.map(p => this.projectPoint(p, x0, y0, size));
+    }
+
     get geometry(): GeoJSON.Geometry {
         if (this._geometry) return this._geometry;
 
         const feature = this._vectorTileFeature;
 
-        const size = feature.extent * Math.pow(2, this.z),
-            x0 = feature.extent * this.x,
-            y0 = feature.extent * this.y,
-            vtCoords = feature.loadGeometry();
+        // Copied from https://github.com/mapbox/vector-tile-js/blob/f1457ee47d0a261e6246d68c959fbd12bf56aeeb/index.js
+        const size = feature.extent * Math.pow(2, this.z);
+        const x0 = feature.extent * this.x;
+        const y0 = feature.extent * this.y;
+        const vtCoords = feature.loadGeometry();
 
-        function projectPoint(p: Point) {
-            return [
-                (p.x + x0) * 360 / size - 180,
-                360 / Math.PI * Math.atan(Math.exp((1 - (p.y + y0) * 2 / size) * Math.PI)) - 90
-            ];
-        }
-
-        function projectLine(line: Point[]) {
-            return line.map(projectPoint);
-        }
-
-        if (feature.type === 1) {
-            const points = [];
-            for (const line of vtCoords) {
-                points.push(line[0]);
+        switch (feature.type) {
+            case 1: {
+                const points = [];
+                for (const line of vtCoords) {
+                    points.push(line[0]);
+                }
+                const coordinates = this.projectLine(points, x0, y0, size);
+                this._geometry = points.length === 1 ?
+                    {type: 'Point', coordinates: coordinates[0]} :
+                    {type: 'MultiPoint', coordinates};
+                break;
             }
-            const coordinates = projectLine(points);
-            this._geometry = points.length === 1 ?
-                {type: 'Point', coordinates: coordinates[0]} :
-                {type: 'MultiPoint', coordinates};
-
-        } else if (feature.type === 2) {
-
-            const coordinates = vtCoords.map(projectLine);
-            this._geometry = coordinates.length === 1 ?
-                {type: 'LineString', coordinates: coordinates[0]} :
-                {type: 'MultiLineString', coordinates};
-
-        } else if (feature.type === 3) {
-            const polygons = classifyRings(vtCoords);
-            const coordinates = [];
-            for (const polygon of polygons) {
-                coordinates.push(polygon.map(projectLine));
+            case 2: {
+                const coordinates = vtCoords.map(coord => this.projectLine(coord, x0, y0, size));
+                this._geometry = coordinates.length === 1 ?
+                    {type: 'LineString', coordinates: coordinates[0]} :
+                    {type: 'MultiLineString', coordinates};
+                break;
             }
-            this._geometry = coordinates.length === 1 ?
-                {type: 'Polygon', coordinates: coordinates[0]} :
-                {type: 'MultiPolygon', coordinates};
-        } else {
-
-            throw new Error('unknown feature type');
+            case 3: {
+                const polygons = classifyRings(vtCoords);
+                const coordinates = [];
+                for (const polygon of polygons) {
+                    coordinates.push(polygon.map(coord => this.projectLine(coord, x0, y0, size)));
+                }
+                this._geometry = coordinates.length === 1 ?
+                    {type: 'Polygon', coordinates: coordinates[0]} :
+                    {type: 'MultiPolygon', coordinates};
+                break;
+            }
+            default:
+                throw new Error(`unknown feature type: ${this.type}`);
         }
 
         return this._geometry;
