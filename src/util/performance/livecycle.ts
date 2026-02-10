0e1709ee417fd68e50c6fe4d5f3ab0d64be9fcb0
Frank Elsinga <frank@elsinga.de>
2026-02-10 19:30:10 +0100

    simplify the LivecyclePerformanceObserver

--- src/util/performance/livecycle.ts
+++ src/util/performance/livecycle.ts
@@ -15,56 +15,30 @@ export type LivecyclePerformanceMetrics = {
  * @group Performance
  */
 export class LivecyclePerformanceObserver implements IPerformanceObserver {
-    private static _nextId = 0;
-    private _id: number;
-
-    // Unique markers for this instance to avoid global collision
-    private _createMarker: string;
-    private _loadMarker: string;
-    private _fullLoadMarker: string;
-    private _loadTimeMeasure: string;
-    private _fullLoadTimeMeasure: string;
-    private _loadTimeMs: number;
-    private _fullLoadTimeMs: number;
-
-    constructor() {
-        this._id = LivecyclePerformanceObserver._nextId++;
-        this._createMarker = `create-${this._id}`;
-        this._loadMarker = `load-${this._id}`;
-        this._fullLoadMarker = `fullLoad-${this._id}`;
-        this._loadTimeMeasure = `load-${this._id}`;
-        this._fullLoadTimeMeasure = `fullLoad-${this._id}`;
-    }
+    private _createTimeAt?: number;
+    private _loadTimeAt?: number;
+    private _fullLoadTimeAt?: number;
 
     /** {@inheritDoc} */
-    observe(type: PerformanceEventType, _timestamp: number): void {
+    observe(type: PerformanceEventType, timestamp: number): void {
         switch (type) {
             case 'create':
-                performance.mark(this._createMarker);
+                this._createTimeAt = timestamp;
                 break;
             case 'load':
-                performance.mark(this._loadMarker);
+                this._loadTimeAt = timestamp;
                 break;
             case 'fullLoad':
-                performance.mark(this._fullLoadMarker);
-
-                // Ensure measures are taken before querying
-                performance.measure(this._loadTimeMeasure, this._createMarker, this._loadMarker);
-                performance.measure(this._fullLoadTimeMeasure, this._createMarker, this._fullLoadMarker);
-
-                this._loadTimeMs = performance.getEntriesByName(this._loadTimeMeasure)[0]?.duration || 0;
-                this._fullLoadTimeMs = performance.getEntriesByName(this._fullLoadTimeMeasure)[0]?.duration || 0;
+                this._fullLoadTimeAt = timestamp;
                 break;
         }
     }
 
     /** {@inheritDoc} */
     disconnect(): void {
-        performance.clearMarks(this._createMarker);
-        performance.clearMarks(this._loadMarker);
-        performance.clearMarks(this._fullLoadMarker);
-        performance.clearMeasures(this._fullLoadTimeMeasure);
-        performance.clearMeasures(this._loadTimeMeasure);
+        this._createTimeAt = undefined;
+        this._loadTimeAt = undefined;
+        this._fullLoadTimeAt = undefined;
     }
 
     /**
@@ -73,8 +47,8 @@ export class LivecyclePerformanceObserver implements IPerformanceObserver {
      */
     takeRecords(): LivecyclePerformanceMetrics {
         return {
-            loadTimeMs: this._loadTimeMs,
-            fullLoadTimeMs: this._fullLoadTimeMs,
+            loadTimeMs: this._loadTimeAt ? this._loadTimeAt - this._createTimeAt : undefined,
+            fullLoadTimeMs: this._fullLoadTimeAt ? this._fullLoadTimeAt - this._createTimeAt : undefined,
         };
     }
 }
