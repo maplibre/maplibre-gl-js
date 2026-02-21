415655c3b31f583dd4f2d65389884fdf8fd8bc6a
Lars Maxfield <83759569+larsmaxfield@users.noreply.github.com>
2026-02-21 19:58:58 +0100

    Add resampling handling to raster layer

--- src/style/style_layer/raster_style_layer.ts
+++ src/style/style_layer/raster_style_layer.ts
@@ -5,8 +5,34 @@ import {type Transitionable, type Transitioning, type PossiblyEvaluated} from '.
 
 import type {RasterPaintProps} from './raster_style_layer_properties.g';
 import type {LayerSpecification} from '@maplibre/maplibre-gl-style-spec';
+import {type StyleSetterOptions} from '../style';
 
 export const isRasterStyleLayer = (layer: StyleLayer): layer is RasterStyleLayer => layer.type === 'raster';
+
+interface Resamplings {
+    'resampling': RasterPaintProps['raster-resampling'];
+    'raster-resampling': RasterPaintProps['raster-resampling'];
+}
+
+function _handleResampling(resamplings: Partial<Resamplings>): Partial<Resamplings> {
+    let resampling = resamplings.resampling;
+    let rasterResampling = resamplings['raster-resampling'];
+    if (resampling === undefined && rasterResampling === undefined) return {};
+    if (resampling && rasterResampling) {
+        console.warn('Raster layer paint property "resampling" and "raster-resampling" are both specified, but only "resampling" needs to be specified.');
+        if (resampling !== rasterResampling) {
+            console.warn(`Value of "resampling" (${resampling}) is different than that of "raster-resampling" (${rasterResampling}); defaulting to that of "resampling".`);
+            rasterResampling = resampling;
+        }
+    } else {
+        if (resampling) {
+            rasterResampling = resampling;
+        } else {
+            resampling = rasterResampling;
+        }
+    }
+    return {resampling, 'raster-resampling': rasterResampling};
+}
 
 export class RasterStyleLayer extends StyleLayer {
     _transitionablePaint: Transitionable<RasterPaintProps>;
@@ -14,6 +40,29 @@ export class RasterStyleLayer extends StyleLayer {
     paint: PossiblyEvaluated<RasterPaintProps, RasterPaintPropsPossiblyEvaluated>;
 
     constructor(layer: LayerSpecification, globalState: Record<string, any>) {
+        const resampling = layer.paint?.['resampling'];
+        const rasterResampling = layer.paint?.['raster-resampling'];
+        if (resampling || rasterResampling) {
+            layer.paint = {
+                ...layer.paint,
+                ..._handleResampling({
+                    ...(resampling ? {resampling} : {}),
+                    ...(rasterResampling ? {'raster-resampling': resampling} : {}),
+                }),
+            };
+        }
+        layer.paint['raster-resampling'] = 'nearest'; 
         super(layer, properties, globalState);
+    }
+
+    override setPaintProperty(name: string, value: unknown, options: StyleSetterOptions): boolean {
+        if (name === 'resampling' || name === 'raster-resampling') {
+            const otherResampling = name === 'resampling' ? 'raster-resampling' : 'resampling';
+            const handledResamplings = _handleResampling({[name]: value});
+            if (handledResamplings?.[otherResampling]) {
+                super.setPaintProperty(otherResampling, handledResamplings[otherResampling]);
+            }
+        }
+        return super.setPaintProperty(name, value, options);
     }
 }
