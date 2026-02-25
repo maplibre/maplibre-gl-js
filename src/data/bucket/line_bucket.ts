26fd972832a8c5314eafaa32f84125e33caa7bf3
HarelM <harel.mazor@gmail.com>
2026-02-25 13:48:26 +0200

    Fix hardcoded clip start and end parameters

--- src/data/bucket/line_bucket.ts
+++ src/data/bucket/line_bucket.ts
@@ -247,9 +247,9 @@ export class LineBucket implements Bucket {
     }
 
     lineFeatureClips(feature: BucketFeature): LineClips | undefined {
-        if (!!feature.properties && Object.prototype.hasOwnProperty.call(feature.properties, 'mapbox_clip_start') && Object.prototype.hasOwnProperty.call(feature.properties, 'mapbox_clip_end')) {
-            const start = +feature.properties['mapbox_clip_start'];
-            const end = +feature.properties['mapbox_clip_end'];
+        if (!!feature.properties && Object.prototype.hasOwnProperty.call(feature.properties, 'geojsonvt_clip_start') && Object.prototype.hasOwnProperty.call(feature.properties, 'geojsonvt_end')) {
+            const start = +feature.properties['geojsonvt_clip_start'];
+            const end = +feature.properties['geojsonvt_clip_end'];
             return {start, end};
         }
     }
