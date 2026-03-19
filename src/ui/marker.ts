maplibre/maplibre-gl-js

commit ff97355ef77566f3196f983659fd359c3db13f92

Add click event handler for Marker (#7028)
* feat(marker): add click event support

* fix(marker): attach click listener in addTo and detach in remove

* feat: Introduce Marker class with support for click events.

---------

Co-authored-by: Harel M <harel.mazor@gmail.com>

src/ui/marker.ts
@@ -123,6 +123,8 @@ export type MarkerOptions = {
  * **Event** `drag` of type {@link Event} will be fired while dragging.
  *
  * **Event** `dragend` of type {@link Event} will be fired when the marker is finished being dragged.
+ *
+ * **Event** `click` of type {@link Event} will be fired when the marker is clicked.
  */
 export class Marker extends Evented {
     _map: Map;
@@ -292,6 +294,7 @@ export class Marker extends Evented {
             e.preventDefault();
         });
         applyAnchorClass(this._element, this._anchor, 'marker');
+        this._element.addEventListener('click', this._onClick);
 
         if (options && options.className) {
             for (const name of options.className.split(' ')) {
@@ -492,6 +495,10 @@ export class Marker extends Evented {
         this._subpixelPositioning = value;
         return this;
     }
+
+    _onClick = (e: MouseEvent) => {
+        this.fire(new Event('click', {originalEvent: e}));
+    };
 
     _onKeyPress = (e: KeyboardEvent) => {
         const code = e.code;

src/ui/marker.test.ts
@@ -791,6 +791,24 @@ describe('marker', () => {
         map.remove();
     });
 
+    test('Marker fires click event when clicked', () => {
+        const map = createMap();
+        const marker = new Marker()
+            .setLngLat([0, 0])
+            .addTo(map);
+        const el = marker.getElement();
+
+        const clickSpy = vi.fn();
+        marker.on('click', clickSpy);
+
+        // Simulate click on the marker element
+        el.click();
+
+        expect(clickSpy).toHaveBeenCalledTimes(1);
+        
+        map.remove();
+    });
+
     test('Marker with draggable:true does not error if removed on mousedown', () => {
         const map = createMap();
         const marker = new Marker({draggable: true})

commit 7107e5a986d138bade1dfa3eb30b9256ccd3da22

Fix memory leak: remove click listener on marker removal
Move click listener attachment from constructor to addTo() and add
removeEventListener in remove(), matching the pattern used by all
other marker listeners. Fixes leak introduced in #7028.

src/ui/marker.ts
@@ -294,7 +294,6 @@ export class Marker extends Evented {
             e.preventDefault();
         });
         applyAnchorClass(this._element, this._anchor, 'marker');
-        this._element.addEventListener('click', this._onClick);
 
         if (options && options.className) {
             for (const name of options.className.split(' ')) {
@@ -335,6 +334,7 @@ export class Marker extends Evented {
         map.on('terrain', this._update);
         map.on('projectiontransition', this._update);
 
+        this._element.addEventListener('click', this._onClick);
         this.setDraggable(this._draggable);
         this._update();
 
@@ -373,6 +373,7 @@ export class Marker extends Evented {
             this._map.off('touchmove', this._onMove);
             delete this._map;
         }
+        this._element.removeEventListener('click', this._onClick);
         DOM.remove(this._element);
         if (this._popup) this._popup.remove();
         return this;