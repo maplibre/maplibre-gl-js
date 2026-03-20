9145f88dbe778658cd9e0b2690a313686b20c3e0
Frank Elsinga <frank@elsinga.de>
2026-03-20 23:01:24 +0100

    Apply suggestions from code review
    Co-authored-by: Frank Elsinga <frank@elsinga.de>

--- src/source/geojson_worker_source.ts
+++ src/source/geojson_worker_source.ts
@@ -1,6 +1,5 @@
 import {getJSON} from '../util/ajax';
 import {RequestPerformance} from '../util/request_performance';
-import rewind from '@mapbox/geojson-rewind';
 import {GeoJSONWrapper} from '@maplibre/vt-pbf';
 import {EXTENT} from '../data/extent';
 import {GeoJSONVT, type GeoJSONVTOptions, type SuperclusterOptions, type ClusterProperties} from '@maplibre/geojson-vt';
--- src/ui/map.ts
+++ src/ui/map.ts
@@ -22,7 +22,6 @@ import {Event, ErrorEvent, type Listener} from '../util/evented';
 import {type MapEventType, type MapLayerEventType, MapMouseEvent, type MapSourceDataEvent, type MapStyleDataEvent} from './events';
 import {TaskQueue} from '../util/task_queue';
 import {throttle} from '../util/throttle';
-import {webpSupported} from '../util/webp_supported';
 import {type Source} from '../source/source';
 import {type StyleLayer} from '../style/style_layer';
 import {Terrain} from '../render/terrain';
