79b30c06893b82e31abbfcaa46250961b5a69ff8
Frank Elsinga <frank@elsinga.de>
2026-03-20 23:50:06 +0100

    Apply suggestion from @CommanderStorm

--- test/build/sourcemaps.test.ts
+++ test/build/sourcemaps.test.ts
@@ -84,6 +84,6 @@ describe('main sourcemap', () => {
         const s1 = setMinus(actualEntriesInSourcemapJSON, expectedEntriesInSourcemapJSON);
         expect(s1.length).toBeLessThan(5);
         const s2 = setMinus(expectedEntriesInSourcemapJSON, actualEntriesInSourcemapJSON);
-        expect(s2.length).toBeLessThan(17);
+        expect(s2.length).toBeLessThan(16);
     });
 });
