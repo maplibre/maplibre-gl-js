1efdf3d0c6ecdd471ca1a3d1b9cf307a9ce50253
Xavier Bourry <xavier.bourry@polytechnique.org>
2026-04-01 13:56:55 -0400

    [QUAL] Make unit test pass - if WebGL2RenderingContext is not defined

--- src/gl/webgl2.ts
+++ src/gl/webgl2.ts
@@ -2,5 +2,5 @@ export function isWebGL2(
     gl: WebGLRenderingContext | WebGL2RenderingContext
 ): gl is WebGL2RenderingContext {
     // this method is really faster than fetching a cache:
-    return gl instanceof WebGL2RenderingContext;
+    return typeof WebGL2RenderingContext !== 'undefined' && gl instanceof WebGL2RenderingContext;
 }
