55356b00ca9250faa927f23b59fd217624fcc7d6
Frank Elsinga <frank@elsinga.de>
2026-04-03 19:39:34 +0200

    fix _demMatrixCache being ineffective

--- test/bench/benchmarks/terrain_render.ts
+++ test/bench/benchmarks/terrain_render.ts
@@ -2,9 +2,6 @@
 import Benchmark from '../lib/benchmark';
 import createMap from '../lib/create_map';
 
-const width = 1024;
-const height = 768;
-
 export default class TerrainRender extends Benchmark {
 
     map: any;
@@ -13,17 +10,13 @@ export default class TerrainRender extends Benchmark {
         try {
             this.map = await createMap({
                 zoom: 12,
-                width,
-                height,
+                width: 1024,
+                height: 768,
                 center: [10.5, 46.9],
                 pitch: 60,
                 style: {
                     version: 8,
                     sources: {
-                        'openmaptiles': {
-                            'type': 'vector',
-                            'url': 'https://api.maptiler.com/tiles/v3/tiles.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL'
-                        },
                         'terrain-rgb': {
                             'type': 'raster-dem',
                             'url': 'https://api.maptiler.com/tiles/terrain-rgb/tiles.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL'
@@ -38,27 +31,6 @@ export default class TerrainRender extends Benchmark {
                             'id': 'background',
                             'type': 'background',
                             'paint': {'background-color': '#f8f4f0'}
-                        },
-                        {
-                            'id': 'water',
-                            'type': 'fill',
-                            'source': 'openmaptiles',
-                            'source-layer': 'water',
-                            'paint': {'fill-color': '#a0c8f0'}
-                        },
-                        {
-                            'id': 'roads',
-                            'type': 'line',
-                            'source': 'openmaptiles',
-                            'source-layer': 'transportation',
-                            'paint': {'line-color': '#ffffff', 'line-width': 1}
-                        },
-                        {
-                            'id': 'labels',
-                            'type': 'symbol',
-                            'source': 'openmaptiles',
-                            'source-layer': 'place',
-                            'layout': {'text-field': '{name}', 'text-size': 12}
                         }
                     ]
                 },
