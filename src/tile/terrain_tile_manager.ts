2939de5bf243e1050fe28fc2e6f82b099d48d87f
Lucas Wojciechowski <hello@lucaswoj.com>
2026-05-01 07:09:18 -0700

    Remove unused Painter import

--- src/tile/terrain_tile_manager.ts
+++ src/tile/terrain_tile_manager.ts
@@ -11,7 +11,6 @@ import {now} from '../util/time_control';
 import {coveringTiles} from '../geo/projection/covering_tiles';
 import {createMat4f64} from '../util/util';
 import {type CanonicalTileRange} from '../source/image_source';
-import type {Painter} from '../render/painter';
 
 /**
  * @internal
