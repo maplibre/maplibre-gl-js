9db4c42199170f4b81f137b3ea642a87288c52ec
HarelM <HarelM@users.noreply.github.com>
2026-05-03 11:34:24 +0000

    deploy: b0de948357d1f33530788ac2d2d59edef3b02f61

--- benchmarks/main/benchmarks_generated.mjs
+++ benchmarks/main/benchmarks_generated.mjs
@@ -43082,7 +43082,7 @@ var DOM = class DOM {
 };
 //#endregion
 //#region package.json
-var version$4 = "6.0.0-6";
+var version$4 = "6.0.0-7";
 //#endregion
 //#region src/data/raster_bounds_attributes.ts
 var raster_bounds_attributes_default = createLayout([{
@@ -60322,7 +60322,7 @@ function buildStyle() {
 const styleLocations = locationsWithTileID(features).filter((v) => v.zoom < 15);
 window.maplibreglBenchmarks = window.maplibreglBenchmarks || {};
 setWorkerUrl(new URL("./benchmarks_worker.mjs", import.meta.url).toString());
-const version = "main 9454bb7";
+const version = "main b0de948";
 function register(name, bench) {
 	window.maplibreglBenchmarks[name] = window.maplibreglBenchmarks[name] || {};
 	window.maplibreglBenchmarks[name][version] = bench;
