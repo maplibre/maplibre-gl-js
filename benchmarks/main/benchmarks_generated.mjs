5fff5366aec87f80914fc0f5977d84796b1fe3cd
birkskyum <birkskyum@users.noreply.github.com>
2026-05-07 11:18:13 +0000

    deploy: 71542f1685c8b1acca501d8bc6ca397fc11ee6c5

--- benchmarks/main/benchmarks_generated.mjs
+++ benchmarks/main/benchmarks_generated.mjs
@@ -60388,7 +60388,7 @@ function buildStyle() {
 const styleLocations = locationsWithTileID(features).filter((v) => v.zoom < 15);
 window.maplibreglBenchmarks = window.maplibreglBenchmarks || {};
 setWorkerUrl(new URL("./benchmarks_worker.mjs", import.meta.url).toString());
-const version = "main 4412ee1";
+const version = "main 71542f1";
 function register(name, bench) {
 	window.maplibreglBenchmarks[name] = window.maplibreglBenchmarks[name] || {};
 	window.maplibreglBenchmarks[name][version] = bench;
