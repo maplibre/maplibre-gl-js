ab8fe93a249aad8410b76e1ce1589404efc106ef
HarelM <HarelM@users.noreply.github.com>
2026-06-09 11:45:19 +0000

    deploy: e56842a54f0705a65f16fa87191e039159b4fbf8

--- benchmarks/main/benchmarks_generated.mjs
+++ benchmarks/main/benchmarks_generated.mjs
@@ -60214,7 +60214,7 @@ function buildStyle() {
 const styleLocations = locationsWithTileID(features).filter((v) => v.zoom < 15);
 window.maplibreglBenchmarks = window.maplibreglBenchmarks || {};
 setWorkerUrl(new URL("./benchmarks_worker.mjs", import.meta.url).toString());
-const version = "main 17d2478";
+const version = "main e56842a";
 function register(name, bench) {
 	window.maplibreglBenchmarks[name] = window.maplibreglBenchmarks[name] || {};
 	window.maplibreglBenchmarks[name][version] = bench;
