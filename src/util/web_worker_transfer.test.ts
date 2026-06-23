4ce473e1735e99f046167147983505593670ab42
Xavier Bourry <xavier.bourry@polytechnique.org>
2026-06-23 13:29:03 -0400

    Do not serialize undefined properties (#7801)
    * [TEST] Add an example to show that the has filter returns true if undefined ptty and false if ptty is not set
    
    * [FIX] Do not serialize an undefined property for the webworkers
    
    * [MINOR] Remove the example
    
    * [MINOR] Update changelog
    
    * [TEST] Add a test for the has filter with undefined property
    
    * [MINOR] Restore package-lock.json
    
    * [QUAL] Move content of web_worker_transfer_has_filter.test.ts to the existing test file

--- src/util/web_worker_transfer.test.ts
+++ src/util/web_worker_transfer.test.ts
@@ -1,6 +1,8 @@
 import {describe, test, expect} from 'vitest';
 import {AJAXError} from './ajax.ts';
 import {register, serialize, deserialize} from './web_worker_transfer.ts';
+import {featureFilter} from '@maplibre/maplibre-gl-style-spec';
+import {EvaluationParameters} from '../style/evaluation_parameters.ts';
 
 describe('web worker transfer', () => {
     test('round trip', () => {
@@ -152,5 +154,20 @@ describe('web worker transfer', () => {
         expect(() => {
             deserialize(BigInt(123) as unknown);
         }).toThrow();
+    });
+
+    test('"has" filter returns false for undefined or missing properties after serialization', () => {
+        const filter = featureFilter(['has', 'testProperty']);
+        const params = new EvaluationParameters(0);
+
+        const makeFeature = (properties: Record<string, unknown>) => ({
+            type: 1 as const,
+            properties: deserialize(serialize(properties)) as Record<string, unknown>,
+            geometry: []
+        });
+
+        expect(filter.filter(params, makeFeature({testProperty: 'value'}))).toBe(true);
+        expect(filter.filter(params, makeFeature({testProperty: undefined}))).toBe(false);
+        expect(filter.filter(params, makeFeature({}))).toBe(false);
     });
 });
