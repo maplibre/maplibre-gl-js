360f7f82399fc0383929d1d315a814cf153f9a2c
Frank Elsinga <frank@elsinga.de>
2026-07-23 21:41:53 +0200

    wip

--- test/build/sourcemaps.test.ts
+++ test/build/sourcemaps.test.ts
@@ -65,6 +65,8 @@ describe('main sourcemap', () => {
         const expectedEntriesInSourcemapJSON = srcFiles.filter(f => {
             if (f.endsWith('.test.ts'))
                 return false;
+            if (f.endsWith('.g.ts'))
+                return false;
             if (f.startsWith(path.join('src', 'style-spec')))
                 return false;
             if (f === path.join('src', 'util', 'test', 'util.ts'))
