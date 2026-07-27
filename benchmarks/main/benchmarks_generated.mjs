463ec82f01d46bc8b4b570df653e6907030f310f
CommanderStorm <CommanderStorm@users.noreply.github.com>
2026-07-27 16:24:59 +0000

    deploy: f838342f37364fe74041c48585d97ab57b1a8c6c

--- benchmarks/main/benchmarks_generated.mjs
+++ benchmarks/main/benchmarks_generated.mjs
@@ -60923,7 +60923,7 @@ function buildStyle() {
 const styleLocations = locationsWithTileID(features).filter((v) => v.zoom < 15);
 window.maplibreglBenchmarks = window.maplibreglBenchmarks || {};
 setWorkerUrl(new URL("./benchmarks_worker.mjs", import.meta.url).toString());
-const version = "main ca16843";
+const version = "main f838342";
 function register(name, bench) {
 	window.maplibreglBenchmarks[name] = window.maplibreglBenchmarks[name] || {};
 	window.maplibreglBenchmarks[name][version] = bench;
