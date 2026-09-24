0dff33ab118c6d8ea41f7d9d30e0df9d4acf9ea3
Birk Skyum <74932975+birkskyum@users.noreply.github.com>
2026-09-24 08:08:42 +0200

    Fix camera settings being undone during camera movement (#8550)
    * Fix camera settings being undone during camera movement
    
    * Changelog

--- src/ui/map.ts
+++ src/ui/map.ts
@@ -1745,7 +1745,7 @@ export class Map extends Evented<MapEventType> {
      * ```
      */
     setMaxBounds(bounds?: LngLatBoundsLike | null): this {
-        this._camera.transform.setMaxBounds(LngLatBounds.convert(bounds));
+        this._camera.applyTransformChange(tr => tr.setMaxBounds(LngLatBounds.convert(bounds)));
         return this._update();
     }
 
@@ -1775,9 +1775,7 @@ export class Map extends Evented<MapEventType> {
 
         if (minZoom >= defaultMinZoom && minZoom <= this._camera.transform.maxZoom) {
             const zoomBefore = this._camera.transform.zoom;
-            const tr = this._camera.getTransformForUpdate();
-            tr.setMinZoom(minZoom);
-            this._camera.applyUpdatedTransform(tr);
+            this._camera.applyTransformChange(tr => tr.setMinZoom(minZoom));
             this._update();
             if (zoomBefore !== this._camera.transform.zoom) {
                 this.fire(new MapMovementEvent('zoomstart'))
@@ -1830,9 +1828,7 @@ export class Map extends Evented<MapEventType> {
 
         if (maxZoom >= this._camera.transform.minZoom) {
             const zoomBefore = this._camera.transform.zoom;
-            const tr = this._camera.getTransformForUpdate();
-            tr.setMaxZoom(maxZoom);
-            this._camera.applyUpdatedTransform(tr);
+            this._camera.applyTransformChange(tr => tr.setMaxZoom(maxZoom));
             this._update();
             if (zoomBefore !== this._camera.transform.zoom) {
                 this.fire(new MapMovementEvent('zoomstart'))
@@ -1880,9 +1876,7 @@ export class Map extends Evented<MapEventType> {
 
         if (minPitch >= defaultMinPitch && minPitch <= this._camera.transform.maxPitch) {
             const pitchBefore = this._camera.transform.pitch;
-            const tr = this._camera.getTransformForUpdate();
-            tr.setMinPitch(minPitch);
-            this._camera.applyUpdatedTransform(tr);
+            this._camera.applyTransformChange(tr => tr.setMinPitch(minPitch));
             this._update();
             if (pitchBefore !== this._camera.transform.pitch) {
                 this.fire(new MapMovementEvent('pitchstart'))
@@ -1926,9 +1920,7 @@ export class Map extends Evented<MapEventType> {
 
         if (maxPitch >= this._camera.transform.minPitch) {
             const pitchBefore = this._camera.transform.pitch;
-            const tr = this._camera.getTransformForUpdate();
-            tr.setMaxPitch(maxPitch);
-            this._camera.applyUpdatedTransform(tr);
+            this._camera.applyTransformChange(tr => tr.setMaxPitch(maxPitch));
             this._update();
             if (pitchBefore !== this._camera.transform.pitch) {
                 this.fire(new MapMovementEvent('pitchstart'))
@@ -2027,7 +2019,7 @@ export class Map extends Evented<MapEventType> {
      * @see [Render world copies](https://maplibre.org/maplibre-gl-js/docs/examples/render-world-copies/)
      */
     setRenderWorldCopies(renderWorldCopies?: boolean | null): this {
-        this._camera.transform.setRenderWorldCopies(renderWorldCopies);
+        this._camera.applyTransformChange(tr => tr.setRenderWorldCopies(renderWorldCopies));
         return this._update();
     }
 
@@ -2046,7 +2038,7 @@ export class Map extends Evented<MapEventType> {
      * @see [Customize the map transform constrain](https://maplibre.org/maplibre-gl-js/docs/examples/customize-the-map-transform-constrain/)
      */
     setTransformConstrain(constrain?: TransformConstrainFunction | null): this {
-        this._camera.transform.setConstrainOverride(constrain);
+        this._camera.applyTransformChange(tr => tr.setConstrainOverride(constrain));
         return this._update();
     }
 
